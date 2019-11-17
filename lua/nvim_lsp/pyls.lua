5d7fcfd4daba362167826abf25d059828ef479bb
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2019-11-17 13:27:35 -0800

    Add package_json for pyls and update docgen

--- lua/nvim_lsp/pyls.lua
+++ lua/nvim_lsp/pyls.lua
@@ -13,46 +13,11 @@ skeleton.pyls = {
   -- on_new_config = function(new_config) end;
   -- on_attach = function(client, bufnr) end;
   docs = {
+    package_json = "https://github.com/palantir/python-language-server/raw/develop/vscode-client/package.json";
     description = [[
 https://github.com/palantir/python-language-server
 
-python-language-server, a language server for Python
-
-the following settings (with default options) are supported:
-```lua
-settings = {
-  pyls = {
-    enable = true;
-    trace = { server = "verbose"; };
-    commandPath = "";
-    configurationSources = { "pycodestyle" };
-    plugins = {
-      jedi_completion = { enabled = true; };
-      jedi_hover = { enabled = true; };
-      jedi_references = { enabled = true; };
-      jedi_signature_help = { enabled = true; };
-      jedi_symbols = {
-        enabled = true;
-        all_scopes = true;
-      };
-      mccabe = {
-        enabled = true;
-        threshold = 15;
-      };
-      preload = { enabled = true; };
-      pycodestyle = { enabled = true; };
-      pydocstyle = {
-        enabled = false;
-        match = "(?!test_).*\\.py";
-        matchDir = "[^\\.].*";
-      };
-      pyflakes = { enabled = true; };
-      rope_completion = { enabled = true; };
-      yapf = { enabled = true; };
-    };
-  };
-};
-```
+`python-language-server`, a language server for Python.
     ]];
     default_config = {
       root_dir = "vim's starting directory";
