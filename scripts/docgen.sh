eaaec1317a85f4aba5d802599a631442c4769afa
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2019-11-17 09:30:20 -0800

    ./scripts/docgen.sh

--- scripts/docgen.sh
+++ scripts/docgen.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-# exec nvim-master -u NONE +'set rtp+=$PWD' +'luafile scripts/docgen.lua'
 exec nvim-master -u NONE -E -R --headless +'set rtp+=$PWD' +'luafile scripts/docgen.lua' +q
-#exec nvim-master -u NONE -Es +'set rtp+=$PWD' +'luafile scripts/docgen.lua' +q
