71c6d073304b9be95b2c36fd140dfcfa702521e4
Hirokazu Hata <h.hata.ai.t@gmail.com>
2019-11-18 02:38:17 +0900

    Add rls support (#24)

---
+++ lua/nvim_lsp/rls.lua
@@ -0,0 +1,36 @@
+local skeleton = require 'nvim_lsp/skeleton'
+local util = require 'nvim_lsp/util'
+local lsp = vim.lsp
+
+skeleton.rls = {
+  default_config = {
+    cmd = {"rls"};
+    filetypes = {"rust"};
+    root_dir = util.root_pattern("Cargo.toml");
+    log_level = lsp.protocol.MessageType.Warning;
+    settings = {};
+  };
+  docs = {
+    description = [[
+https://github.com/rust-lang/rls
+
+rls, a language server for Rust
+
+Refer to the following for how to setup rls itself.
+https://github.com/rust-lang/rls#setup
+
+See below for rls specific settings.
+https://github.com/rust-lang/rls#configuration
+
+If you want to use rls for a particular build, eg nightly, set cmd as follows:
+
+```lua
+cmd = {"rustup", "run", "nightly", "rls"}
+```
+    ]];
+    default_config = {
+      root_dir = [[root_pattern("Cargo.toml")]];
+    };
+  };
+};
+-- vim:et ts=2 sw=2
