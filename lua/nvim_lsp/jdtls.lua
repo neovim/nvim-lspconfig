ce633e3642a64319b2bba65e598fc9c3f806bfb7
Daniel Voogsgerd <daniel@voogsgerd.nl>
2019-11-18 23:19:49 +0100

    Move jdtls directory to project root

--- lua/nvim_lsp/jdtls.lua
+++ lua/nvim_lsp/jdtls.lua
@@ -3,16 +3,19 @@ local util = require 'nvim_lsp/util'
 local lsp = vim.lsp
 
 local server_name = "jdtls"
-
 skeleton[server_name] = {
   default_config = {
     cmd = {"jdtls"};
     filetypes = {"java"};
-    root_dir = vim.loop.os_homedir;
+    root_dir = util.root_pattern('.project', 'pom.xml', 'project.xml', 'build.gradle', '.git');
     log_level = lsp.protocol.MessageType.Warning;
     settings = {};
   };
   -- on_attach = function(client, bufnr) end;
+  on_new_config = function(new_config)
+    new_config.cmd[2] = '-data';
+    new_config.cmd[3] = new_config.root_dir('.');
+  end;
   docs = {
     description = [[
 For language server related questions visit:
