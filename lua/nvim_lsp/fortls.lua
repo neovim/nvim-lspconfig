2abfde64579e458dc7621b84051122d81bb7f290
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2019-11-26 23:01:42 +0900

    Add support for fortls (#52)

---
+++ lua/nvim_lsp/fortls.lua
@@ -0,0 +1,29 @@
+local skeleton = require 'nvim_lsp/skeleton'
+local util = require 'nvim_lsp/util'
+local lsp = vim.lsp
+
+skeleton.fortls = {
+  default_config = {
+    cmd = {"fortls"};
+    filetypes = {"fortran"};
+    root_dir = util.root_pattern(".fortls");
+    log_level = lsp.protocol.MessageType.Warning;
+    settings = {
+      nthreads = 1,
+    };
+  };
+  -- on_new_config = function(new_config) end;
+  -- on_attach = function(client, bufnr) end;
+  docs = {
+    description = [[
+https://github.com/hansec/fortran-language-server
+
+Fortran Language Server for the Language Server Protocol
+    ]];
+    default_config = {
+      root_dir = [[root_pattern(".fortls")]];
+    };
+  };
+};
+
+-- vim:et ts=2 sw=2
