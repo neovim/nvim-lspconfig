4009499042a1275943488e119a8e89a046a680ad
Shatur95 <genaloner@gmail.com>
2020-02-20 03:12:48 +0200

    Fix review issues

--- lua/nvim_lsp/gdscript.lua
+++ lua/nvim_lsp/gdscript.lua
@@ -9,8 +9,6 @@ configs.gdscript = {
     root_dir = util.root_pattern("project.godot", ".git");
     log_level = lsp.protocol.MessageType.Warning;
   };
-  -- on_new_config = function(new_config) end;
-  -- on_attach = function(client, bufnr) end;
   docs = {
     description = [[
 https://github.com/godotengine/godot
@@ -18,7 +16,7 @@ https://github.com/godotengine/godot
 Language server for GDScript, used by Godot Engine.
 ]];
     default_config = {
-      root_dir = "vim's starting directory";
+      root_dir = util.root_pattern("project.godot", ".git");
     };
   };
 };
