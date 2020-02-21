d45452f41c597a039c33cf50c0b77550bffce556
Alvaro Muñoz Sanchez <pwntester@github.com>
2020-02-21 21:02:41 +0100

    Add support for CodeQL LS

---
+++ lua/nvim_lsp/codeqlls.lua
@@ -0,0 +1,40 @@
+local configs = require "nvim_lsp/configs"
+local util = require "nvim_lsp/util"
+local lsp = vim.lsp
+
+local server_name = "codeqlls"
+
+local root_pattern = util.root_pattern("qlpack.yml")
+
+configs[server_name] = {
+    default_config = {
+        filetypes = {'codeql'};
+        root_dir = function(fname)
+            return root_pattern("qlpack.yml") or util.path.dirname(fname)
+        end;
+        log_level = vim.lsp.protocol.MessageType.Warning;
+        before_init = function(initialize_params, config)
+            initialize_params['workspaceFolders'] = {{
+                name = 'workspace',
+                uri = initialize_params['rootUri']
+            }}
+        end;
+        settings = {
+            search_path = vim.empty_dict()
+        };
+    };
+    docs = {
+        description = [[test]]
+    };
+    on_new_config = function(config)
+        if config.settings.search_path ~= nil and config.settings.search_path ~= '' then
+            search_path="--search-path="
+            for _, path in ipairs(config.settings.search_path) do
+                search_path = search_path..vim.fn.expand(path)..":"
+            end
+            config.cmd = {"codeql", "execute", "language-server", "--check-errors", "ON_CHANGE", "-q", search_path}
+        else
+            config.cmd = {"codeql", "execute", "language-server", "--check-errors", "ON_CHANGE", "-q"}
+        end
+    end;
+}
