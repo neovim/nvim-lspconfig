39bdc2ee09436241da39952e8a90715d387c0d9e
Alvaro Muñoz <alvaro@pwntester.com>
2020-02-23 22:32:49 +0100

    add code review feedback

--- lua/nvim_lsp/codeqlls.lua
+++ lua/nvim_lsp/codeqlls.lua
@@ -5,8 +5,10 @@ local server_name = "codeqlls"
 
 local root_pattern = util.root_pattern("qlpack.yml")
 
+-- vim.empty_dict()
 configs[server_name] = {
     default_config = {
+        cmd = {"codeql", "execute", "language-server", "--check-errors", "ON_CHANGE", "-q"};
         filetypes = {'codeql'};
         root_dir = function(fname)
             return root_pattern("qlpack.yml") or util.path.dirname(fname)
@@ -19,10 +21,11 @@ configs[server_name] = {
             }}
         end;
         settings = {
-            search_path = vim.empty_dict()
+            search_path = vim.empty_dict() 
         };
     };
     docs = {
+        package_json = "https://github.com/github/vscode-codeql/blob/master/extensions/ql-vscode/package.json";
         description = [[
 Reference:
 https://help.semmle.com/codeql/codeql-cli.html
@@ -30,17 +33,19 @@ https://help.semmle.com/codeql/codeql-cli.html
 Binaries:
 https://github.com/github/codeql-cli-binaries
         ]];
-        package_json = "https://github.com/github/vscode-codeql/blob/master/extensions/ql-vscode/package.json";
+        default_config = {
+            settings = {
+                search_path = [[list containing all search paths, eg: '~/codeql-home/codeql-repo']];
+            };
+        };
     };
     on_new_config = function(config)
-        if config.settings.search_path ~= nil and config.settings.search_path ~= '' then
+        if type(config.settings.search_path) == 'table' and not vim.tbl_isempty(config.settings.search_path) then
             local search_path = "--search-path="
             for _, path in ipairs(config.settings.search_path) do
                 search_path = search_path..vim.fn.expand(path)..":"
             end
             config.cmd = {"codeql", "execute", "language-server", "--check-errors", "ON_CHANGE", "-q", search_path}
-        else
-            config.cmd = {"codeql", "execute", "language-server", "--check-errors", "ON_CHANGE", "-q"}
         end
     end;
 }
