70fb48a65e517a7fab020a91545353da9f716727
Benjamin Brittain <bwb@google.com>
2020-03-18 13:01:28 -0700

    Support rust-analyzer's rust-project.json format

--- lua/nvim_lsp/rust_analyzer.lua
+++ lua/nvim_lsp/rust_analyzer.lua
@@ -5,7 +5,7 @@ configs.rust_analyzer = {
   default_config = util.utf8_config {
     cmd = {"rust-analyzer"};
     filetypes = {"rust"};
-    root_dir = util.root_pattern("Cargo.toml");
+    root_dir = util.root_pattern("Cargo.toml", "rust-project.json");
   };
   docs = {
     package_json = "https://github.com/rust-analyzer/rust-analyzer/raw/master/editors/code/package.json";
@@ -17,7 +17,7 @@ rust-analyzer (aka rls 2.0), a language server for Rust
 See [docs](https://github.com/rust-analyzer/rust-analyzer/tree/master/docs/user#settings) for extra settings.
     ]];
     default_config = {
-      root_dir = [[root_pattern("Cargo.toml")]];
+      root_dir = [[root_pattern("Cargo.toml", "rust-project.json")]];
     };
   };
 };
