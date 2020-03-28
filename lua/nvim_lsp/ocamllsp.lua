d500455f076a542be627f376b6a3b1aaf46221ca
Georgy Komarov <jubnzv@gmail.com>
2020-03-28 16:39:10 +0300

    Add ocaml-lsp support

---
+++ lua/nvim_lsp/ocamllsp.lua
@@ -0,0 +1,28 @@
+local configs = require 'nvim_lsp/configs'
+local util = require 'nvim_lsp/util'
+
+configs.ocamllsp = {
+  default_config = {
+    cmd = {"ocamllsp",};
+    filetypes = {'ocaml', 'reason'};
+    root_dir = function(fname)
+      return util.find_git_ancestor(fname) or vim.loop.os_homedir()
+    end;
+  };
+  docs = {
+    description = [[
+https://github.com/ocaml/ocaml-lsp
+
+`ocaml-lsp` can be installed as described in [installation guide](https://github.com/ocaml/ocaml-lsp#installation).
+
+To install the lsp server in a particular opam switch:
+```sh
+opam pin add ocaml-lsp-server https://github.com/ocaml/ocaml-lsp.git
+opam install ocaml-lsp-server
+```
+    ]];
+    default_config = {
+      root_dir = [[root_pattern(".merlin", "package.json")]];
+    };
+  };
+}
