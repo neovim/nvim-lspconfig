77746ca710722414f5cc5be81015b962ce3f7084
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-04-22 16:52:33 +0200

    Show correct lua syntax for configuring rust-analyzer

--- lua/nvim_lsp/rust_analyzer.lua
+++ lua/nvim_lsp/rust_analyzer.lua
@@ -6,6 +6,9 @@ configs.rust_analyzer = {
     cmd = {"rust-analyzer"};
     filetypes = {"rust"};
     root_dir = util.root_pattern("Cargo.toml", "rust-project.json");
+    settings = {
+      ["rust-analyzer"] = {}
+    }
   };
   docs = {
     package_json = "https://raw.githubusercontent.com/rust-analyzer/rust-analyzer/master/editors/code/package.json";
