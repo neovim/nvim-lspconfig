434a34b24641f71869caeaa936232687df165818
thchha <thomas.hage@live.de>
2020-04-28 07:53:52 +0200

    replaced hardtabs with softtabs added vim-layouting hint removed noise: log-level already defined in default config; grab root_dir explicitly replaced vscode in favor of package_json

--- lua/nvim_lsp/kotlin_language_server.lua
+++ lua/nvim_lsp/kotlin_language_server.lua
@@ -9,50 +9,45 @@
 local vim = vim	-- from global table
 local util = require 'nvim_lsp/util'
 local configs = require 'nvim_lsp/configs'
---- kotlin-language-server supports multiple types of projects.
---- The default works with single- and multi-module gradle projects.
-local function get_project_root()
-  return util.root_pattern("settings.gradle")
-end
 
 configs.kotlin_language_server = {
   default_config = util.utf8_config {
-	filetypes = { "kotlin" };
-	root_dir = get_project_root();
-	log_level = vim.lsp.protocol.MessageType.Warning;
+    filetypes = { "kotlin" };
+    root_dir = util.root_pattern("settings.gradle");
   };
   docs = {
-	vscode = "0.5.2";
-	description = [[
-A kotlin language server which was developed for internal usage and
-released afterwards. Maintaining is not done by the original author,
-but by fwcd.
+    package_json = "https://raw.githubusercontent.com/fwcd/vscode-kotlin/master/package.json";
+    description = [[
+    A kotlin language server which was developed for internal usage and
+    released afterwards. Maintaining is not done by the original author,
+    but by fwcd.
 
-It is builded via gradle and developed on github.
-Source and additional description:
-	https://github.com/fwcd/kotlin-language-server
-]];
-	default_config = {
-	  root_dir = [[root_pattern("settings.gradle")]];
-	  capabilities = [[
-	  smart code completion,
-	  diagnostics,
-	  hover,
-	  document symbols,
-	  definition lookup,
-	  method signature help,
-	  dependency resolution,
-	  additional plugins from: https://github.com/fwcd
+    It is builded via gradle and developed on github.
+    Source and additional description:
+    https://github.com/fwcd/kotlin-language-server
+    ]];
+    default_config = {
+      root_dir = [[root_pattern("settings.gradle")]];
+      capabilities = [[
+      smart code completion,
+      diagnostics,
+      hover,
+      document symbols,
+      definition lookup,
+      method signature help,
+      dependency resolution,
+      additional plugins from: https://github.com/fwcd
 
-		Snipped of License (refer to source for full License):
+      Snipped of License (refer to source for full License):
 
-		The MIT License (MIT)
+      The MIT License (MIT)
 
-		Copyright (c) 2016 George Fraser
-		Copyright (c) 2018 fwcd
+      Copyright (c) 2016 George Fraser
+      Copyright (c) 2018 fwcd
 
-	  ]];
-	};
+      ]];
+    };
   };
 }
 
+-- vim:et ts=2 sw=2
