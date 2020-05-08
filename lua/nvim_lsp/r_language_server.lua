e906ed7b607851336d978e35ab754a783599b2e9
phelipe <phelipe_teles@hotmail.com>
2020-05-08 17:17:12 -0300

    Add docs and installer to R language server

--- lua/nvim_lsp/r_language_server.lua
+++ lua/nvim_lsp/r_language_server.lua
@@ -1,11 +1,5 @@
-local configs = require 'nvim_lsp/configs'
 local util = require 'nvim_lsp/util'
-
-local function installer()
-  local cmd = [[Rscript -e "install.packages('languageserver')"]]
-  local f = io.popen(cmd)
-  f:close()
-end
+local configs = require 'nvim_lsp/configs'
 
 configs.r_language_server = {
   default_config = {
@@ -16,7 +10,50 @@ configs.r_language_server = {
     end;
     log_level = vim.lsp.protocol.MessageType.Warning;
   };
-  on_new_config = function()
-    installer()
-  end;
+  docs = {
+    description = [[
+    [languageserver](https://github.com/REditorSupport/languageserver) is an
+    implementation of the Microsoft's Language Server Protocol for the R
+    language.
+
+    It is released on CRAN and can be easily installed by
+
+    ```R
+    install.packages("languageserver")
+    ```
+    ]];
+    default_config = {
+      root_dir = [[root_pattern(".git") or os_homedir]]
+    };
+  };
 }
+
+
+local install = function()
+  local f = io.popen([[Rscript -e "install.packages('languageserver')"]])
+  f:close()
+end
+
+local get_r_version = function()
+  local f = io.popen([[R --version]])
+  local version = f:read("*a"):match("[0-9.]+")
+  f:close()
+
+  return version
+end
+
+local install_info = function()
+  local f = io.popen([[Rscript -e "'languageserver' %in% installed.packages()"]])
+  local l  = f:read("*a")
+  f:close()
+
+  local status = not l:match("FALSE")
+
+  return {
+    version = get_r_version(),
+    is_installed = status,
+  }
+end
+
+configs.r_language_server.install = install
+configs.r_language_server.install_info = install_info
