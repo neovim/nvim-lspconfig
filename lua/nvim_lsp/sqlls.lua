9bf8e04825e3133bc4e2d9ea9cfe84a0595a71cd
tkolleh <tinatuh@gmail.com>
2020-08-31 18:12:24 -0400

    reference install bin path from installer

--- lua/nvim_lsp/sqlls.lua
+++ lua/nvim_lsp/sqlls.lua
@@ -14,18 +14,36 @@ local root_pattern = util.root_pattern(".sqllsrc.json")
 
 configs[server_name] = {
   default_config = {
-    cmd = {bin_name, "up", "--method", "stdio"};
     filetypes = {"sql", "mysql"};
     root_dir = function(fname)
-      return root_pattern(fname)
-              or vim.fn.getcwd()
-              or vim.loop.os_homedir()
+      return root_pattern(fname) or vim.loop.os_homedir()
     end;
   };
+  on_new_config = function(config)
+    local install_info = installer.info();
+    local P = util.path.join
+    if install_info.is_installed then
+       local bin_ex = P{install_info.bin_dir, bin_name}
+       config.cmd = {bin_ex, "up", "--method", "stdio"}
+    end
+  end;
   docs = {
     package_json = "https://raw.githubusercontent.com/joe-re/sql-language-server/release/package.json";
     description = [[
       https://github.com/joe-re/sql-language-server
+
+      `cmd` value is **not set** by default. An installer is provided via this `:LspInstall` that uses the 
+       nvm_lsp node_modules directory to find the executable. The `cmd` value can be overriden in `setup`;
+
+             
+      ```lua
+      require'nvim_lsp'.sqlls.setup{
+        cmd = {"path/to/command", "up", "--method", "stdio"};
+        ...
+      }
+      ```
+
+      Find further instructions on manual installation of the sql-language-server lsp at [joe-re/sql-language-server](https://github.com/joe-re/sql-language-server).
 
       `sql-language-server` can be installed via `:LspInstall sqlls` or by yourself with `npm`:
       ```sh
