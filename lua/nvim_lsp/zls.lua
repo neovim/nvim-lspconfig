c402bbd595d794bc4021310f4956b70c5371703e
Ogromny <ogromnycoding@gmail.com>
2020-09-24 08:57:05 +0200

    [ZLS] initial commit

---
+++ lua/nvim_lsp/zls.lua
@@ -0,0 +1,20 @@
+local configs = require 'nvim_lsp/configs'
+local util = require 'nvim_lsp/util'
+
+configs.zls = {
+    default_config = {
+        cmd = {"zls"};
+        filetypes = {"zig", "zir"};
+        root_dir = util.root_pattern("zls.json", ".git", ".");
+    };
+    docs = {
+        description = [[
+           https://github.com/zigtools/zls
+
+           `Zig LSP implementation + Zig Language Server`.
+        ]];
+        default_config = {
+            root_dir = [[util.root_pattern("zls.json", ".git") or cwd]];
+        };
+    };
+};
