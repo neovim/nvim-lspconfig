e69ef852df2bd119d1db352c7c8c6a08967790d6
Lazerbeak12345 <nfblaster@live.com>
2020-10-25 15:58:03 -0600

    Add racket support

---
+++ lua/nvim_lsp/racketls.lua
@@ -0,0 +1,23 @@
+local configs = require 'nvim_lsp/configs'
+local util = require 'nvim_lsp/util'
+
+configs.cmake = {
+  default_config = {
+    cmd = {"racket -l racket-langserver"};
+    filetypes = {"racket"};
+    root_dir = util.path.dirname;
+  };
+  docs = {
+    description = [[
+https://github.com/jeapostrophe/racket-langserver
+Racket LSP Implementation
+
+May also require https://github.com/wlangstroth/vim-racket
+]];
+    default_config = {
+      root_dir = "vim's starting directory";
+    };
+  };
+};
+
+-- vim:et ts=2 sw=2
