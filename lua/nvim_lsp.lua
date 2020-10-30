f61282c01c5da6f7d5fb15ba759d1e0b3eb4ad02
Hirokazu Hata <h.hata.ai.t@gmail.com>
2020-10-30 15:35:20 +0900

    Add notice message for loading a old lua module

---
+++ lua/nvim_lsp.lua
@@ -0,0 +1,2 @@
+local vim = vim
+vim.api.nvim_command('echomsg "NOTICE: \\"nvim_lsp\\" module was renamed to \\"lspconfig\\". Update your config/plugins to require(\\"lspconfig\\") instead of require(\\"nvim_lsp\\")."')
