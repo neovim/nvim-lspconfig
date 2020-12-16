5a9e80acdbec99704f12458f6788b0ae9208c9a3
haze <isnt@haze.cool>
2020-12-16 02:20:36 -0500

    fix zls

--- lua/nvim_lsp/zls.lua
+++ lua/nvim_lsp/zls.lua
@@ -4,11 +4,12 @@ local util = require 'nvim_lsp/util'
 configs.zls = {
     default_config = {
         cmd = {"zls"};
-        filetypes = {"zig", "zir"};
-        root_dir = function(fname)
-            return util.root_pattern("zls.json", ".git")(fname) or util.path.dirname(fname)
-        end
-    };
+        filetypes = {"zig"};
+        settings = {
+            ["zls"] = {}
+        },
+        root_dir = util.path.dirname;
+    },
     docs = {
         description = [[
            https://github.com/zigtools/zls
@@ -16,7 +17,7 @@ configs.zls = {
            `Zig LSP implementation + Zig Language Server`.
         ]];
         default_config = {
-            root_dir = [[util.root_pattern("zls.json", ".git") or current_file_dirname]];
+            root_dir = "vim's starting directory";
         };
     };
 };
