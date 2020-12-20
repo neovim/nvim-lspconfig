7066dbbb422464afb9a7b8a6ea5dce576eabb8e5
Hirokazu Hata <h.hata.ai.t@gmail.com>
2020-12-21 03:15:06 +0900

    Add deprecation message to ghcide

--- lua/lspconfig/ghcide.lua
+++ lua/lspconfig/ghcide.lua
@@ -1,5 +1,7 @@
 local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
+local vim = vim
+vim.api.nvim_command('echomsg "NOTICE: \\"ghcide\\" has been deprecated. Use \\"haskell-language-server\\" instead of \\"ghcide\\". For more information, see https://github.com/haskell/ghcide/pull/939 .')
 
 configs.ghcide = {
   default_config = {
@@ -9,16 +11,15 @@ configs.ghcide = {
   };
 
   docs = {
-    package_json = "https://raw.githubusercontent.com/digital-asset/ghcide/master/extension/package.json";
     description = [[
 https://github.com/digital-asset/ghcide
 
 A library for building Haskell IDE tooling.
+"ghcide" isn't for end users now. Use "haskell-language-server" instead of "ghcide".
 ]];
     default_config = {
       root_dir = [[root_pattern("stack.yaml", "hie-bios", "BUILD.bazel", "cabal.config", "package.yaml")]];
     };
   };
 };
-
 -- vim:et ts=2 sw=2
