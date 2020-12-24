afbc99f80b61203654aa4c22331e0fae390d682d
Abhishek Cherath <abhicherath@gmail.com>
2020-12-23 22:26:24 -0500

    added perl language server

---
+++ lua/lspconfig/perlls.lua
@@ -0,0 +1,37 @@
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+configs.perlls = {
+  default_config = {
+    cmd = {"perl",
+    "-MPerl::LanguageServer",
+    "-e", "Perl::LanguageServer::run","--",
+    "--port 13603", "--nostdio 0", "--version 2.1.0"};
+    settings = {
+      perl = {
+        perlCmd = 'perl';
+        perlInc = ' ';
+        fileFilter = {".pm",".pl"};
+        ignoreDirs =  '.git';
+      };
+    };
+    filetypes = {"perl"};
+    root_dir = function(fname)
+      return util.root_pattern(".git")(fname) or vim.loop.os_homedir()
+    end;
+  };
+  docs = {
+    package_json = "https://github.com/richterger/Perl-LanguageServer/blob/master/clients/vscode/perl/package.json";
+    description = [[
+    https://github.com/richterger/Perl-LanguageServer/tree/master/clients/vscode/perl
+
+    `Perl-LanguageServer`, a language server for Perl.
+
+    To make this work, ensure that you have Perl::LanguageServer installed and the perl command in your $PATH
+    ]];
+    default_config = {
+      root_dir = "vim's starting directory";
+    };
+  };
+};
+-- vim:et ts=2 sw=2
