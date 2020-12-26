5ded292b8c144ed22d22ab0dbbc5782b34c91263
Michael Lingelbach <m.j.lbach@gmail.com>
2020-12-25 16:11:11 -0800

    Fix html langserver completion by broadcasting snippet support

--- lua/lspconfig/html.lua
+++ lua/lspconfig/html.lua
@@ -10,12 +10,17 @@ local installer = util.npm_installer {
   binaries = {bin_name};
 }
 
+-- This is an ugly hack since the html-languageserver requires snippet support to be registered in capabilities for completion
+-- https://github.com/vscode-langservers/vscode-html-languageserver/blob/3292e4cc7d1ac40d3de9a21bca5cc0dae6f7271e/src/htmlServer.ts#L152
+local default_capabilities = vim.lsp.protocol.make_client_capabilities()
+default_capabilities.textDocument.completion.completionItem.snippetSupport = true;
 local root_pattern = util.root_pattern("package.json")
 
 configs[server_name] = {
   default_config = {
     cmd = {bin_name, "--stdio"};
     filetypes = {"html"};
+    capabilities = default_capabilities;
     root_dir = function(fname)
       return root_pattern(fname) or vim.loop.os_homedir()
     end;
