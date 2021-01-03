f60ba8558fa9ecdf423837e980ac560341a503ab
Michael Lingelbach <m.j.lbach@gmail.com>
2021-01-03 06:26:45 -0800

    Add compatibility functions with deprecation notice

--- lua/lspconfig.lua
+++ lua/lspconfig.lua
@@ -10,6 +10,35 @@ function M.available_servers()
   return vim.tbl_keys(configs)
 end
 
+function M.installable_servers()
+  print("deprecated, see https://github.com/neovim/neovim/wiki/Following-HEAD")
+end
+
+-- Called from plugin/lspconfig.vim because it requires knowing that the last
+-- script in scriptnames to be executed is lspconfig.
+function M._root._setup()
+  M._root.commands = {
+    LspInstall = {
+      function()
+        print("deprecated, see https://github.com/neovim/neovim/wiki/Following-HEAD")
+      end;
+      "-nargs=?";
+      "-complete=custom,v:lua.lsp_complete_installable_servers";
+      description = '`:LspInstall {name}` installs a server under stdpath("cache")/lspconfig/{name}';
+    };
+    LspInstallInfo = {
+      function()
+        print("deprecated, see https://github.com/neovim/neovim/wiki/Following-HEAD")
+      end;
+      "-nargs=?";
+      "-complete=custom,v:lua.lsp_complete_servers";
+      description = 'Print installation info for {name} if one is specified, or all installable servers.';
+    };
+  };
+
+  M.util.create_module_commands("_root", M._root.commands)
+end
+
 local mt = {}
 function mt:__index(k)
   if configs[k] == nil then
