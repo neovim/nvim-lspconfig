f4ad90854b849991c9d0426b72feff86a7f0f780
Michael Lingelbach <m.j.lbach@gmail.com>
2021-01-07 23:40:33 -0800

    rls: remove vscode extension link, fix documentation

--- lua/lspconfig/rls.lua
+++ lua/lspconfig/rls.lua
@@ -8,7 +8,6 @@ configs.rls = {
     root_dir = util.root_pattern("Cargo.toml");
   };
   docs = {
-    package_json = "https://raw.githubusercontent.com/rust-lang/rls-vscode/master/package.json";
     description = [[
 https://github.com/rust-lang/rls
 
@@ -16,6 +15,20 @@ rls, a language server for Rust
 
 See https://github.com/rust-lang/rls#setup to setup rls itself.
 See https://github.com/rust-lang/rls#configuration for rls-specific settings.
+All settings listed on the rls configuration section of the readme
+must be set under settings.rust as follows:
+
+```lua
+nvim_lsp.rls.setup {
+  settings = {
+    rust = {
+      unstable_features = true,
+      build_on_save = false,
+      all_features = true,
+    },
+  },
+}
+```
 
 If you want to use rls for a particular build, eg nightly, set cmd as follows:
 
