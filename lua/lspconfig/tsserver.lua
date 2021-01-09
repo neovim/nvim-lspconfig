4e7a2c32628e674348d0bf8029e1c580b05e5cc4
Michael Lingelbach <m.j.lbach@gmail.com>
2021-01-08 23:02:43 -0800

    tsserver: fix luacheck error

--- lua/lspconfig/tsserver.lua
+++ lua/lspconfig/tsserver.lua
@@ -12,7 +12,7 @@ configs[server_name] = {
     cmd = {bin_name, "--stdio"};
     filetypes = {"javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"};
     root_dir = function(fname)
-      return util.root_pattern("tsconfig.json")(fname) or 
+      return util.root_pattern("tsconfig.json")(fname) or
       util.root_pattern("package.json", "jsconfig.json", ".git")(fname);
     end
   };
