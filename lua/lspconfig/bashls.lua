651cacfeb3f92e6dc8180fcfdc339b5d43eb34b0
Peter Lithammer <peter.lithammer@gmail.com>
2021-01-20 19:20:28 +0100

    bashls: Set GLOB_PATTERN to prevent recursive scanning

--- lua/lspconfig/bashls.lua
+++ lua/lspconfig/bashls.lua
@@ -6,6 +6,13 @@ local server_name = "bashls"
 configs[server_name] = {
   default_config = {
     cmd = {"bash-language-server", "start"};
+    cmd_env = {
+      -- Prevent recursive scanning which will cause issues when opening a file
+      -- directly in the home directory (e.g. ~/foo.sh).
+      --
+      -- Default upstream pattern is "**/*@(.sh|.inc|.bash|.command)".
+      GLOB_PATTERN = vim.env.GLOB_PATTERN or "*@(.sh|.inc|.bash|.command)"
+    },
     filetypes = {"sh"};
     root_dir = util.path.dirname;
   };
