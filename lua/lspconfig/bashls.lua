95239b21accc34ce220ec071363192995925b267
Peter Lithammer <peter.lithammer@gmail.com>
2021-01-20 17:17:07 +0100

    bashls: Set root_dir to nil when editing file in ~
    A problem with bash-language-server is that it has a bad habit of trying
    to recursively scan the entire home folder if you for example tried to
    edit a shell script directly in `~` (e.g. `~/foo.sh`).
    
    This causes a bunch of weird behaviour and causes the server to take
    forever to start.
    
    To prevent this we set `root_dir` to `nil` when the resolved `root_dir`
    happened to be `~`. This solution is kind of hinted towards if you look
    at their own README for how you should configure it with coc.nvim:
    
        "languageserver": {
          "bash": {
            "command": "bash-language-server",
            "args": ["start"],
            "filetypes": ["sh"],
            "ignoredRootPaths": ["~"]
          }
        }

--- lua/lspconfig/bashls.lua
+++ lua/lspconfig/bashls.lua
@@ -1,3 +1,4 @@
+local log = require('vim/lsp/log')
 local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
 
@@ -7,7 +8,17 @@ configs[server_name] = {
   default_config = {
     cmd = {"bash-language-server", "start"};
     filetypes = {"sh"};
-    root_dir = util.path.dirname;
+    root_dir = function(fname)
+      local dirname = util.path.dirname(fname)
+
+      -- Prevent the bash-language-server from scanning the entire home folder.
+      if dirname == vim.loop.os_homedir() then
+        log.info('Not setting root_dir to prevent scanning everything in $HOME')
+        return nil
+      end
+
+      return dirname
+    end
   };
   docs = {
     description = [[
