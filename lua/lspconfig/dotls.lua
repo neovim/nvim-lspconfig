73d9349d21a2801d770175a6c58da16ed13b28ed
Michael Green <micke.green@gmail.com>
2021-03-22 22:22:52 +0100

    Added dot language server to the mix.
    Following mjlback's suggestion to have a fallback root dir.
    
    Removed redundant config example from docs.

---
+++ lua/lspconfig/dotls.lua
@@ -0,0 +1,32 @@
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+local server_name = "dotls"
+local bin_name = "dot-language-server"
+
+local root_files = {
+  '.git',
+}
+
+configs[server_name] = {
+  default_config = {
+    cmd = {bin_name, "--stdio"};
+    filetypes = {"dot"};
+    root_dir = function(filename)
+      return util.root_pattern(unpack(root_files))(filename) or
+             util.path.dirname(filename)
+    end;
+  };
+  docs = {
+    description = [[
+https://github.com/nikeee/dot-language-server
+
+`dot-language-server` can be installed via `npm`:
+```sh
+npm install -g dot-language-server
+```
+    ]];
+  };
+};
+
+-- vim:et ts=2 sw=2
