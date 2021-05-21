a6fc57b5353f0d4783cbe9710523324afb202f93
tiagovla <tiagovla@gmail.com>
2021-05-20 21:01:04 -0300

    Update: try to require config if dofile cannot find file

--- lua/lspconfig.lua
+++ lua/lspconfig.lua
@@ -89,6 +89,9 @@ function mt:__index(k)
     -- dofile is used here as a performance hack to increase the speed of calls to setup({})
     -- dofile does not cache module lookups, and requires the absolute path to the target file
     pcall(dofile, script_path .. 'lspconfig/' .. k .. ".lua")
+    if configs[k] == nil then
+      pcall(require, 'lspconfig/' .. k)
+    end
   end
   return configs[k]
 end
