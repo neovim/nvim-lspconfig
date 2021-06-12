3ff17a9154af3176ac6c29fa48c7b5170032a7c4
Ben Frain <bf@benfrain.com>
2021-06-13 00:33:01 +0100

    Update to provide updated CSS instructions
    Updated language server references based on #965
    Also added basic rules override example.
    
    Apologies if this is still not correct, unfamiliar with Lua!

--- lua/lspconfig/cssls.lua
+++ lua/lspconfig/cssls.lua
@@ -21,7 +21,77 @@ configs[server_name] = {
   };
   docs = {
     description = [[
-https://github.com/vscode-langservers/vscode-css-languageserver-bin
+
+    
+https://www.npmjs.com/package/vscode-langservers-extracted
+
+`css-languageserver` can be installed via `npm`:
+
+```sh
+npm i -g vscode-langservers-extracted
+```
+    
+Neovim does not currently include built-in snippets. vscode-css-language-server only provides completions when snippet support is enabled. To enable completion, install a snippet plugin and add the following override to your language client capabilities during setup.
+
+```lua
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
+```
+
+Here is the most basic setup and the default values that provides:
+    
+```lua
+require'lspconfig'.cssls.setup{}
+
+  Commands:
+  
+  Default Values:
+    cmd = { "vscode-css-language-server", "--stdio" }
+    filetypes = { "css", "scss", "less" }
+    root_dir = root_pattern("package.json")
+    settings = {
+      css = {
+        validate = true
+      },
+      less = {
+        validate = true
+      },
+      scss = {
+        validate = true
+      }
+    }
+```
+    
+Here is a further exmaple lua setup, where an override has been set to warn if an #id selector is used in Sass (scss) files:
+
+```lua
+require'lspconfig'.cssls.setup({
+    cmd = { "vscode-css-language-server", "--stdio" },
+    settings = {
+    scss = {
+      lint = {
+        idSelector = "warning",
+      },
+    },
+  },
+})
+```
+
+]];
+    
+  };
+}
+
+
+    
+    
+    
+    https://github.com/vscode-langservers/vscode-css-languageserver-bin
 
 `css-languageserver` can be installed via `npm`:
 ```sh
