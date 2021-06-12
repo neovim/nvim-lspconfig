6b47b49a97ddfe1acb75f5c8ef83e3c0abc6ced1
Adelar da Silva Queiróz <adelarsq@gmail.com>
2021-06-12 16:54:57 -0300

    Update removing some inconsistencies

--- lua/lspconfig/fsautocomplete.lua
+++ lua/lspconfig/fsautocomplete.lua
@@ -9,46 +9,28 @@ configs.fsautocomplete = {
       AutomaticWorkspaceInit = true;
     };
   };
-  docs = {
     description = [[
+  docs = {
 https://github.com/fsharp/FsAutoComplete
 
 Language Server for F# provided by FsAutoComplete (FSAC).
 
 FsAutoComplete requires the [dotnet-sdk](https://dotnet.microsoft.com/download) to be installed.
 
-With dotnet-sdk installed FsAutoComplete can be installed with `dotnet tool install --global fsautocomplete`.
+The prefered way to install FsAutoComplete is with `dotnet tool install --global fsautocomplete`.
 
-Also it's possible build from source from [here](https://github.com/fsharp/FsAutoComplete/releases).
-Instructions to compile from source are found on the main repository.
+Instructions to compile from source are found on the main [repository](https://github.com/fsharp/FsAutoComplete).
 
 You may also need to configure the filetype as Vim defaults to Forth for `*.fs` files:
 
 `autocmd BufNewFile,BufRead *.fs,*.fsx,*.fsi set filetype=fsharp`
 
-This is automatically done by plugins such as [vim-polyglot](https://github.com/sheerun/vim-polyglot), [PhilT/vim-fsharp](https://github.com/PhilT/vim-fsharp), [fsharp/vim-fsharp](https://github.com/fsharp/vim-fsharp) or [adelarsq/neofsharp.vim](https://github.com/adelarsq/neofsharp.vim).
+This is automatically done by plugins such as [PhilT/vim-fsharp](https://github.com/PhilT/vim-fsharp), [fsharp/vim-fsharp](https://github.com/fsharp/vim-fsharp) or [adelarsq/neofsharp.vim](https://github.com/adelarsq/neofsharp.vim).
 
 ```lua
 require'lspconfig'.fsautocomplete.setup{
   cmd = {'dotnet', 'fsautocomplete', '--background-service-enabled'}
-  -- Or for custom build:
-  -- cmd = {'dotnet', 'path/to/fsautocomplete.dll', '--background-service-enabled'}
 }
-```
-
-```lua
-require'lspconfig'.fsautocomplete.setup{}
-
-  Commands:
-  
-  Default Values:
-    filetypes = { "fsharp" }
-    init_options = {
-      AutomaticWorkspaceInit = true
-    }
-    root_dir = function(startpath)
-        return M.search_ancestors(startpath, matcher)
-      end
 ```
     ]];
   };
