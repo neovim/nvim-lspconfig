06fff166854e740895bea11052f084388848ba09
Daniel M. Capella <polyzen@archlinux.org>
2021-06-14 03:30:07 -0400

    tailwindcss: Add installation instructions

--- lua/lspconfig/tailwindcss.lua
+++ lua/lspconfig/tailwindcss.lua
@@ -79,6 +79,22 @@ configs[server_name] = {
 https://github.com/tailwindlabs/tailwindcss-intellisense
 
 Tailwind CSS Language Server
+
+**NOTE:** The current tailwindcss-language-server npm package is a different project.
+
+Until the standalone server is published to npm, you can extract the server from the VS Code package:
+
+```bash
+curl -L -o tailwindcss-intellisense.vsix $(curl -s https://api.github.com/repos/tailwindlabs/tailwindcss-intellisense/releases/latest | grep 'browser_' | cut -d\" -f4)
+unzip tailwindcss-intellisense.vsix -d tailwindcss-intellisense
+echo "#\!/usr/bin/env node\n$(cat tailwindcss-intellisense/extension/dist/server/tailwindServer.js)" > tailwindcss-language-server
+chmod +x tailwindcss-language-server
+```
+
+Copy or symlink tailwindcss-language-server to somewhere in your $PATH.
+
+Alternatively, it might be packaged for your operating system, eg.:
+https://aur.archlinux.org/packages/tailwindcss-language-server/
 ]],
     default_config = {
       root_dir = [[root_pattern('tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js', 'postcss.config.ts', 'package.json', 'node_modules', '.git')]],
