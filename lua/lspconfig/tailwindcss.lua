8cf830d3d13365e4711993c4915d314d71fe5fbd
Karim Abou Zeid <karim23697@gmail.com>
2021-06-15 21:48:45 +0200

    improve(tailwindcss): missing validation settings, provide tab size for hover

--- lua/lspconfig/tailwindcss.lua
+++ lua/lspconfig/tailwindcss.lua
@@ -65,6 +65,28 @@ configs[server_name] = {
         eruby = 'erb',
       },
     },
+    settings = {
+      tailwindCSS = {
+        validate = true,
+        lint = {
+          cssConflict = "warning",
+          invalidApply = "error",
+          invalidScreen = "error",
+          invalidVariant = "error",
+          invalidConfigPath = "error",
+          invalidTailwindDirective = "error",
+          recommendedVariantOrder = "warning",
+        },
+      },
+    },
+    on_new_config = function(new_config)
+      if not new_config.settings then new_config.settings = {} end
+      if not new_config.settings.editor then new_config.settings.editor = {} end
+      if not new_config.settings.editor.tabSize then
+        -- set tab size for hover
+        new_config.settings.editor.tabSize = vim.lsp.util.get_effective_tabstop()
+      end
+    end,
     root_dir = function(fname)
       return util.root_pattern('tailwind.config.js', 'tailwind.config.ts')(fname) or
       util.root_pattern('postcss.config.js', 'postcss.config.ts')(fname) or
