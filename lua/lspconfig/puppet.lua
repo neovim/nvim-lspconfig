0d65c5f7c9cf5bedc2da4ae5fe5a49f8a4c42846
Lucas Crownover <lcrown@uoregon.edu>
2021-07-02 14:51:56 -0700

    adjusting root_pattern for common puppet-things

--- lua/lspconfig/puppet.lua
+++ lua/lspconfig/puppet.lua
@@ -8,7 +8,7 @@ configs[server_name] = {
   default_config = {
     cmd = {bin_name, "--stdio"};
     filetypes = {"puppet"};
-    root_dir = util.root_pattern("manifests", ".git", ".puppet-lint.rc");
+    root_dir = util.root_pattern("manifests", ".puppet-lint.rc", "hiera.yaml", ".git");
   };
   -- on_new_config = function(new_config) end;
   -- on_attach = function(client, bufnr) end;
@@ -19,7 +19,7 @@ https://github.com/puppetlabs/puppet-editor-services
 LSP server for Puppet.
 ]];
     default_config = {
-      root_dir = [[root_pattern("manifests", ".git")]];
+      root_dir = [[root_pattern("manifests", ".puppet-lint.rc", "hiera.yaml", ".git")]];
     };
   };
 }
