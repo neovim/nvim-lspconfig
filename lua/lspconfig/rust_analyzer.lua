e959244071dca8ec4ce488a7055d72d185471f15
Damien DeVille <damien@ddeville.me>
2021-07-03 13:46:15 -0700

    fix(rust-analyzer): improve root directory detection

--- lua/lspconfig/rust_analyzer.lua
+++ lua/lspconfig/rust_analyzer.lua
@@ -16,15 +16,20 @@ configs.rust_analyzer = {
     cmd = {"rust-analyzer"};
     filetypes = {"rust"};
     root_dir = function(fname)
-      local cargo_metadata = vim.fn.system("cargo metadata --no-deps --format-version 1")
-      local cargo_root = nil
-      if vim.v.shell_error == 0 then
-        cargo_root = vim.fn.json_decode(cargo_metadata)["workspace_root"]
+      local cargo_crate_dir = util.root_pattern("Cargo.toml")(fname)
+      local cmd = "cargo metadata --no-deps --format-version 1"
+      if cargo_crate_dir ~= nil then
+        cmd = cmd .. " --manifest-path " .. util.path.join(cargo_crate_dir, "Cargo.toml")
       end
-      return cargo_root or
-        util.find_git_ancestor(fname) or
+      local cargo_metadata = vim.fn.system(cmd)
+      local cargo_workspace_dir = nil
+      if vim.v.shell_error == 0 then
+        cargo_workspace_dir = vim.fn.json_decode(cargo_metadata)["workspace_root"]
+      end
+      return cargo_workspace_dir or
+        cargo_crate_dir or
         util.root_pattern("rust-project.json")(fname) or
-        util.root_pattern("Cargo.toml")(fname)
+        util.find_git_ancestor(fname)
     end;
     settings = {
       ["rust-analyzer"] = {}
