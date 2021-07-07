0340e6c7ff0b41e6e49867e0262800803f7789b4
Scott Little <50145141+CodeLongAndProsper90@users.noreply.github.com>
2021-07-07 16:58:47 -0500

    Add serve-d support

---
+++ lua/lspconfig/dls.lua
@@ -0,0 +1,19 @@
+local configs = require "lspconfig/configs"
+local util = require "lspconfig/util"
+
+configs.serve-d = {
+  default_config = {
+    cmd = { "serve-d" },
+    filetypes = { "d" },
+    root_dir = util.root_pattern("dub.json", "dub.sdl", ".git")
+  },
+  docs = {
+    description = [[
+        https://github.com/Pure-D/serve-d
+        Microsoft language server protocol implementation for D using workspace-d.
+        ]],
+    default_config = {
+      root_dir = [[util.root_pattern("dub.json","dub.sdl", ".git"]],
+    },
+  },
+}
