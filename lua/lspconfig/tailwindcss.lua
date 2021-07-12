0036590f87ca883f52719dec18afdd373ebe8494
Michael Lingelbach <m.j.lbach@gmail.com>
2021-07-12 16:43:06 -0700

    chore: format with stylua (#1065)

--- lua/lspconfig/tailwindcss.lua
+++ lua/lspconfig/tailwindcss.lua
@@ -92,12 +92,11 @@ configs[server_name] = {
       end
     end,
     root_dir = function(fname)
-      return util.root_pattern("tailwind.config.js", "tailwind.config.ts")(fname) or util.root_pattern(
-        "postcss.config.js",
-        "postcss.config.ts"
-      )(fname) or util.find_package_json_ancestor(fname) or util.find_node_modules_ancestor(fname) or util.find_git_ancestor(
-        fname
-      )
+      return util.root_pattern("tailwind.config.js", "tailwind.config.ts")(fname)
+        or util.root_pattern("postcss.config.js", "postcss.config.ts")(fname)
+        or util.find_package_json_ancestor(fname)
+        or util.find_node_modules_ancestor(fname)
+        or util.find_git_ancestor(fname)
     end,
   },
   docs = {
