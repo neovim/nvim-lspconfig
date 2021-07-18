37d350fda1d75605854cb840fb7587684426c842
crypto <dhiadah@gmail.com>
2021-07-18 06:11:46 +0100

    added pascal lsp

---
+++ lua/lspconfig/pasls.lua
@@ -0,0 +1,22 @@
+local configs = require 'lspconfig.configs'
+local util = require 'lspconfig.util'
+
+configs.pasls = {
+	default_config = {
+		cmd = { 'paslsp' },
+		cmd_env = {
+			PP = 'fpc',
+			FPCDIR = '/usr/lib/fpc/src',
+			LAZARUSDIR =  '',
+			FPCTARGET = '',
+			FPCTARGETCPU = ''
+		},
+		filetypes = { 'pascal' },
+		root_dir = util.path.dirname,
+	},
+	docs = [[
+	https://github.com/arjanadriaanse/pascal-language-server
+
+	An LSP server implementation for Pascal variants that are supported by Free Pascal, including Object Pascal. It uses CodeTools from Lazarus as backend.
+	]],
+}
