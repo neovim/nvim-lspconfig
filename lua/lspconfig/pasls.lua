c3ee8dca8c619f3435f18dbc2758579af9c4fcbb
crypto <dhiadah@gmail.com>
2021-07-18 23:18:12 +0100

    fixed style.

--- lua/lspconfig/pasls.lua
+++ lua/lspconfig/pasls.lua
@@ -2,22 +2,22 @@ local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
 
 configs.pasls = {
-	default_config = {
-		cmd = { 'pasls' },
-		cmd_env = {
-			PP = '/usr/lib/fpc/3.2.2/ppcx64',
-			FPCDIR = '/usr/lib/fpc/src',
-			LAZARUSDIR =  '/usr/lib/lazarus',
-			FPCTARGET = '',
-			FPCTARGETCPU = 'x86_64',
-		},
-		filetypes = { 'pascal' },
-		root_dir = util.path.dirname,
-	},
-	docs = {
-		description=[[
+  default_config = {
+    cmd = { 'pasls' },
+    cmd_env = {
+      PP = '/usr/lib/fpc/3.2.2/ppcx64',
+      FPCDIR = '/usr/lib/fpc/src',
+      LAZARUSDIR = '/usr/lib/lazarus',
+      FPCTARGET = '',
+      FPCTARGETCPU = 'x86_64',
+    },
+    filetypes = { 'pascal' },
+    root_dir = util.path.dirname,
+  },
+  docs = {
+    description = [[
 		https://github.com/genericptr/pascal-language-server
 		An LSP server implementation for Pascal variants that are supported by Free Pascal, including Object Pascal. It uses CodeTools from Lazarus as backend.
 		]],
-	},
+  },
 }
