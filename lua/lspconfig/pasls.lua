81fd8c93523b204f7b1b6d73653b74c33dd91e4b
crypto <dhiadah@gmail.com>
2021-07-22 15:50:44 +0100

    fixed style

--- lua/lspconfig/pasls.lua
+++ lua/lspconfig/pasls.lua
@@ -16,27 +16,35 @@ configs.pasls = {
   },
   docs = {
     description = [[
-		https://github.com/genericptr/pascal-language-server
-		An LSP server implementation for Pascal variants that are supported by Free Pascal, including Object Pascal. It uses CodeTools from Lazarus as backend.
-		First set `cmd` to the Pascal lsp binary.
-		Customization options are passed to pasls as environment variables for example:
-		```lua
-		require'lspconfig'.pasls.setup {
-		cmd = { 'pasls' },
-		cmd_env = {
-		FPCDIR = '/usr/lib/fpc/src',
-		PP = '/usr/lib/fpc/3.2.2/ppcx64',
-		LAZARUSDIR = '/usr/lib/lazarus',
-		FPCTARGET = '',
-		FPCTARGETCPU = 'x86_64',
-		},
-		}
-		```
-		`FPCDIR` : set this to your FPC source location.
-		`PP` : Path to the Free Pascal compiler executable.
-		`LAZARUSDIR` : Path to the Lazarus sources.
-		`FPCTARGET` : Target operating system for cross compiling.
-		`FPCTARGETCPU` : Target CPU for cross compiling.
-		]],
+https://github.com/genericptr/pascal-language-server
+
+An LSP server implementation for Pascal variants that are supported by Free Pascal, including Object Pascal. It uses CodeTools from Lazarus as backend.
+
+First set `cmd` to the Pascal lsp binary.
+
+Customization options are passed to pasls as environment variables for example:
+
+```lua
+require'lspconfig'.pasls.setup {
+cmd = { 'pasls' },
+cmd_env = {
+FPCDIR = '/usr/lib/fpc/src',
+PP = '/usr/lib/fpc/3.2.2/ppcx64',
+LAZARUSDIR = '/usr/lib/lazarus',
+FPCTARGET = '',
+FPCTARGETCPU = 'x86_64',
+},
+}
+```
+`FPCDIR` : set this to your FPC source location.
+
+`PP` : Path to the Free Pascal compiler executable.
+
+`LAZARUSDIR` : Path to the Lazarus sources.
+
+`FPCTARGET` : Target operating system for cross compiling.
+
+`FPCTARGETCPU` : Target CPU for cross compiling.
+]],
   },
 }
