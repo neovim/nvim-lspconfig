be328181a0b828eb4cbe27e940daf91416b28c8a
Jared Johnson <jaredj@gmx.com>
2021-08-14 14:29:02 -0400

    Update diagnosticls config to support Windows

--- lua/lspconfig/diagnosticls.lua
+++ lua/lspconfig/diagnosticls.lua
@@ -3,6 +3,9 @@ local util = require 'lspconfig/util'
 
 local server_name = 'diagnosticls'
 local bin_name = 'diagnostic-languageserver'
+if vim.fn.has 'win32' == 1 then
+  bin_name = bin_name .. '.cmd'
+end
 
 configs[server_name] = {
   default_config = {
