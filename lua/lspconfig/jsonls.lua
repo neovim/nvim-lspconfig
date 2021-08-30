38b3f63b2e1ddf4e61718973f36bf047d60bf03f
Jens Claes <jensclaes33@gmail.com>
2021-08-30 11:07:08 +0200

    Use extracted vscode-json-languageserver

--- lua/lspconfig/jsonls.lua
+++ lua/lspconfig/jsonls.lua
@@ -2,7 +2,7 @@ local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
 
 local server_name = 'jsonls'
-local bin_name = 'vscode-json-language-server'
+local bin_name = 'vscode-json-languageserver'
 
 configs[server_name] = {
   default_config = {
@@ -16,19 +16,18 @@ configs[server_name] = {
     end,
   },
   docs = {
-    -- this language server config is in VSCode built-in package.json
-    package_json = 'https://raw.githubusercontent.com/microsoft/vscode/master/extensions/json-language-features/package.json',
+    package_json = 'https://github.com/vscode-langservers/vscode-json-languageserver/blob/master/package.json',
     description = [[
-https://github.com/hrsh7th/vscode-langservers-extracted
+https://github.com/vscode-langservers/vscode-json-languageserver
 
 vscode-json-language-server, a language server for JSON and JSON schema
 
-`vscode-json-language-server` can be installed via `npm`:
+`vscode-json-languageserver` can be installed via `npm`:
 ```sh
-npm i -g vscode-langservers-extracted
+npm i -g vscode-json-languageservice
 ```
 
-vscode-json-language-server only provides range formatting. You can map a command that applies range formatting to the entire document:
+vscode-json-languageserver only provides range formatting. You can map a command that applies range formatting to the entire document:
 
 ```lua
 require'lspconfig'.jsonls.setup {
@@ -42,7 +41,7 @@ require'lspconfig'.jsonls.setup {
 }
 ```
 
-Neovim does not currently include built-in snippets. `vscode-json-language-server` only provides completions when snippet support is enabled. To enable completion, install a snippet plugin and add the following override to your language client capabilities during setup.
+Neovim does not currently include built-in snippets. `vscode-json-languageserver` only provides completions when snippet support is enabled. To enable completion, install a snippet plugin and add the following override to your language client capabilities during setup.
 
 ```lua
 --Enable (broadcasting) snippet capability for completion
