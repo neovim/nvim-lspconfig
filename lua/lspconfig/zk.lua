4bf384fdde12c065223bced75d95044e58b669c4
wurosh <uros.m.perisic@gmail.com>
2021-09-11 12:36:44 +0800

    Add compat layer

--- lua/lspconfig/zk.lua
+++ lua/lspconfig/zk.lua
@@ -28,16 +28,21 @@ configs.zk.index = function()
 end
 
 configs.zk.new = function(...)
-  vim.lsp.buf_request(0, 'workspace/executeCommand', {
-    command = 'zk.new',
-    arguments = {
-      vim.api.nvim_buf_get_name(0),
-      ...,
+  vim.lsp.buf_request(
+    0,
+    'workspace/executeCommand',
+    {
+      command = 'zk.new',
+      arguments = {
+        vim.api.nvim_buf_get_name(0),
+        ...,
+      },
     },
-  }, function(_, _, result)
-    if not (result and result.path) then
-      return
-    end
-    vim.cmd('edit ' .. result.path)
-  end)
+    util.compat_handler(function(_, result, _, _)
+      if not (result and result.path) then
+        return
+      end
+      vim.cmd('edit ' .. result.path)
+    end)
+  )
 end
