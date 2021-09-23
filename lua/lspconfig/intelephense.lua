861e8c42ef3040bf6812bde48dd65f7b03302bde
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-09-23 23:23:50 +0200

    fixup!: update link

--- lua/lspconfig/intelephense.lua
+++ lua/lspconfig/intelephense.lua
@@ -33,7 +33,7 @@ npm install -g intelephense
         globalStoragePath = Optional absolute path to a global storage dir. Defaults to os.homedir().
         licenceKey = Optional licence key or absolute path to a text file containing the licence key.
         clearCache = Optional flag to clear server state. State can also be cleared by deleting {storagePath}/intelephense
-        -- See https://github.com/bmewburn/intelephense-docs
+        -- See https://github.com/bmewburn/intelephense-docs/blob/master/installation.md#initialisation-options
       }]],
       settings = [[{
         intelephense = {
