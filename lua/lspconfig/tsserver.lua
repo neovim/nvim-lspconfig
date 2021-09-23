52251a1a047fcacbb740870764699d90f1f42306
Peter Lithammer <peter.lithammer@gmail.com>
2021-09-23 11:09:59 +0200

    style: format files with stylua v0.11.0

--- lua/lspconfig/tsserver.lua
+++ lua/lspconfig/tsserver.lua
@@ -11,7 +11,14 @@ configs[server_name] = {
   default_config = {
     init_options = { hostInfo = 'neovim' },
     cmd = { bin_name, '--stdio' },
-    filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'javascript.jsx',
+      'typescript',
+      'typescriptreact',
+      'typescript.tsx',
+    },
     root_dir = function(fname)
       return util.root_pattern 'tsconfig.json'(fname)
         or util.root_pattern('package.json', 'jsconfig.json', '.git')(fname)
