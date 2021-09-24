90f6697d7beff36d1445c8cbcb675e06055c5eb3
Fabian <copy@copy.sh>
2021-09-24 16:11:44 -0500

    Add fstar support

---
+++ lua/lspconfig/fstar.lua
@@ -0,0 +1,21 @@
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+configs.fstar = {
+  default_config = {
+    cmd = { 'fstar.exe', '--lsp' },
+    filetypes = { 'fstar' },
+    root_dir = util.root_pattern '.git',
+  },
+  docs = {
+    description = [[
+https://github.com/FStarLang/FStar
+
+LSP support is included in FStar. Make sure `fstar.exe` is in your PATH.
+```
+    ]],
+    default_config = {
+      root_dir = [[root_pattern(".git")]],
+    },
+  },
+}
