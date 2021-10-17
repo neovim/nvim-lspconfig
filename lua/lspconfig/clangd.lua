5fb3537c17f6a995b04049a9288fad3bff487e38
igorlfs <igorlfs@ufmg.br>
2021-10-16 22:43:13 -0300

    Update ccls/clangd configurations to offer Bear as an alternative to generate compile_commands.json
    fixup

--- lua/lspconfig/clangd.lua
+++ lua/lspconfig/clangd.lua
@@ -68,7 +68,7 @@ https://clangd.llvm.org/installation.html
 
 clangd relies on a [JSON compilation database](https://clang.llvm.org/docs/JSONCompilationDatabase.html) specified
 as compile_commands.json or, for simpler projects, a compile_flags.txt.
-For details on how to automatically generate one using CMake look [here](https://cmake.org/cmake/help/latest/variable/CMAKE_EXPORT_COMPILE_COMMANDS.html).
+For details on how to automatically generate one using CMake look [here](https://cmake.org/cmake/help/latest/variable/CMAKE_EXPORT_COMPILE_COMMANDS.html). Alternatively, you can use [Bear](https://github.com/rizsotto/Bear).
 ]],
     default_config = {
       root_dir = [[root_pattern("compile_commands.json", "compile_flags.txt", ".git") or dirname]],
