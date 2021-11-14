7a2e105e5f58c8346a7bc7c08b03723067a15109
Ultra-Hax <68920205+Ultra-Hax@users.noreply.github.com>
2021-11-14 12:09:30 -0500

    feat(clojure_lsp): add support for boot in clojure_lsp (#1416)
    Co-authored-by: Michael Lingelbach <m.j.lbach@gmail.com>

--- lua/lspconfig/clojure_lsp.lua
+++ lua/lspconfig/clojure_lsp.lua
@@ -5,7 +5,7 @@ configs.clojure_lsp = {
   default_config = {
     cmd = { 'clojure-lsp' },
     filetypes = { 'clojure', 'edn' },
-    root_dir = util.root_pattern('project.clj', 'deps.edn', '.git'),
+    root_dir = util.root_pattern('project.clj', 'deps.edn', 'build.boot', '.git'),
   },
   docs = {
     description = [[
@@ -14,7 +14,7 @@ https://github.com/snoe/clojure-lsp
 Clojure Language Server
 ]],
     default_config = {
-      root_dir = [[root_pattern("project.clj", "deps.edn", ".git")]],
+      root_dir = [[root_pattern("project.clj", "deps.edn", "build.boot", ".git")]],
     },
   },
 }
