19efe4a87423ee818a5543cc815bc50d081a0321
Michael Lingelbach <m.j.lbach@gmail.com>
2021-11-14 09:16:05 -0800

    Update bsl_ls.lua

--- lua/lspconfig/bsl_ls.lua
+++ lua/lspconfig/bsl_ls.lua
@@ -11,7 +11,7 @@ configs[name] = {
   docs = {
     description = [[
     https://github.com/1c-syntax/bsl-language-server
-    
+
     Language Server Protocol implementation for 1C (BSL) - 1C:Enterprise 8 and OneScript languages.
 
     ]],
