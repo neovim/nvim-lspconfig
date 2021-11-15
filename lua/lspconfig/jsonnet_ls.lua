c7bf7d4bf9065c6f01edb325c6e8dcfa732ebb3d
Zach Leslie <zach.leslie@grafana.com>
2021-11-15 02:11:52 -0700

    feat: add jsonnet-language-server (#1372)

---
+++ lua/lspconfig/jsonnet_ls.lua
@@ -0,0 +1,22 @@
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+configs.jsonnet_ls = {
+  default_config = {
+    cmd = { 'jsonnet-language-server' },
+    filetypes = { 'jsonnet', 'libsonnet' },
+    root_dir = function(fname)
+      return util.root_pattern 'jsonnetfile.json'(fname) or util.find_git_ancestor(fname)
+    end,
+  },
+  docs = {
+    description = [[
+https://github.com/jdbaldry/jsonnet-language-server
+
+A Language Server Protocol (LSP) server for Jsonnet.
+]],
+    default_config = {
+      root_dir = [[root_pattern("jsonnetfile.json")]],
+    },
+  },
+}
