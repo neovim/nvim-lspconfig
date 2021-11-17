a0cd45e7ca7d9693509554572f742f44041a53c3
Christian Haudum <christian.haudum@gmail.com>
2021-11-17 20:52:36 +0100

    feat(jsonnet): add JSONNET_PATH environement variable
    <CWD>/lib and <CWD>/vendor are common jsonnet library paths.
    The jsonnet language server can use the JSONNET_PATH env variable to
    resolve referenced files within these folders.
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- lua/lspconfig/jsonnet_ls.lua
+++ lua/lspconfig/jsonnet_ls.lua
@@ -1,9 +1,19 @@
 local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
 
+-- common jsonnet library paths
+local function jsonnet_path()
+  local paths = {
+    vim.fn.getcwd() .. '/lib',
+    vim.fn.getcwd() .. '/vendor',
+  }
+  return table.concat(paths, ':')
+end
+
 configs.jsonnet_ls = {
   default_config = {
     cmd = { 'jsonnet-language-server' },
+    cmd_env = { JSONNET_PATH = jsonnet_path() };
     filetypes = { 'jsonnet', 'libsonnet' },
     root_dir = function(fname)
       return util.root_pattern 'jsonnetfile.json'(fname) or util.find_git_ancestor(fname)
