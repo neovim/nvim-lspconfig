94409c5993bfc58ae7ad450bceab65d6438118f6
Christian Haudum <christian.haudum@gmail.com>
2021-11-18 21:51:49 +0100

    fixup! fixup! fixup! fixup! feat(jsonnet): add JSONNET_PATH environement variable

--- lua/lspconfig/jsonnet_ls.lua
+++ lua/lspconfig/jsonnet_ls.lua
@@ -10,18 +10,20 @@ local function jsonnet_path(root_dir)
   return table.concat(paths, ':')
 end
 
+local function get_root_dir(fname)
+  return util.root_pattern('jsonnetfile.json')(fname) or util.find_git_ancestor(fname)
+end
+
 configs.jsonnet_ls = {
   default_config = {
     cmd = { 'jsonnet-language-server' },
     cmd_env = { JSONNET_PATH = vim.fn.getcwd() }, -- will be overwritten by `on_new_config`
     filetypes = { 'jsonnet', 'libsonnet' },
-    root_dir = vim.fn.getcwd(), -- will be overwritten by `on_new_config`
+    root_dir = get_root_dir,
     on_new_config = function(new_config, file_dir)
-      local root_dir = util.root_pattern('jsonnetfile.json')(file_dir) or util.find_git_ancestor(file_dir)
       new_config.cmd_env = {
-        JSONNET_PATH = jsonnet_path(root_dir)
+        JSONNET_PATH = jsonnet_path(get_root_dir(file_dir))
       }
-      new_config.root_dir = root_dir
     end
   },
   docs = {
