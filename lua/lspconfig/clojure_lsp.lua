1f6c4e939a7608a1337b24b069dd6f5fcfcd5fe4
Loreo13 <94546272+Loreo13@users.noreply.github.com>
2021-11-19 08:10:36 -0500

    feat(clojure_lsp) add support for shadow-cljs in clojure_lsp
    Shadow-cljs is a build tool for cljs with many features such as npm integration and live reloading.
    Link: https://github.com/thheller/shadow-cljs

--- lua/lspconfig/clojure_lsp.lua
+++ lua/lspconfig/clojure_lsp.lua
@@ -5,7 +5,7 @@ configs.clojure_lsp = {
   default_config = {
     cmd = { 'clojure-lsp' },
     filetypes = { 'clojure', 'edn' },
-    root_dir = util.root_pattern('project.clj', 'deps.edn', 'build.boot', '.git'),
+    root_dir = util.root_pattern('project.clj', 'deps.edn', 'build.boot', 'shadow-cljs.edn', '.git'),
   },
   docs = {
     description = [[
@@ -14,7 +14,8 @@ https://github.com/snoe/clojure-lsp
 Clojure Language Server
 ]],
     default_config = {
-      root_dir = [[root_pattern("project.clj", "deps.edn", "build.boot", ".git")]],
+      root_dir = [[root_pattern("project.clj", "deps.edn", "build.boot", "shadow-cljs.edn", ".git")]],
     },
   },
 }
+"
