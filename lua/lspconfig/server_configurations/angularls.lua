c86ad814ab43ad324bf7fc6174f0bc638673a5a5
Michael Lingelbach <m.j.lbach@gmail.com>
2021-11-20 10:55:46 -0500

    feat(angularls): add support for legacy projects

--- lua/lspconfig/server_configurations/angularls.lua
+++ lua/lspconfig/server_configurations/angularls.lua
@@ -1,23 +1,9 @@
 local util = require 'lspconfig.util'
-
--- Angular requires a node_modules directory to probe for @angular/language-service and typescript
--- in order to use your projects configured versions.
--- This defaults to the vim cwd, but will get overwritten by the resolved root of the file.
-local function get_probe_dir(root_dir)
-  local project_root = util.find_node_modules_ancestor(root_dir)
-
-  return project_root and (project_root .. '/node_modules') or ''
-end
-
-local default_probe_dir = get_probe_dir(vim.fn.getcwd())
+local uv = vim.loop
 
 local bin_name = 'ngserver'
 local args = {
   '--stdio',
-  '--tsProbeLocations',
-  default_probe_dir,
-  '--ngProbeLocations',
-  default_probe_dir,
 }
 
 local cmd = { bin_name, unpack(args) }
@@ -36,17 +22,41 @@ return {
     root_dir = util.root_pattern('angular.json', '.git'),
   },
   on_new_config = function(new_config, new_root_dir)
-    local new_probe_dir = get_probe_dir(new_root_dir)
+    -- Angular requires a node_modules directory to probe for @angular/language-service
+    -- and typescript in order to use your projects configured versions.
+    local node_root = util.find_node_modules_ancestor(new_root_dir)
 
-    -- We need to check our probe directories because they may have changed.
+    if not node_root then
+      return
+    end
+
+    local node_modules_path = util.path.join(node_root, 'node_modules')
+
     new_config.cmd = {
       'ngserver',
       '--stdio',
       '--tsProbeLocations',
-      new_probe_dir,
+      node_modules_path,
       '--ngProbeLocations',
-      new_probe_dir,
+      node_modules_path,
     }
+
+    local angular_package_json = util.path.join(node_modules_path, '@angular', 'core', 'package.json')
+
+    -- Checking if we should provide legacy support
+    local fd = uv.fs_open(angular_package_json, 'r', 438)
+    if not fd then
+      return
+    end
+
+    local stat = uv.fs_fstat(fd)
+    local data = uv.fs_read(fd, stat.size, 0)
+    uv.fs_close(fd)
+    local decoded_json = vim.fn.json_decode(data)
+    local version = vim.split(decoded_json.version, '.', true)
+    if tonumber(version[1]) < 9 then
+      table.insert(new_config.cmd, '--viewEngine')
+    end
   end,
   docs = {
     description = [[
