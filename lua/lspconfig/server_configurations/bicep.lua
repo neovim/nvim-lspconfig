bc01b262bebb3aa81dcc83258aeb669d10c6b859
Johan Håkansson <jhakanzon@gmail.com>
2021-11-25 15:09:41 +0100

    docs(bicep): document required filetype autocommand

--- lua/lspconfig/server_configurations/bicep.lua
+++ lua/lspconfig/server_configurations/bicep.lua
@@ -7,7 +7,7 @@ return {
     init_options = {},
   },
   docs = {
-    description = [[
+    description = [=[
 https://github.com/azure/bicep
 Bicep language server
 
@@ -15,7 +15,14 @@ Bicep language server can be installed by downloading and extracting a release o
 
 Bicep language server requires the [dotnet-sdk](https://dotnet.microsoft.com/download) to be installed.
 
-**By default, bicep language server doesn't have a `cmd` set.** This is because nvim-lspconfig does not make assumptions about your path. You must add the following to your init.vim or init.lua to set `cmd` to the absolute path ($HOME and ~ are not expanded) of the unzipped run script or binary.
+Neovim does not have built-in support for the bicep filetype which is required for lspconfig to automatically launch the language server.
+
+Filetype detection can be added via an autocmd:
+```lua
+vim.cmd [[ autocmd BufNewFile,BufRead *.bicep set filetype=bicep ]]
+```
+
+**By default, bicep language server does not have a `cmd` set.** This is because nvim-lspconfig does not make assumptions about your path. You must add the following to your init.vim or init.lua to set `cmd` to the absolute path ($HOME and ~ are not expanded) of the unzipped run script or binary.
 
 ```lua
 local bicep_lsp_bin = "/path/to/bicep-langserver/Bicep.LangServer.dll"
@@ -32,7 +39,7 @@ To download the latest release and place in /usr/local/bin/bicep-langserver:
     && rm -rf /usr/local/bin/bicep-langserver \
     && unzip -d /usr/local/bin/bicep-langserver bicep-langserver.zip)
 ```
-]],
+]=],
     default_config = {
       root_dir = [[util.find_git_ancestor]],
     },
