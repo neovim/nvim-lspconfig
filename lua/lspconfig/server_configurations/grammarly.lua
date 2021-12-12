5669ee8dd4982003b65fa92795e2bb18e909b302
Matthew Toohey <contact@mtoohey.com>
2021-12-12 18:06:10 -0500

    docs(grammarly): remove first/second person language

--- lua/lspconfig/server_configurations/grammarly.lua
+++ lua/lspconfig/server_configurations/grammarly.lua
@@ -11,23 +11,23 @@ Grammarly in an LSP, using the Grammarly API.
 
 Requires some manual installation:
 
-1. Clone the Grammarly repo, the upstream is [here](https://github.com/znck/grammarly), it's designed to work with a custom client though, so it requires some modifications. I'd recommend using [this branch of my fork](https://github.com/mtoohey31/grammarly/tree/generic-client-compat) might work for you.
+1. Clone the Grammarly repo, the upstream is [here](https://github.com/znck/grammarly), it's designed to work with a custom client though, so it requires some modifications. Using [this fork's `generic-client-compat` branch](https://github.com/mtoohey31/grammarly/tree/generic-client-compat) is recommended.
 2. Build the server:
 
 ```bash
 pnpm install && pnpm run build:packages && pnpm run build:server
 ```
 
-(you can install pnpm with `npm install -g pnpm`)
+(pnpm can be installed with `npm install -g pnpm`)
 
-3. Configure the server, by adding the path to the server's `index.js`, you'll need to modify this to point to where you cloned the Grammarly repo.
+3. Configure the server, by adding the path to the server's `index.js`. The example below will need to be modified to point to the location of the Grammarly repo.
 
 ```lua
 cmd = { "node", ".../grammarly/extension/dist/server/index.js", "--stdio" }
 ```
 ]],
     default_config = {
-      root_dir = [[current directory]],
+      root_dir = [[util.find_git_ancestor]],
     },
   },
 }
