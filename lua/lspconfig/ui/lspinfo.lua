aaf72e09890e3fdfd7d0e395a2153576478a1e89
Michael Lingelbach <m.j.lbach@gmail.com>
2021-12-15 09:47:09 -0800

    fix: remove dependency on deprecated close_preview_autocommand

--- lua/lspconfig/ui/lspinfo.lua
+++ lua/lspconfig/ui/lspinfo.lua
@@ -203,7 +203,9 @@ return function()
   vim.api.nvim_buf_set_option(bufnr, 'filetype', 'lspinfo')
 
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<esc>', '<cmd>bd<CR>', { noremap = true })
-  vim.lsp.util.close_preview_autocmd({ 'BufHidden', 'BufLeave' }, win_id)
+  vim.api.nvim_command(
+    string.format('autocmd BufHidden,BufLeave <buffer> ++once lua pcall(vim.api.nvim_win_close, %d, true)', win_id)
+  )
 
   vim.fn.matchadd(
     'Error',
