73c75eee0965b28e58953693084e19a129cce996
luisiacc <lccpacc@gmail.com>
2021-12-19 23:38:45 -0500

    docs: correct example for settings.workspace.library on sumneko lua server

--- lua/lspconfig/server_configurations/sumneko_lua.lua
+++ lua/lspconfig/server_configurations/sumneko_lua.lua
@@ -38,7 +38,7 @@ require'lspconfig'.sumneko_lua.setup {
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
+        library = {vim.api.nvim_get_runtime_file("", true)},
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
