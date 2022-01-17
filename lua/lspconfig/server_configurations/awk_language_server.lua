7baa0930bc2274b7dfcf470b0125a669139684e9
lilibyte <lily@lilibyte.net>
2022-01-17 23:10:50 +0000

    feat: add awk-language-server
    without handling `workspace/workspaceFolders` the server crashes at start:
    `server_request: no handler found for workspace/workspaceFolders`

---
+++ lua/lspconfig/server_configurations/awk_language_server.lua
@@ -0,0 +1,30 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'awk-language-server' },
+    filetypes = { 'awk' },
+    root_dir = function(fname)
+        return util.path.dirname(fname)
+    end,
+    single_file_support = true,
+    handlers = {
+        ['workspace/workspaceFolders'] = function()
+        return {
+          result = nil,
+          error = nil,
+        }
+        end
+      }
+  },
+  docs = {
+    description = [[
+https://github.com/Beaglefoot/awk-language-server/
+
+`awk-language-server` can be installed via `npm`:
+```sh
+npm install -g awk-language-server
+```
+]],
+  },
+}
