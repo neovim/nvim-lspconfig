f9f799b69f98051aa3b52db38e87369dca01d08d
Oliver <or@4morgen.de>
2022-01-21 08:29:39 +0100

    feat(html): enable formatter by default  (#1669)

--- lua/lspconfig/server_configurations/html.lua
+++ lua/lspconfig/server_configurations/html.lua
@@ -15,6 +15,7 @@ return {
     single_file_support = true,
     settings = {},
     init_options = {
+      provideFormatter = true,
       embeddedLanguages = { css = true, javascript = true },
       configurationSection = { 'html', 'css', 'javascript' },
     },
@@ -30,6 +31,8 @@ npm i -g vscode-langservers-extracted
 
 Neovim does not currently include built-in snippets. `vscode-html-language-server` only provides completions when snippet support is enabled.
 To enable completion, install a snippet plugin and add the following override to your language client capabilities during setup.
+
+The code-formatting feature of the lsp can be controlled with the `provideFormatter` option.
 
 ```lua
 --Enable (broadcasting) snippet capability for completion
