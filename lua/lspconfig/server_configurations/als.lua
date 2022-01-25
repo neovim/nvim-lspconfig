94a2114296a88e2eb2e0e78072ee3e62993aa044
Tama McGlinn <t.mcglinn@gmail.com>
2022-01-25 07:50:19 +0100

    feat(als): show GPS project file in LspInfo

--- lua/lspconfig/server_configurations/als.lua
+++ lua/lspconfig/server_configurations/als.lua
@@ -10,6 +10,31 @@ return {
     cmd = { bin_name },
     filetypes = { 'ada' },
     root_dir = util.root_pattern('Makefile', '.git', '*.gpr', '*.adc'),
+    lspinfo = function(cfg)
+      local extra = {}
+      local function find_gpr_project()
+        local function split(inputstr, sep)
+          if sep == nil then
+            sep = '%s'
+          end
+          local t = {}
+          for str in string.gmatch(inputstr, '([^' .. sep .. ']+)') do
+            table.insert(t, str)
+          end
+          return t
+        end
+        local projectfiles = split(vim.fn.glob(cfg.root_dir .. '/*.gpr'))
+        if #projectfiles == 0 then
+          return 'None (error)'
+        elseif #projectfiles == 1 then
+          return projectfiles[1]
+        else
+          return 'Ambiguous (error)'
+        end
+      end
+      table.insert(extra, 'GPR project:     ' .. ((cfg.settings.ada or {}).projectFile or find_gpr_project()))
+      return extra
+    end,
   },
   docs = {
     package_json = 'https://raw.githubusercontent.com/AdaCore/ada_language_server/master/integration/vscode/ada/package.json',
