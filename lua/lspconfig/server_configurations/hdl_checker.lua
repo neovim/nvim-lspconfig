d325ae6d1d3e735819c271faea6459b1e01e5b18
Arslan Majid <arslan.majid@inl.gov>
2022-02-17 10:50:16 -0700

    Stylua

--- lua/lspconfig/server_configurations/hdl_checker.lua
+++ lua/lspconfig/server_configurations/hdl_checker.lua
@@ -1,23 +1,23 @@
-local configs = require("lspconfig.configs")
-local util = require("lspconfig.util")
+local configs = require 'lspconfig.configs'
+local util = require 'lspconfig.util'
 
-local server_name = "hdl_checker"
+local server_name = 'hdl_checker'
 
 configs[server_name] = {
-	default_config = {
-		cmd = { "hdl_checker", "--lsp" },
-		filetypes = { "vhdl", "verilog", "systemverilog" },
-		root_dir = util.find_git_ancestor,
-		single_file_support = true,
-	},
-	docs = {
-		description = [[
+  default_config = {
+    cmd = { 'hdl_checker', '--lsp' },
+    filetypes = { 'vhdl', 'verilog', 'systemverilog' },
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
 https://github.com/suoto/hdl_checker
 Language server for hdl-checker.
 Install using: `pip install hdl-checker --upgrade`
 ]],
-		default_config = {
-			root_dir = [[util.find_git_ancestor]],
-		},
-	},
+    default_config = {
+      root_dir = [[util.find_git_ancestor]],
+    },
+  },
 }
