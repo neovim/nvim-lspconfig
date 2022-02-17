4d7d38869c615ff8f7aea4b5ff897e04676d3c29
Arslan Majid <arslan.majid@inl.gov>
2022-02-17 10:49:17 -0700

    Fixed comments

--- lua/lspconfig/server_configurations/hdl_checker.lua
+++ lua/lspconfig/server_configurations/hdl_checker.lua
@@ -1,24 +1,16 @@
--- Only `configs` must be required, util is optional if you are using the root
--- resolver functions, which is usually the case.
 local configs = require("lspconfig.configs")
 local util = require("lspconfig.util")
 
--- Having server name defined here is the convention, this is often times also the first entry in the `cmd` table.
 local server_name = "hdl_checker"
 
 configs[server_name] = {
 	default_config = {
-		-- This should be executable on the command line, arguments (such as
-		-- `--stdio`) are additional entries in the list.
-		-- cmd = { 'pyright-langserver' },
 		cmd = { "hdl_checker", "--lsp" },
 		filetypes = { "vhdl", "verilog", "systemverilog" },
 		root_dir = util.find_git_ancestor,
 		single_file_support = true,
 	},
 	docs = {
-		-- The description should include at minimum the link to the github
-		-- project, and ideally the steps to install the language server.
 		description = [[
 https://github.com/suoto/hdl_checker
 Language server for hdl-checker.
