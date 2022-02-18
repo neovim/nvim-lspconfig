5ed39e2dcb666bd3753899607a03448d9856f783
ri7nz <ri7nz@evilfactory.id>
2022-02-18 13:32:34 +0700

    feat(reason_ls): rename reasonls to reason_ls

--- lua/lspconfig/server_configurations/reason_ls.lua
+++ lua/lspconfig/server_configurations/reason_ls.lua
@@ -11,7 +11,7 @@ return {
     description = [[
 Reason language server
 
-**By default, reasonls doesn't have a `cmd` set.** This is because nvim-lspconfig does not make assumptions about your path.
+**By default, reason_ls doesn't have a `cmd` set.** This is because nvim-lspconfig does not make assumptions about your path.
 You have to install the language server manually.
 
 You can install reason language server from [reason-language-server](https://github.com/jaredly/reason-language-server) repository.
