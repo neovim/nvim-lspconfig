6132354ae7fce161beeca73f435109e833b4f507
Anton Kastritskiy <antonk52@fb.com>
2022-02-21 12:09:14 +0000

    code review changes

--- lua/lspconfig/server_configurations/hhvm.lua
+++ lua/lspconfig/server_configurations/hhvm.lua
@@ -1,25 +1,19 @@
 local util = require 'lspconfig.util'
 
-local bin_name = 'hh_client'
-local cmd = {bin_name, 'lsp'}
-
-local description = [[
+return {
+  default_config = {
+    cmd = {'hh_client', 'lsp'},
+    filetypes = { 'php', 'hack' },
+    root_dir = util.root_pattern '.hhconfig',
+  },
+  docs = {
+    description = [[
 Language server for programs written in Hack
 https://hhvm.com/
 https://github.com/facebook/hhvm
 See below for how to setup HHVM & typechecker:
 https://docs.hhvm.com/hhvm/getting-started/getting-started
-]]
-
-return {
-  default_config = {
-    cmd = cmd,
-    filetypes = { 'php', 'hack' },
-    root_dir = util.root_pattern '.hhconfig',
-    settings = {},
-  },
-  docs = {
-    description = description,
+    ]],
     default_config = {
       root_dir = [[root_pattern(".hhconfig")]],
     },
