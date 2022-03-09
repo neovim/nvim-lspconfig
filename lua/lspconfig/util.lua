neovim/nvim-lspconfig

commit a209627886c3edc6926bede732543f9c09aaf22c

feat: improve interface for `:Lsp*` commands (#1324)

lua/lspconfig/util.lua
@@ -373,4 +373,15 @@ function M.get_other_matching_providers(filetype)
   return other_matching_configs
 end
 
+function M.get_clients_from_cmd_args(arg)
+  local result = {}
+  for id in (arg or ''):gmatch '(%d+) %((%w+)%)' do
+    result[id] = vim.lsp.get_client_by_id(tonumber(id))
+  end
+  if vim.tbl_isempty(result) then
+    return vim.lsp.get_active_clients()
+  end
+  return vim.tbl_values(result)
+end
+
 return M

lua/lspconfig.lua
@@ -26,15 +26,15 @@ function M._root._setup()
     LspStart = {
       function(server_name)
         if server_name then
-          require('lspconfig')[server_name].autostart()
+          if configs[server_name] then
+            configs[server_name].autostart()
+          end
         else
           local buffer_filetype = vim.bo.filetype
-          for client_name, config in pairs(configs) do
-            if config.filetypes then
-              for _, filetype_match in ipairs(config.filetypes) do
-                if buffer_filetype == filetype_match then
-                  require('lspconfig')[client_name].autostart()
-                end
+          for _, config in pairs(configs) do
+            for _, filetype_match in ipairs(config.filetypes or {}) do
+              if buffer_filetype == filetype_match then
+                config.autostart()
               end
             end
           end
@@ -44,39 +44,25 @@ function M._root._setup()
       description = '`:LspStart` Manually launches a language server.',
     },
     LspStop = {
-      function(client_id)
-        if not client_id then
-          vim.lsp.stop_client(vim.lsp.get_active_clients())
-        else
-          local client = vim.lsp.get_client_by_id(tonumber(client_id))
-          if client then
-            client.stop()
-          end
+      function(cmd_args)
+        for _, client in ipairs(M.util.get_clients_from_cmd_args(cmd_args)) do
+          client.stop()
         end
       end,
       '-nargs=? -complete=customlist,v:lua.lsp_get_active_client_ids',
-      description = '`:LspStop` Manually stops the given language client.',
+      description = '`:LspStop` Manually stops the given language client(s).',
     },
     LspRestart = {
-      function(client_id)
-        local clients
-
-        if client_id == nil then
-          clients = vim.lsp.buf_get_clients(0)
-        else
-          clients = { vim.lsp.get_client_by_id(tonumber(client_id)) }
-        end
-
-        for _, client in pairs(clients) do
-          local client_name = client.name
+      function(cmd_args)
+        for _, client in ipairs(M.util.get_clients_from_cmd_args(cmd_args)) do
           client.stop()
           vim.defer_fn(function()
-            require('lspconfig')[client_name].autostart()
+            configs[client.name].autostart()
           end, 500)
         end
       end,
       '-nargs=? -complete=customlist,v:lua.lsp_get_active_client_ids',
-      description = '`:LspRestart` Manually restart the given language client.',
+      description = '`:LspRestart` Manually restart the given language client(s).',
     },
   }
 

plugin/lspconfig.vim
@@ -8,11 +8,9 @@ lsp_complete_configured_servers = function()
   return table.concat(require'lspconfig'.available_servers(), '\n')
 end
 lsp_get_active_client_ids = function()
-  client_ids = {}
-  for idx, client in ipairs(vim.lsp.get_active_clients()) do
-    table.insert(client_ids, tostring(client.id))
-  end
-  return client_ids
+  return vim.tbl_map(function(client)
+    return ("%d (%s)"):format(client.id, client.name)
+  end, vim.lsp.get_active_clients())
 end
 require'lspconfig'._root._setup()
 EOF

commit 53c841a8a36ae8f9ecd4bf1eef4b0d7417f520e5

fix(lspstop): make LspStop behave as described in the docs
This bug was introduced in #1324.

When calling `:LspStop <args>`, the current code tries to match a string
with the format "123 (blablabla)", which will result in terminating the
server with ID 123. This is fine when using autocompletion, but if doing
this programmatically (or just following the docs) something like
`:LspStop 1` will not match anything, resulting in terminating *all*
managed servers.

This should be enough to fix the bug, allow terminating multiple servers
at once and keeping the nice UX provided by the autocompletion.

lua/lspconfig/util.lua
@@ -395,7 +395,7 @@ end
 
 function M.get_clients_from_cmd_args(arg)
   local result = {}
-  for id in (arg or ''):gmatch '(%d+) %((%w+)%)' do
+  for id in (arg or ''):gmatch '(%d+)' do
     result[id] = vim.lsp.get_client_by_id(tonumber(id))
   end
   if vim.tbl_isempty(result) then