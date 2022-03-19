233f59e05f0f4e4a92319cd6fcd22e12c261969f
Dan Sully <dan+github@sully.org>
2022-03-19 13:51:31 -0700

    Add prosemd-lsp.

---
+++ lua/lspconfig/server_configurations/prosemd_lsp.lua
@@ -0,0 +1,22 @@
+local util = require 'lspconfig.util'
+
+return {
+    default_config = {
+        cmd = { "prosemd-lsp", "--stdio" },
+        filetypes = { "markdown" },
+        root_dir = function(fname)
+            return util.find_git_ancestor(fname) or vim.fn.getcwd()
+        end,
+        single_file_support = true,
+    },
+    docs = {
+    description = [[
+https://github.com/kitten/prosemd-lsp
+
+An experimental LSP for Markdown.
+]],
+    default_config = {
+      root_dir = [[find_git_ancestor(fname) or vim.fn.getcwd()]]
+    },
+  },
+}
