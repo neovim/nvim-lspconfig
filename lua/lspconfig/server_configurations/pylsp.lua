e333afc150bae164b1acaad08387b3eef60def12
gerazov <gerazov@feit.ukim.edu.mk>
2022-05-23 16:38:37 +0200

    updated pylsp lua

--- lua/lspconfig/server_configurations/pylsp.lua
+++ lua/lspconfig/server_configurations/pylsp.lua
@@ -26,6 +26,30 @@ The language server can be installed via `pipx install 'python-lsp-server[all]'`
 Further instructions can be found in the [project's README](https://github.com/python-lsp/python-lsp-server).
 
 Note: This is a community fork of `pyls`.
+
+**Enabling/disabling plugins:**
+- Disable `mccabe` complexity checking (enabled by default), enable `pylint` (disabled by default), and add `E501` (line too long) error to ignore list: 
+```lua
+require('lspconfig').pylsp.setup {
+  on_attach = on_attach,
+  flags = {
+    -- This will be the default in neovim 0.7+
+    debounce_text_changes = 150,
+  },
+  settings = {
+    pylsp = {
+      plugins = {
+        mccabe = { enabled = false },
+        pylint = { enabled = true },
+        pycodestyle = { ignore = {'E501'} }
+      }
+    }
+  }
+}
+```
+
+You can use [williamboman/nvim-lsp-installer](https://github.com/williamboman/nvim-lsp-installer) to find a listing of all the plugins and their settings.
+
     ]],
   },
 }
