17648ad00add7a7255e628675e4c7fed99781596
Moshe Avni <mavni@netapp.com>
2022-05-26 00:34:04 +0300

    Revert server_configurations and add default

--- lua/lspconfig/util.lua
+++ lua/lspconfig/util.lua
@@ -14,6 +14,7 @@ M.default_config = {
   init_options = vim.empty_dict(),
   handlers = {},
   autostart = true,
+  unnamed_buffer_support = false,
   capabilities = lsp.protocol.make_client_capabilities(),
 }
 
