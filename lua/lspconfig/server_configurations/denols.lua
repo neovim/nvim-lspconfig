3c246c571f7ca683a801662bd70d5cd7640ed3aa
David <12832280+David-Else@users.noreply.github.com>
2022-05-30 13:20:52 +0100

    quick-fix

--- lua/lspconfig/server_configurations/denols.lua
+++ lua/lspconfig/server_configurations/denols.lua
@@ -107,7 +107,7 @@ vim.g.markdown_fenced_languages = {
 
 ]]   ,
     default_config = {
-      root_dir = [[root_pattern("deno.json", "deno.jsonc", "tsconfig.json", ".git")]],
+      root_dir = [[root_pattern("deno.json", "deno.jsonc", ".git")]],
     },
   },
 }
