75d792b57cf8cd18b7467da952f816e4c312dfc7
David <12832280+David-Else@users.noreply.github.com>
2022-05-30 13:07:20 +0100

    fix(denols):update settings for lint and tsconfig

--- lua/lspconfig/server_configurations/denols.lua
+++ lua/lspconfig/server_configurations/denols.lua
@@ -72,10 +72,9 @@ return {
       'typescriptreact',
       'typescript.tsx',
     },
-    root_dir = util.root_pattern('deno.json', 'deno.jsonc', 'tsconfig.json', '.git'),
+    root_dir = util.root_pattern('deno.json', 'deno.jsonc', '.git'),
     init_options = {
       enable = true,
-      lint = false,
       unstable = false,
     },
     handlers = {
@@ -106,7 +105,7 @@ vim.g.markdown_fenced_languages = {
 }
 ```
 
-]],
+]]   ,
     default_config = {
       root_dir = [[root_pattern("deno.json", "deno.jsonc", "tsconfig.json", ".git")]],
     },
