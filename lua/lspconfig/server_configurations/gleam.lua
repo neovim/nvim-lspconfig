7b96d8fb11c5d07915981fd3a9ac93cbcdb91937
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2022-07-08 10:38:16 -0300

    chore: some updates

--- lua/lspconfig/server_configurations/gleam.lua
+++ lua/lspconfig/server_configurations/gleam.lua
@@ -5,20 +5,18 @@ return {
     cmd = {'gleam', 'lsp'},
     filetypes = { 'gleam' },
     root_dir = function(fname)
-      return util.root_pattern('gleam.toml', '.git')(fname) or vim.loop.os_homedir()
+      return util.root_pattern('gleam.toml', '.git')(fname)
     end,
   },
   docs = {
     description = [[
 https://github.com/gleam-lang/gleam
 
-```lua
-require('lspconfig').gleam.setup({})
-```
+A language server for Gleam Programming Language.
 ]],
     default_config = {
       cmd = { 'gleam', 'lsp' },
-      root_dir = [[root_pattern("gleam.toml", ".git") or vim.loop.os_homedir()]],
+      root_dir = [[root_pattern("gleam.toml", ".git")]],
     },
   },
 }
