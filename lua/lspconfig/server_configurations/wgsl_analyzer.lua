06d43802ae8a86a8eb7c0103e9466d23c4c0d44a
Jan Cristina <jan.cristina@gmail.com>
2022-08-17 22:05:13 +0200

    wgsl-analyzer-custom-settings - allow custom settings

--- lua/lspconfig/server_configurations/wgsl_analyzer.lua
+++ lua/lspconfig/server_configurations/wgsl_analyzer.lua
@@ -6,6 +6,32 @@ local cmd = { bin_name }
 if vim.fn.has 'win32' == 1 then
   cmd = { 'cmd.exe', '/C', bin_name }
 end
+local localSettings = {
+    customImports = {},
+    shaderDefs = {},
+    trace = {
+        extension = true,
+        server = true,
+    },
+    inlayHints = {
+        enabled = true,
+        typeHints = true,
+        parameterHints = true,
+        structLayoutHints = true,
+        typeVerbosity = "compact",
+    },
+    diagnostics = {
+        typeErrors = true,
+        nagaParsingErrors = true,
+        nagaValidationErrors = true,
+        nagaVersion = "main",
+    }
+}
+
+vim.lsp.handlers['wgsl-analyzer/requestConfiguration'] = function(err, result, ctx, config) 
+    return localSettings, nil
+end
+
 
 return {
   default_config = {
