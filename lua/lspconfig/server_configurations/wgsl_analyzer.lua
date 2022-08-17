b7cafb44c770c9402223efdf433901e34c420682
Jan Cristina <jan.cristina@gmail.com>
2022-08-17 23:12:40 +0200

    wgsl-analyzer-custom-imports - get it working

--- lua/lspconfig/server_configurations/wgsl_analyzer.lua
+++ lua/lspconfig/server_configurations/wgsl_analyzer.lua
@@ -7,39 +7,44 @@ if vim.fn.has 'win32' == 1 then
   cmd = { 'cmd.exe', '/C', bin_name }
 end
 local localSettings = {
-    customImports = {},
-    shaderDefs = {},
-    trace = {
-        extension = true,
-        server = true,
-    },
-    inlayHints = {
-        enabled = true,
-        typeHints = true,
-        parameterHints = true,
-        structLayoutHints = true,
-        typeVerbosity = "compact",
-    },
-    diagnostics = {
-        typeErrors = true,
-        nagaParsingErrors = true,
-        nagaValidationErrors = true,
-        nagaVersion = "main",
-    }
+  customImports = {}, -- ['import'] = [[code to be imported]] or io.open("path to code")
+  shaderDefs = {},
+  trace = {
+    extension = true,
+    server = true,
+  },
+  inlayHints = {
+    enabled = true,
+    typeHints = true,
+    parameterHints = true,
+    structLayoutHints = true,
+    typeVerbosity = "compact", -- "full", "inner"
+  },
+  diagnostics = {
+    typeErrors = true,
+    nagaParsingErrors = true,
+    nagaValidationErrors = true,
+    nagaVersion = "main", -- "0.8", "0.9"
+  }
 }
+
 
 vim.lsp.handlers['wgsl-analyzer/requestConfiguration'] = function(err, result, ctx, config) 
     return localSettings, nil
 end
-
 
 return {
   default_config = {
     cmd = cmd,
     filetypes = { 'wgsl' },
     root_dir = util.root_pattern '.git',
-    settings = {},
+    settings = localSettings ,
   },
+  on_new_config = function(newConfig, _) 
+    vim.lsp.handlers['wgsl-analyzer/requestConfiguration'] = function(err, result, ctx, config) 
+      return newConfig.settings, nil
+    end
+  end,
   docs = {
     description = [[
 https://github.com/wgsl-analyzer/wgsl-analyzer
