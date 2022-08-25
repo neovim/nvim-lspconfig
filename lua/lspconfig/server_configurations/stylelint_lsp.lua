b4354e96a2cc31c6599571ec33e48282d635f87c
Benny Powers <web@bennypowers.com>
2022-08-25 17:11:29 +0300

    feat(stylelint): add `StylelintFixAll` command

--- lua/lspconfig/server_configurations/stylelint_lsp.lua
+++ lua/lspconfig/server_configurations/stylelint_lsp.lua
@@ -7,6 +7,37 @@ if vim.fn.has 'win32' == 1 then
   cmd = { 'cmd.exe', '/C', bin_name, '--stdio' }
 end
 
+local function fix_all(opts)
+  opts = opts or { sync = true, bufnr = 0 }
+  local bufnr = util.validate_bufnr(opts.bufnr or 0)
+
+  local stylelint_lsp_client = util.get_active_client_by_name(bufnr, 'stylelint_lsp')
+  if stylelint_lsp_client == nil then
+    return
+  end
+
+  local request
+  if opts.sync then
+    request = function(buf, method, params)
+      stylelint_lsp_client.request_sync(method, params, nil, buf)
+    end
+  else
+    request = function(buf, method, params)
+      stylelint_lsp_client.request(method, params, nil, buf)
+    end
+  end
+
+  request(bufnr, 'workspace/executeCommand', {
+    command = 'stylelint.applyAutoFixes',
+    arguments = {
+      {
+        uri = vim.uri_from_bufnr(bufnr),
+        version = vim.lsp.util.buf_versions[bufnr],
+      },
+    },
+  })
+end
+
 return {
   default_config = {
     cmd = cmd,
@@ -24,6 +55,14 @@ return {
     },
     root_dir = util.root_pattern('.stylelintrc', 'package.json'),
     settings = {},
+    commands = {
+      StylelintFixAll = {
+        function()
+          fix_all { sync = true, bufnr = 0 }
+        end,
+        description = 'Fix all stylelint problems for this buffer',
+      },
+    },
   },
   docs = {
     description = [[
@@ -46,7 +85,21 @@ require'lspconfig'.stylelint_lsp.setup{
   }
 }
 ```
-]],
+
+`stylelint_lsp` provides a `StylelintFixAll` command that can be used to format a document on save:
+
+```lua
+local ag = vim.api.nvim_create_augroup
+local au = vim.api.nvim_create_autocmd
+
+local autoformat_workaround = ag('lsp_format_workaround', {})
+au('BufWritePre', {
+  group = autoformat_workaround,
+  pattern = { '*.css', '*.scss', },
+  command = 'StylelintFixAll',
+})
+```
+]]   ,
     default_config = {
       root_dir = [[ root_pattern('.stylelintrc', 'package.json') ]],
     },
