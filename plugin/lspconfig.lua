9f4b45ad5f1bbfe81a58a26b0fc26e83aafe440e
Andrew <x.and.i.rew.e@gmail.com>
2022-08-27 02:03:41 +1000

    fix(ui): update default `LspInfoBorder` link to `FloatBorder`

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -49,7 +49,7 @@ local get_clients_from_cmd_args = function(arg)
 end
 
 for group, hi in pairs {
-  LspInfoBorder = { link = 'Label', default = true },
+  LspInfoBorder = { link = 'FloatBorder', default = true },
   LspInfoList = { link = 'Function', default = true },
   LspInfoTip = { link = 'Comment', default = true },
   LspInfoTitle = { link = 'Title', default = true },
