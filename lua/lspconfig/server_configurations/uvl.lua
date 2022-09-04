15ffae14346fd4307e6b210d89b0c86c9cdcc717
Max <caradhras11@gmail.com>
2022-09-05 00:44:58 +0200

    add lsp

---
+++ lua/lspconfig/server_configurations/uvl.lua
@@ -0,0 +1,24 @@
+
+local util = require 'lspconfig.util'
+
+local bin_name = 'uvl-ls'
+local cmd = { bin_name, 'start' }
+
+
+return {
+  default_config = {
+    cmd = cmd,
+    filetypes = { '.uvl' },
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+```
+Language server for uvl, written using tree sitter in rust.
+]],
+    default_config = {
+      root_dir = [[util.find_git_ancestor]],
+    },
+  },
+}
