5e03510714e45c63e3f31d48e99a04e308044d95
Max <caradhras11@gmail.com>
2022-09-05 01:27:07 +0200

    fix name

--- lua/lspconfig/server_configurations/uvls.lua
+++ lua/lspconfig/server_configurations/uvls.lua
@@ -1,20 +1,14 @@
 
 local util = require 'lspconfig.util'
-
-local bin_name = 'uvls'
-local cmd = { bin_name, 'start' }
-
-
 return {
   default_config = {
-    cmd = cmd,
+    cmd = 'uvls',
     filetypes = { 'uvl' },
     root_dir = util.find_git_ancestor,
     single_file_support = true,
   },
   docs = {
     description = [[
-```
 Language server for uvl, written using tree sitter in rust.
 ]],
     default_config = {
