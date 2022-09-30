41394035301fee2789479e9bb2fce594739599ec
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-09-30 13:19:57 +0300

    fix(marksman): enable single_file_support

--- lua/lspconfig/server_configurations/marksman.lua
+++ lua/lspconfig/server_configurations/marksman.lua
@@ -11,6 +11,7 @@ return {
       local root_files = { '.marksman.toml' }
       return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
     end,
+    single_file_support = true,
   },
   docs = {
     description = [[
