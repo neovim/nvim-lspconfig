0c6bc96a33867c0f4e985f8882ef89cb5ac97bb0
Patrick Li <1543162420@qq.com>
2022-10-13 15:18:23 -0400

    feat: fix dafny lsp

--- lua/lspconfig/server_configurations/dafny.lua
+++ lua/lspconfig/server_configurations/dafny.lua
@@ -1,9 +1,16 @@
 local util = require 'lspconfig.util'
 
+local root_files = {
+    'compile_commands.json',
+    '.ccls',
+}
 return {
   default_config = {
     filetypes = { 'dfy', 'dafny'},
-    cmd = { 'dotnet', '/opt/dafny/DafnyLanguageServer.dll'}
+    cmd = { 'dotnet', '/opt/dafny/DafnyLanguageServer.dll' },
+    root_dir = function(fname)
+      return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
+    end,
   },
   docs = {
     description = [[
