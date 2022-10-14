c01ca49d8a7a34b4f95c29a2213016e648edabaf
Patrick Li <1543162420@qq.com>
2022-10-14 15:42:20 -0400

    fix: possible windows compatibility

--- lua/lspconfig/server_configurations/dafny.lua
+++ lua/lspconfig/server_configurations/dafny.lua
@@ -1,9 +1,17 @@
 local util = require 'lspconfig.util'
 
+local bin_name = 'donat'
+local dll_path = '/opt/dafny/DafnyLanguageServer.dll'
+local cmd = {bin_name, dll_path}
+if vim.fn.has 'win32' == 1 then
+  dll_path = 'C:\\Program Files\\dafny\\DafnyLanguageServer.dll'
+  cmd = { 'cmd.exe', '/C', bin_name, dll_path }
+end
+
 return {
   default_config = {
     filetypes = { 'dfy', 'dafny' },
-    cmd = { 'dotnet', '/opt/dafny/DafnyLanguageServer.dll' },
+    cmd = cmd,
     root_dir = function(fname)
       util.find_git_ancestor(fname)
     end,
