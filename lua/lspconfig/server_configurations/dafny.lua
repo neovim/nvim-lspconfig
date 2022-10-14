7c1044ad840ed47e25102ab5e02413487769c602
Patrick Li <1543162420@qq.com>
2022-10-14 15:31:56 -0400

    fix: a better way of single file mode

--- lua/lspconfig/server_configurations/dafny.lua
+++ lua/lspconfig/server_configurations/dafny.lua
@@ -1,16 +1,13 @@
 local util = require 'lspconfig.util'
 
-local root_files = {
-    'compile_commands.json',
-    '.ccls',
-}
 return {
   default_config = {
     filetypes = { 'dfy', 'dafny' },
     cmd = { 'dotnet', '/opt/dafny/DafnyLanguageServer.dll' },
     root_dir = function(fname)
-      return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
+      util.find_git_ancestor(fname)
     end,
+    single_file_support = true
   },
   docs = {
     description = [[
