9e0b8f6aeaf7ea0bf9f8c8375c21730cb0be1583
Patrick Li <1543162420@qq.com>
2022-10-15 16:43:38 -0400

    fix: path to dll removed

--- lua/lspconfig/server_configurations/dafny.lua
+++ lua/lspconfig/server_configurations/dafny.lua
@@ -1,17 +1,8 @@
 local util = require 'lspconfig.util'
 
-local bin_name = 'dotnet'
-local dll_path = '/opt/dafny/DafnyLanguageServer.dll'
-local cmd = {bin_name, dll_path}
-if vim.fn.has 'win32' == 1 then
-  dll_path = 'C:\\Program Files\\dafny\\DafnyLanguageServer.dll'
-  cmd = { 'cmd.exe', '/C', bin_name, dll_path }
-end
-
 return {
   default_config = {
     filetypes = { 'dfy', 'dafny' },
-    cmd = cmd,
     root_dir = function(fname)
       util.find_git_ancestor(fname)
     end,
