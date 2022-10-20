efe8c8530860f5524c6d7252c95d34f609ee3884
Saltaformajo <odarchenko.n.d@gmail.com>
2022-10-20 09:14:17 +0300

    docs(golangci_lint_ls): update a version of binary (#2204)

--- lua/lspconfig/server_configurations/golangci_lint_ls.lua
+++ lua/lspconfig/server_configurations/golangci_lint_ls.lua
@@ -23,7 +23,7 @@ Installation of binaries needed is done via
 
 ```
 go install github.com/nametake/golangci-lint-langserver@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 ```
 
 ]],
