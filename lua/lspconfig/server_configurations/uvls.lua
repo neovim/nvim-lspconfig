b0ed1f7cd634b4d72c4440a6048163ba05e89271
Max <caradhras11@gmail.com>
2022-12-16 00:18:41 +0100

    add some doc

--- lua/lspconfig/server_configurations/uvls.lua
+++ lua/lspconfig/server_configurations/uvls.lua
@@ -1,18 +1,30 @@
-
 local util = require 'lspconfig.util'
 return {
-  default_config = {
-    cmd = {'uvls'},
-    filetypes = { 'uvl' },
-    root_dir = util.find_git_ancestor,
-    single_file_support = true,
-  },
-  docs = {
-    description = [[
-Language server for uvl, written using tree sitter in rust.
-]],
     default_config = {
-      root_dir = [[util.find_git_ancestor]],
+        cmd = { 'uvls' },
+        filetypes = { 'uvl' },
+        root_dir = util.find_git_ancestor,
+        single_file_support = true,
     },
-  },
+    docs = {
+        description = [[
+https://codeberg.org/caradhras/uvls
+
+Language server for UVL, written using tree sitter and rust.
+You can install the server easily using cargo:
+```sh
+git clone https://codeberg.org/caradhras/uvls
+cd  uvls
+cargo install --path .
+```
+Note: To activate properly nvim needs to know the uvl filetype.
+You can add it via: 
+```lua
+vim.cmd(\[\[au BufRead,BufNewFile *.uvl		setfiletype uvl\]\])
+```
+]],
+        default_config = {
+            root_dir = [[util.find_git_ancestor]],
+        },
+    },
 }
