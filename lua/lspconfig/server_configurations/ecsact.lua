621ebacbd59f1edde24d22be6549e404189cf916
Ezekiel Warren <zekewarren@gmail.com>
2022-12-16 10:14:02 -0800

    feat: add ecsact support

---
+++ lua/lspconfig/server_configurations/ecsact.lua
@@ -0,0 +1,24 @@
+local util = require 'lspconfig.util'
+
+local bin_name = 'ecsact_lsp_server'
+local cmd = { bin_name, "--stdio" }
+
+return {
+	default_config = {
+		cmd = cmd,
+		filetypes = { 'ecsact' },
+		root_dir = util.root_pattern('.git'),
+		single_file_support = true,
+	},
+
+	docs = {
+		description = [[
+https://github.com/ecsact-dev/ecsact_lsp_server
+
+Language server for Ecsact.
+
+The default cmd assumes `ecsact_lsp_server` is in your PATH. Typically from the
+Ecsact SDK: https://ecsact.dev/start
+		]],
+	},
+}
