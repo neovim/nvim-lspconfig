f899686b6024f4e62abcd9d6e511171df395a561
Raphael <glephunter@gmail.com>
2023-01-03 13:57:22 +0800

    fix: format

--- lua/lspconfig/server_configurations/coffeesense.lua
+++ lua/lspconfig/server_configurations/coffeesense.lua
@@ -4,7 +4,7 @@ local bin_name = 'coffeesense-language-server'
 local cmd = { bin_name, '--stdio' }
 
 if vim.fn.has 'win32' == 1 then
-  cmd = { 'cmd.exe', '/C', bin_name, "--stdio" }
+  cmd = { 'cmd.exe', '/C', bin_name, '--stdio' }
 end
 
 return {
