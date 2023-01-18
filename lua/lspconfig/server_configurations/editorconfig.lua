2823999dbf989e52014476fafe74bd1bba6d36cd
Erik Lilja <erikvlilja@gmail.com>
2023-01-18 12:21:07 +0100

    feat(editorconfig): add editorconfig ls

---
+++ lua/lspconfig/server_configurations/editorconfig.lua
@@ -0,0 +1,24 @@
+local util = require 'lspconfig.util'
+
+local root_files = { '.editorconfig', '.git' }
+return {
+  default_config = {
+    cmd = { 'ecls', '--stdio' },
+    filetypes = { 'editorconfig' },
+    root_dir = function(fname)
+      return util.root_pattern(unpack(root_files))(fname)
+    end,
+    init_options = {
+      buildDirectory = 'build',
+    },
+  },
+  docs = {
+    description = [[
+https://github.com/Lilja/ecls
+Editorconfig language server
+]],
+    default_config = {
+      root_dir = [[root_pattern('.editorconfig')]],
+    },
+  },
+}
