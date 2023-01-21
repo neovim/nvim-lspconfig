d569c4a4ef3027ac3e304bfb425346a09eef0256
Raphael <glephunter@gmail.com>
2023-01-21 08:02:21 +0800

    fix: format

--- lua/lspconfig/server_configurations/nomad_lsp.lua
+++ lua/lspconfig/server_configurations/nomad_lsp.lua
@@ -17,7 +17,7 @@ https://github.com/juliosueiras/nomad-lsp
 
 Written in Go, compilation is needed for `nomad_lsp` to be used. Please see the [original repository](https://github.com/juliosuieras/nomad-lsp).
 
-Add the executable to your system or vim PATH and it will be set to go. 
+Add the executable to your system or vim PATH and it will be set to go.
 
 No configuration option is needed unless you choose not to add `nomad-lsp` executable to the PATH. You should know what you are doing if you choose so.
 
