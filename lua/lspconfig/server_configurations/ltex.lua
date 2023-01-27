d3f6f0d4c58ab68b94fbd934db55f5b9cbcae7f1
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-01-27 07:46:08 +0000

    docs(ltex): add  settings example  (#2421)

--- lua/lspconfig/server_configurations/ltex.lua
+++ lua/lspconfig/server_configurations/ltex.lua
@@ -37,6 +37,16 @@ LTeX Language Server: LSP language server for LanguageTool 🔍✔️ with suppo
 
 To install, download the latest [release](https://github.com/valentjn/ltex-ls/releases) and ensure `ltex-ls` is on your path.
 
+This server accepts configuration via the `settings` key.
+
+```lua
+  settings = {
+		ltex = {
+			language = "en-GB",
+		},
+	},
+```
+
 To support org files or R sweave, users can define a custom filetype autocommand (or use a plugin which defines these filetypes):
 
 ```lua
