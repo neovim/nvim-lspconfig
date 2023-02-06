457dbd95049e9cdd420a91023bfed18bdb56916f
figsoda <figsoda@pm.me>
2023-02-06 16:45:53 -0500

    refactor(sumneko_lua): use vim.deprecate

--- lua/lspconfig/server_configurations/sumneko_lua.lua
+++ lua/lspconfig/server_configurations/sumneko_lua.lua
@@ -1,2 +1,2 @@
-vim.notify('`sumneko_lua` has been renamed to `lua_language_server`', vim.log.levels.WARN)
+vim.deprecate('sumneko_lua', 'lua_language_server', '0.2.0', 'lspconfig')
 return require 'lspconfig.server_configurations.lua_language_server'
