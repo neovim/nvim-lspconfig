31e391525530259ba990dd3d135375c01346acd0
Marc Deop <marc@marcdeop.com>
2023-02-14 08:54:06 +0100

    feat: add docker-compose-language-service support (#2455)

---
+++ lua/lspconfig/server_configurations/docker_compose_language_service.lua
@@ -0,0 +1,32 @@
+local util = require 'lspconfig.util'
+
+local bin_name = 'docker-compose-language-service'
+local cmd = { bin_name, '--stdio' }
+
+if vim.fn.has 'win32' == 1 then
+  cmd = { 'cmd.exe', '/C', bin_name, '--stdio' }
+end
+
+return {
+  default_config = {
+    cmd = cmd,
+    filetypes = { 'yaml' },
+    root_dir = util.root_pattern 'docker-compose.yaml',
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+https://github.com/microsoft/compose-language-service
+This project contains a language service for Docker Compose.
+
+`compose-language-service` can be installed via `npm`:
+
+```sh
+npm install @microsoft/compose-language-service
+```
+]],
+    default_config = {
+      root_dir = [[root_pattern("docker-compose.yaml")]],
+    },
+  },
+}
