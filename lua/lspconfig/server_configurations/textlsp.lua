96eadde0a694a34732d394db8f12673316a9236c
Viktor Hangya <hangyav@gmail.com>
2023-02-19 14:37:55 +0100

    feat: add textLSP support (#2468)
    * feat: add textLSP support
    
    * fix(textLSP): spell errors

---
+++ lua/lspconfig/server_configurations/textlsp.lua
@@ -0,0 +1,57 @@
+local util = require 'lspconfig.util'
+
+local bin_name = 'textlsp'
+local cmd = { bin_name }
+
+if vim.fn.has 'win32' == 1 then
+  cmd = { 'cmd.exe', '/C', bin_name }
+end
+
+return {
+  default_config = {
+    cmd = cmd,
+    filetypes = { 'text', 'tex', 'org' },
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+    settings = {
+      textLSP = {
+        analysers = {
+          -- by default all analysers are disabled in textLSP, since many of them
+          -- need custom settings. See github page. LanguageTool is enaled here
+          -- only for a quick test.
+          languagetool = {
+            enabled = true,
+            check_text = {
+              on_open = true,
+              on_save = true,
+              on_change = false,
+            },
+          },
+        },
+        documents = {
+          org = {
+            org_todo_keywords = {
+              'TODO',
+              'IN_PROGRESS',
+              'DONE',
+            },
+          },
+        },
+      },
+    },
+    docs = {
+      description = [[
+https://github.com/hangyav/textLSP
+
+`textLSP` is an LSP server for text spell and grammar checking with various AI tools.
+It supports multiple text file formats, such as LaTeX, Org or txt.
+
+For the available text analyzer tools and their configuration, see the [GitHub](https://github.com/hangyav/textLSP) page.
+By default, all analyzers are disabled in textLSP, since most of them need special settings.
+For quick testing, LanguageTool is enabled in the default configuration.
+
+To install run: `pip install textLSP`
+]],
+    },
+  },
+}
