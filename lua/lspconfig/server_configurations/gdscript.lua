6a9f922eb6e7412a4e940e20f4d76fd4e0e3feec
Pierre Poupin <pierrep@bam.tech>
2023-03-06 01:15:23 +0100

    feat: support Godot 4 port by default (#2494)
    Godot 4 changed its default port. Since it reached stable a few days ago, we should propose the stable version's port by default.

--- lua/lspconfig/server_configurations/gdscript.lua
+++ lua/lspconfig/server_configurations/gdscript.lua
@@ -1,6 +1,6 @@
 local util = require 'lspconfig.util'
 
-local port = os.getenv 'GDScript_Port' or '6008'
+local port = os.getenv 'GDScript_Port' or '6005'
 local cmd = { 'nc', 'localhost', port }
 
 if vim.fn.has 'nvim-0.8' == 1 then
