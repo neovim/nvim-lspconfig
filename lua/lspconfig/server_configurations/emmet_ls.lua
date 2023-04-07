fcec1fc2e561a3223a044440e55b3997d6507c4d
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-04-06 23:49:09 -0400

    feat: add `svelte` to emmet_ls

--- lua/lspconfig/server_configurations/emmet_ls.lua
+++ lua/lspconfig/server_configurations/emmet_ls.lua
@@ -10,7 +10,19 @@ end
 return {
   default_config = {
     cmd = cmd,
-    filetypes = { 'astro', 'css', 'eruby', 'html', 'javascriptreact', 'less', 'sass', 'scss', 'typescriptreact', 'vue' },
+    filetypes = {
+      'astro',
+      'css',
+      'eruby',
+      'html',
+      'javascriptreact',
+      'less',
+      'sass',
+      'scss',
+      'svelte',
+      'typescriptreact',
+      'vue',
+    },
     root_dir = util.find_git_ancestor,
     single_file_support = true,
   },
