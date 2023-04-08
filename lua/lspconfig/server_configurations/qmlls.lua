c7f16e235623787b8d0101bcf6eb04e9a48834fb
denstiny <2254228017@qq.com>
2023-04-08 15:14:51 +0800

    feat: qmlls use general path

--- lua/lspconfig/server_configurations/qmlls.lua
+++ lua/lspconfig/server_configurations/qmlls.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 
 return {
   default_config = {
-    cmd = { '/usr/bin/qmlls6' },
+    cmd = { 'qmlls6' },
     filetypes = { 'qmljs' },
     root_dir = function(fname)
       return util.find_git_ancestor(fname)
