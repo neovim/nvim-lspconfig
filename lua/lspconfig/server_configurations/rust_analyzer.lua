2571d22724e4e3112e0adfe15f1398d85404fb69
Raphael <glephunter@gmail.com>
2023-04-16 13:24:39 +0800

    fix: improve rust check

--- lua/lspconfig/server_configurations/rust_analyzer.lua
+++ lua/lspconfig/server_configurations/rust_analyzer.lua
@@ -1,4 +1,5 @@
 local util = require 'lspconfig.util'
+local uv = vim.loop
 
 local function reload_workspace(bufnr)
   bufnr = util.validate_bufnr(bufnr)
@@ -10,47 +11,91 @@ local function reload_workspace(bufnr)
   end)
 end
 
+local function get_workspace_dir(args)
+  local co = coroutine.running()
+  print(co)
+  if not co then
+    return
+  end
+
+  local function safe_close(handle)
+    if not uv.is_closing(handle) then
+      uv.close(handle)
+    end
+  end
+
+  local stdout = uv.new_pipe(false)
+  local stderr = uv.new_pipe(false)
+  local chunks = {}
+  local handle
+  handle = uv.spawn('cargo', {
+    args = args,
+    cwd = uv.cwd(),
+    stdio = { nil, stdout, stderr },
+  }, function(err, code)
+    safe_close(stdout)
+    safe_close(stderr)
+    safe_close(handle)
+    if next(chunks) == nil then
+      return
+    end
+
+    local data = table.concat(chunks, '')
+    chunks = vim.json.decode(data)
+    local workspace_root = chunks and chunks['workspace_root'] or nil
+    print(workspace_root, 'on_exit', coroutine.status(co))
+    coroutine.resume(co, workspace_root)
+  end)
+
+  stdout:read_start(function(err, data)
+    assert(not err)
+    if data then
+      chunks[#chunks + 1] = data
+    end
+  end)
+
+  stderr:read_start(function(err, data)
+    assert(not err)
+    if data then
+      vim.notify(string.format('[lspconfig] cmd (%q) failed:\n%s', table.concat(args, ' '), data), vim.log.levels.WARN)
+    end
+  end)
+  print 'in dir'
+  return coroutine.yield()
+end
+
 return {
   default_config = {
     cmd = { 'rust-analyzer' },
     filetypes = { 'rust' },
     root_dir = function(fname)
       local cargo_crate_dir = util.root_pattern 'Cargo.toml'(fname)
-      local cmd = { 'cargo', 'metadata', '--no-deps', '--format-version', '1' }
+      local args = { 'metadata', '--no-deps', '--format-version', '1' }
       if cargo_crate_dir ~= nil then
-        cmd[#cmd + 1] = '--manifest-path'
-        cmd[#cmd + 1] = util.path.join(cargo_crate_dir, 'Cargo.toml')
+        args[#args + 1] = '--manifest-path'
+        args[#args + 1] = util.path.join(cargo_crate_dir, 'Cargo.toml')
       end
-      local cargo_metadata = ''
-      local cargo_metadata_err = ''
-      local cm = vim.fn.jobstart(cmd, {
-        on_stdout = function(_, d, _)
-          cargo_metadata = table.concat(d, '\n')
-        end,
-        on_stderr = function(_, d, _)
-          cargo_metadata_err = table.concat(d, '\n')
-        end,
-        stdout_buffered = true,
-        stderr_buffered = true,
-      })
-      if cm > 0 then
-        cm = vim.fn.jobwait({ cm })[1]
-      else
-        cm = -1
+
+      local dir = function()
+        return get_workspace_dir(args)
       end
-      local cargo_workspace_dir = nil
-      if cm == 0 then
-        cargo_workspace_dir = vim.json.decode(cargo_metadata)['workspace_root']
-        if cargo_workspace_dir ~= nil then
-          cargo_workspace_dir = util.path.sanitize(cargo_workspace_dir)
-        end
-      else
-        vim.notify(
-          string.format('[lspconfig] cmd (%q) failed:\n%s', table.concat(cmd, ' '), cargo_metadata_err),
-          vim.log.levels.WARN
-        )
+
+      local co = coroutine.create(function()
+        print 'here'
+        local result = dir()
+        print 'here1'
+        coroutine.yield(result)
+      end)
+      local _, cargo_workspace_root = coroutine.resume(co)
+
+      print(cargo_workspace_root, coroutine.status(co), co)
+
+      if cargo_workspace_root then
+        cargo_workspace_root = util.path.sanitize(cargo_workspace_root)
       end
-      return cargo_workspace_dir
+
+      print 'end'
+      return cargo_workspace_root
         or cargo_crate_dir
         or util.root_pattern 'rust-project.json'(fname)
         or util.find_git_ancestor(fname)
