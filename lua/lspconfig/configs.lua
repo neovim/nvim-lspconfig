f4ade309939a4e9198a9557855bc2afaa377cfad
Raphael <glephunter@gmail.com>
2023-05-05 17:22:37 +0800

    fix: use suggestion
    Co-authored-by: ii14 <59243201+ii14@users.noreply.github.com>

--- lua/lspconfig/configs.lua
+++ lua/lspconfig/configs.lua
@@ -270,6 +270,9 @@ function configs.__newindex(t, config_name, config_def)
         local root_dir
         if get_root_dir then
           root_dir = get_root_dir(buf_path, bufnr)
+          if not api.nvim_buf_is_valid(bufnr) then
+            return
+          end
         end
 
         if root_dir then
