664de12a7816dfa348bb475607edfa4fe0122025
An Hoang <xuanan2001@pm.me>
2023-06-05 07:48:56 +0700

    fix(astro): correct the typescript.tsdk path (#2649)
    It's supposed to be a path to the TypeScript server folder, not a file.

--- lua/lspconfig/server_configurations/astro.lua
+++ lua/lspconfig/server_configurations/astro.lua
@@ -2,8 +2,7 @@ local util = require 'lspconfig.util'
 
 local function get_typescript_server_path(root_dir)
   local project_root = util.find_node_modules_ancestor(root_dir)
-  return project_root and (util.path.join(project_root, 'node_modules', 'typescript', 'lib', 'tsserverlibrary.js'))
-    or ''
+  return project_root and (util.path.join(project_root, 'node_modules', 'typescript', 'lib')) or ''
 end
 
 return {
