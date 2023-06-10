179803f537807fff5b5961a2b3efd9e7ddac54a2
Ilia Choly <ilia.choly@gmail.com>
2023-06-09 21:00:04 -0400

    feat(gopls): attach previous lsp instance for GOMODCACHE (#2661)

--- lua/lspconfig/server_configurations/gopls.lua
+++ lua/lspconfig/server_configurations/gopls.lua
@@ -1,10 +1,19 @@
 local util = require 'lspconfig.util'
 
+local mod_cache = vim.trim(vim.fn.system 'go env GOMODCACHE')
+
 return {
   default_config = {
     cmd = { 'gopls' },
     filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
     root_dir = function(fname)
+      -- see: https://github.com/neovim/nvim-lspconfig/issues/804
+      if fname:sub(1, #mod_cache) == mod_cache then
+        local clients = vim.lsp.get_active_clients { name = 'gopls' }
+        if #clients > 0 then
+          return clients[#clients].config.root_dir
+        end
+      end
       return util.root_pattern('go.work', 'go.mod', '.git')(fname)
     end,
     single_file_support = true,
