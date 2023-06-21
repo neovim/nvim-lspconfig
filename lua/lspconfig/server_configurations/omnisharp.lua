f6336e240cafa6fecb139eed443b315fd320f906
Miles Ramage <53498605+JustALawnGnome7@users.noreply.github.com>
2023-06-21 12:36:35 -0500

    fix(omnisharp): reinitialize cmd on new config
    Reinitialize `new_config.cmd` when `on_new_config` gets called, as opposed to erroneously appending argument duplicates to the current Omnisharp command line. Fixes #2392.

--- lua/lspconfig/server_configurations/omnisharp.lua
+++ lua/lspconfig/server_configurations/omnisharp.lua
@@ -42,6 +42,7 @@ return {
       return util.root_pattern '*.sln'(fname) or util.root_pattern '*.csproj'(fname)
     end,
     on_new_config = function(new_config, new_root_dir)
+      new_config.cmd = { 'omnisharp' }
       table.insert(new_config.cmd, '-z') -- https://github.com/OmniSharp/omnisharp-vscode/pull/4300
       vim.list_extend(new_config.cmd, { '-s', new_root_dir })
       vim.list_extend(new_config.cmd, { '--hostPID', tostring(vim.fn.getpid()) })
