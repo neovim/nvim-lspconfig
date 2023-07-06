38613c4e0d7c2c1bc2a9d18212de555b99ed36be
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-07-05 20:21:25 -0400

    feat(emmet_language_server): add emmet_language_server

---
+++ lua/lspconfig/server_configurations/emmet_language_server.lua
@@ -0,0 +1,38 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'emmet-language-server', '--stdio' },
+    filetypes = {
+      'astro',
+      'css',
+      'eruby',
+      'html',
+      'htmldjango',
+      'javascriptreact',
+      'less',
+      'pug',
+      'sass',
+      'scss',
+      'svelte',
+      'typescriptreact',
+      'vue',
+    },
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+https://github.com/olrtg/emmet-language-server
+
+Package can be installed via `npm`:
+```sh
+npm install -g @olrtg/emmet-language-server
+```
+]],
+    default_config = {
+      root_dir = 'git root',
+      single_file_support = true,
+    },
+  },
+}
