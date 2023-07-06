9442842b8e42dc402c8ea76a1569a8234ba6b527
Kevin Liao <q85292542000@gmail.com>
2023-07-06 17:39:09 +0800

    docs: fix documentation for missing commas (#2703)

--- lua/lspconfig/server_configurations/yamlls.lua
+++ lua/lspconfig/server_configurations/yamlls.lua
@@ -42,9 +42,9 @@ require('lspconfig').yamlls.setup {
     yaml = {
       ... -- other settings. note this overrides the lspconfig defaults.
       schemas = {
-        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
-        ["../path/relative/to/file.yml"] = "/.github/workflows/*"
-        ["/path/from/root/of/project"] = "/.github/workflows/*"
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+        ["../path/relative/to/file.yml"] = "/.github/workflows/*",
+        ["/path/from/root/of/project"] = "/.github/workflows/*",
       },
     },
   }
