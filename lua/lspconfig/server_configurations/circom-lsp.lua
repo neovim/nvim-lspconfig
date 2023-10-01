bcaa080a8be117fbe59d56e746c3764d7bcbb80e
Gokul2003g <gokul2003g@gmail.com>
2023-10-01 22:15:38 +0530

    feat: add circom-lsp support

---
+++ lua/lspconfig/server_configurations/circom-lsp.lua
@@ -0,0 +1,27 @@
+local util = require 'lspconfig.util'
+
+local root_files = {
+  'package.json',
+  'tsconfig.json',
+  'pyproject.toml',
+  'Cargo.toml',
+}
+
+return {
+  default_config = {
+    cmd = { 'circom-lsp' },
+    filetypes = { 'circom' },
+    root_dir = function()
+      local root = util.root_pattern(root_files)(vim.fn.expand('%:p'))
+      return root or vim.api.nvim_buf_get_name(0)
+    end,
+  },
+  docs = {
+    description = [[
+[Circom Language Server](https://github.com/rubydusa/circom-lsp)
+
+`circom-lsp`, the language server for the Circom language.
+    ]],
+  },
+}
+
