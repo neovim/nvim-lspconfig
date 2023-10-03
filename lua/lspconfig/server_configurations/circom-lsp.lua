15bb05781745a88b7c029e887f5dd5eeeadca850
Gokul2003g <gokul2003g@gmail.com>
2023-10-03 14:57:47 +0530

    feat: Enhance Circom Language Server Configuration

--- lua/lspconfig/server_configurations/circom-lsp.lua
+++ lua/lspconfig/server_configurations/circom-lsp.lua
@@ -1,7 +1,10 @@
+local util = require 'lspconfig/util'
+
 return {
   default_config = {
     cmd = { 'circom-lsp' },
     filetypes = { 'circom' },
+    root_dir = util.find_git_ancestor,
     single_file_support = true,
   },
   docs = {
