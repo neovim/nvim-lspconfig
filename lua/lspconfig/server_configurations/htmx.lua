28803c3b2f561b06dffe89adfafaa01a8acded21
OZoneGuy <oalkersh@protonmail.com>
2023-10-15 00:57:23 -0400

    style: fixing lint issues

--- lua/lspconfig/server_configurations/htmx.lua
+++ lua/lspconfig/server_configurations/htmx.lua
@@ -1,14 +1,14 @@
 local util = require 'lspconfig.util'
 -- NOTE: the binary can be installed in custom a location. The user should
 -- change the cmd to match the installation path
-local cargo_home = os.getenv("CARGO_INSTALL_ROOT") or os.getenv("CARGO_HOME") or os.getenv("HOME") .. "/.cargo"
+local cargo_home = os.getenv 'CARGO_INSTALL_ROOT' or os.getenv 'CARGO_HOME' or os.getenv 'HOME' .. '/.cargo'
 
 return {
   default_config = {
-    cmd = { cargo_home .. "/bin/htmx-lsp" },
-    filetypes = { "html" },
+    cmd = { cargo_home .. '/bin/htmx-lsp' },
+    filetypes = { 'html' },
     root_dir = function(fname)
-      return util.find_git_ancestor(fname) or vim.fn.expand("%:p:h") or vim.loop.os_homedir()
+      return util.find_git_ancestor(fname) or vim.fn.expand '%:p:h' or vim.loop.os_homedir()
     end,
     settings = {},
   },
