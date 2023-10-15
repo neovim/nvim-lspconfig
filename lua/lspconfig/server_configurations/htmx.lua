3ac4a392083f64b3697201441594c1711bd4bf07
OZoneGuy <oalkersh@protonmail.com>
2023-10-15 01:02:23 -0400

    fix: calling the binary without the path
    Assuming the binary is in the user's path

--- lua/lspconfig/server_configurations/htmx.lua
+++ lua/lspconfig/server_configurations/htmx.lua
@@ -1,11 +1,8 @@
 local util = require 'lspconfig.util'
--- NOTE: the binary can be installed in custom a location. The user should
--- change the cmd to match the installation path
-local cargo_home = os.getenv 'CARGO_INSTALL_ROOT' or os.getenv 'CARGO_HOME' or os.getenv 'HOME' .. '/.cargo'
 
 return {
   default_config = {
-    cmd = { cargo_home .. '/bin/htmx-lsp' },
+    cmd = { 'htmx-lsp' },
     filetypes = { 'html' },
     root_dir = function(fname)
       return util.find_git_ancestor(fname) or vim.fn.expand '%:p:h' or vim.loop.os_homedir()
