6c9e0f8c8377e1267cd319d3265f162c1819eaaa
Emilio Ziniades <emilioziniades@protonmail.com>
2023-11-13 22:04:42 +0200

    fix: util.root_pattern prioritises pattern order
    Instead of traversing the filesystem upwards once and
    returning the first match of all the patterns, it traverses
    the filesystem upwards once for each pattern. This means
    that the order of the patterns provided matters, and the
    highest priority patterns should be put first. Also updated
    corresponding tests.

--- lua/lspconfig/util.lua
+++ lua/lspconfig/util.lua
@@ -250,18 +250,21 @@ end
 
 function M.root_pattern(...)
   local patterns = vim.tbl_flatten { ... }
-  local function matcher(path)
-    for _, pattern in ipairs(patterns) do
-      for _, p in ipairs(vim.fn.glob(M.path.join(M.path.escape_wildcards(path), pattern), true, true)) do
-        if M.path.exists(p) then
-          return path
-        end
-      end
-    end
-  end
   return function(startpath)
     startpath = M.strip_archive_subpath(startpath)
-    return M.search_ancestors(startpath, matcher)
+    for _, pattern in ipairs(patterns) do
+      local match = M.search_ancestors(startpath, function(path)
+        for _, p in ipairs(vim.fn.glob(M.path.join(M.path.escape_wildcards(path), pattern), true, true)) do
+          if M.path.exists(p) then
+            return path
+          end
+        end
+      end)
+
+      if match ~= nil then
+        return match
+      end
+    end
   end
 end
 
