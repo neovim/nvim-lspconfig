63c48dca174deead252d9735522c66ca576ee803
Pedro Castro <aspeddro@gmail.com>
2023-11-13 21:31:05 -0300

    feat(rescriptls): add init_options

--- lua/lspconfig/server_configurations/rescriptls.lua
+++ lua/lspconfig/server_configurations/rescriptls.lua
@@ -6,14 +6,23 @@ return {
     filetypes = { 'rescript' },
     root_dir = util.root_pattern('bsconfig.json', 'rescript.json', '.git'),
     settings = {},
+    init_options = {
+      extensionConfiguration = {
+        askToStartBuild = false,
+      },
+    },
   },
   docs = {
     description = [[
 https://github.com/rescript-lang/rescript-vscode/tree/master/server
+
 ReScript Language Server can be installed via npm:
 ```sh
 npm install -g @rescript/language-server
 ```
+
+See the init_options supported (see https://github.com/rescript-lang/rescript-vscode/tree/master/server/config.md):
 ]],
+    root_dir = [[root_pattern('bsconfig.json', 'rescript.json', '.git')]],
   },
 }
