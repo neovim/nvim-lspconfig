9c9cbfbd02783215d82a7615bd62052d24414310
Jonathan Slenders <jonathan@slenders.be>
2023-11-22 13:14:58 +0100

    feat: add typos-lsp server (#2886)
    * feat: add typos-lsp server configuration
    
    * fixup!
    
    * fixup!
    
    * fixup!
    
    ---------
    
    Co-authored-by: Raphael <glephunter@gmail.com>

---
+++ lua/lspconfig/server_configurations/typos_lsp.lua
@@ -0,0 +1,21 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'typos-lsp' },
+    filetypes = { '*' },
+    root_dir = util.root_pattern('pyproject.toml', '.git'),
+    single_file_support = true,
+    settings = {},
+  },
+  docs = {
+    description = [[
+https://github.com/crate-ci/typos
+https://github.com/tekumara/typos-vscode
+
+A Language Server Protocol implementation for Typos, a low false-positive
+source code spell checker, written in Rust. Download it from the releases page
+on GitHub: https://github.com/tekumara/typos-vscode/releases
+    ]],
+  },
+}
