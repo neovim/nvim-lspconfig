53ff4f7358bcace4355edc2855b9671a38e82cd2
Marc Busqué <marc@lamarciana.com>
2023-12-06 09:57:05 +0100

    fix(rubocop): don't auto-detect bundler, to avoid surprising configuration behavior (#2923)
    Ref. #2716. The referenced PR changes got somehow lost when it came to the removal of the `on_new_config` function.

--- lua/lspconfig/server_configurations/rubocop.lua
+++ lua/lspconfig/server_configurations/rubocop.lua
@@ -6,13 +6,6 @@ return {
     filetypes = { 'ruby' },
     root_dir = util.root_pattern('Gemfile', '.git'),
   },
-  on_new_config = function(config, root_dir)
-    -- prepend 'bundle exec' to cmd if bundler is being used
-    local gemfile_path = util.path.join(root_dir, 'Gemfile')
-    if util.path.exists(gemfile_path) then
-      config.cmd = { 'bundle', 'exec', unpack(config.cmd) }
-    end
-  end,
   docs = {
     description = [[
 https://github.com/rubocop/rubocop
