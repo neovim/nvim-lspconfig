9ebfa1e5260ecd6c06c56518a8869a6ba709fc7d
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2024-01-11 19:07:56 +0100

    fix(docs): Add note about filetype for docker-compose

--- lua/lspconfig/server_configurations/docker_compose_language_service.lua
+++ lua/lspconfig/server_configurations/docker_compose_language_service.lua
@@ -17,6 +17,8 @@ This project contains a language service for Docker Compose.
 ```sh
 npm install @microsoft/compose-language-service
 ```
+
+Note: If the docker-compose-langserver doesn't startup when entering a `docker-compose.yaml` file, make sure that the filetype is `yaml.docker-compose`. You can set with: `:set filetype=yaml.docker-compose`.
 ]],
     default_config = {
       root_dir = [[root_pattern("docker-compose.yaml")]],
