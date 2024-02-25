547d5996594daeedd2aa993886475481d36e2f9a
St8Razor <themist3@gmail.com>
2024-02-24 22:56:37 -0500

    Update delphi_ls.lua, formatting replace tab with spaces
    feat: add delphi lsp support

--- lua/lspconfig/server_configurations/delphi_ls.lua
+++ lua/lspconfig/server_configurations/delphi_ls.lua
@@ -1,25 +1,25 @@
 local util = require("lspconfig.util")
 
 return {
-	default_config = {
-		cmd = { "DelphiLSP.exe" },
-		filetypes = { "pascal" },
-		root_dir = util.root_pattern("*.dpr"),
-		single_file_support = true,
-		settings = {
-			--look for *.delphilsp.json - Delphi lsp config file
-			settingsFile = vim.fs.find(function(name)
-				return name:match(".*%.delphilsp.json$")
-			end, { limit = math.huge, type = "file", upward = true })[1],
-		},
-	},
-	docs = {
-		description = [[
+  default_config = {
+    cmd = { "DelphiLSP.exe" },
+    filetypes = { "pascal" },
+    root_dir = util.root_pattern("*.dpr"),
+    single_file_support = true,
+    settings = {
+      --look for *.delphilsp.json - Delphi lsp config file
+      settingsFile = vim.fs.find(function(name)
+        return name:match(".*%.delphilsp.json$")
+      end, { limit = math.huge, type = "file", upward = true })[1],
+    },
+  },
+  docs = {
+    description = [[
 Language server for Delphi from Embarcadero.
 https://marketplace.visualstudio.com/items?itemName=EmbarcaderoTechnologies.delphilsp
 
 Note, the '*.delphilsp.json' file is required, more details at:
 https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Using_DelphiLSP_Code_Insight_with_Other_Editors
 ]],
-	},
+  },
 }
