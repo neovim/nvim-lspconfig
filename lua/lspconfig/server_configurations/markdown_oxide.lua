990847175da282b1d68d6940067a478e3ae70231
Felix Zeller <felixazeller@gmail.com>
2024-02-25 08:28:51 -0500

    fix: style

--- lua/lspconfig/server_configurations/markdown_oxide.lua
+++ lua/lspconfig/server_configurations/markdown_oxide.lua
@@ -13,6 +13,6 @@ https://github.com/Feel-ix-343/markdown-oxide
 Markdown language server with advanced linking support made to be completely compatible with Obsidian; An Obsidian Language Server
 
 Check the readme to see how to get nvim-cmp properly working.
-    ]]
-  }
+    ]],
+  },
 }
