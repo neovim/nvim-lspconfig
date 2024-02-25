d0a1ac1bed658cf6fbc0cb32af2ac355d6944afc
Felix Zeller <felixazeller@gmail.com>
2024-02-25 08:12:02 -0500

    feat: add markdown-oxide lsp

---
+++ lua/lspconfig/server_configurations/markdown_oxide.lua
@@ -0,0 +1,18 @@
+return {
+  default_config = {
+    root_dir = function()
+      return vim.fn.getcwd()
+    end, -- this is applicable because markdown vaults have no defined root files
+    filetypes = { 'markdown' },
+    cmd = { 'markdown-oxide' },
+  },
+  docs = {
+    description = [[
+https://github.com/Feel-ix-343/markdown-oxide
+
+Markdown language server with advanced linking support made to be completely compatible with Obsidian; An Obsidian Language Server
+
+Check the readme to see how to get nvim-cmp properly working.
+    ]]
+  }
+}
