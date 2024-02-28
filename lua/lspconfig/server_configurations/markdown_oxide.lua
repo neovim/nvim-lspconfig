d5886e28516d70bf940bbf0da02076705f43b46b
Felix Zeller <felixazeller@gmail.com>
2024-02-28 07:25:45 -0500

    fix: use find git ancestor
    Co-authored-by: Raphael <glephunter@gmail.com>

--- lua/lspconfig/server_configurations/markdown_oxide.lua
+++ lua/lspconfig/server_configurations/markdown_oxide.lua
@@ -2,7 +2,8 @@ local util = require 'lspconfig.util'
 
 return {
   default_config = {
-    root_dir = lspconfig.util.root_pattern('.git', vim.fn.getcwd()),
+    root_dir = util.find_git_ancestor
+    single_file_support = true,
     filetypes = { 'markdown' },
     cmd = { 'markdown-oxide' },
   },
