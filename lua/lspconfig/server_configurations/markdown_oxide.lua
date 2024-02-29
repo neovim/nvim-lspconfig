3a2e38a4b287c6c8fb66a57e27ab1c27e829d2a2
Felix Zeller <felixazeller@gmail.com>
2024-02-29 01:17:08 -0500

    Update markdown_oxide.lua
    Co-authored-by: Raphael <glephunter@gmail.com>

--- lua/lspconfig/server_configurations/markdown_oxide.lua
+++ lua/lspconfig/server_configurations/markdown_oxide.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 
 return {
   default_config = {
-    root_dir = util.find_git_ancestor
+    root_dir = util.find_git_ancestor,
     single_file_support = true,
     filetypes = { 'markdown' },
     cmd = { 'markdown-oxide' },
