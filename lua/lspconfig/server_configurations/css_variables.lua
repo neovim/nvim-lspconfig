1b1a4dcc34a53283a1a8269909628e440a8a4f51
Rogin Farrer <rfarrer@wayfair.com>
2024-03-03 14:22:50 -0800

    fix: CSS Variables LSP default settings

--- lua/lspconfig/server_configurations/css_variables.lua
+++ lua/lspconfig/server_configurations/css_variables.lua
@@ -5,6 +5,27 @@ return {
     cmd = { 'css-variables-language-server', '--stdio' },
     filetypes = { 'css', 'scss', 'less' },
     root_dir = util.root_pattern('package.json', '.git'),
+    -- Same as inlined defaults that don't seem to work without hardcoding them in the lua config
+    -- https://github.com/vunguyentuan/vscode-css-variables/blob/763a564df763f17aceb5f3d6070e0b444a2f47ff/packages/css-variables-language-server/src/CSSVariableManager.ts#L31-L50
+    settings = {
+      cssVariables = {
+        lookupFiles = { '**/*.less', '**/*.scss', '**/*.sass', '**/*.css' },
+        blacklistFolders = {
+          '**/.cache',
+          '**/.DS_Store',
+          '**/.git',
+          '**/.hg',
+          '**/.next',
+          '**/.svn',
+          '**/bower_components',
+          '**/CVS',
+          '**/dist',
+          '**/node_modules',
+          '**/tests',
+          '**/tmp',
+        },
+      },
+    },
   },
   docs = {
     description = [[
@@ -22,6 +43,27 @@ npm i -g css-variables-language-server
 ]],
     default_config = {
       root_dir = [[root_pattern("package.json", ".git") or bufdir]],
+      settings = [[
+settings = {
+  cssVariables = {
+    lookupFiles = { '**/*.less', '**/*.scss', '**/*.sass', '**/*.css' },
+    blacklistFolders = {
+      '**/.cache',
+      '**/.DS_Store',
+      '**/.git',
+      '**/.hg',
+      '**/.next',
+      '**/.svn',
+      '**/bower_components',
+      '**/CVS',
+      '**/dist',
+      '**/node_modules',
+      '**/tests',
+      '**/tmp',
+    },
+  },
+},
+      ]],
     },
   },
 }
