da8d3d150c8222d488faa5f344652129bced554d
Aldric.li <31800073+Parsifa1@users.noreply.github.com>
2024-03-16 17:27:44 +0800

    feat: Add  tinymist

---
+++ lua/lspconfig/server_configurations/tinymist.lua
@@ -0,0 +1,21 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'tinymist' },
+    filetypes = { 'typst' },
+    root_dir = function()
+      return vim.fn.getcwd()
+    end,
+    settings = {
+      exportPdf = "never", -- doesn't export PDF by default
+    },
+  },
+  docs = {
+    description = [[
+https://github.com/Myriad-Dreamin/tinymist
+
+An integrated language service for Typst [taɪpst]. You can also call it "微霭" [wēi ǎi] in Chinese.
+    ]],
+  },
+}
