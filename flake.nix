6ac628b1ed5c19e303edfaa95b1859181fcda313
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-04-15 22:20:34 +0530

    Minor cleanup of flake.nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,21 @@
 {
   description = "Quickstart configurations for the Nvim LSP client";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       rec {
-        devShell = pkgs.mkShell {
+        devShell = with pkgs; mkShell {
           buildInputs = [
-            pkgs.stylua
-            pkgs.luaPackages.luacheck
-            pkgs.luajitPackages.vusted
-            pkgs.selene
+            stylua
+            luaPackages.luacheck
+            luajitPackages.vusted
+            selene
           ];
         };
       }
