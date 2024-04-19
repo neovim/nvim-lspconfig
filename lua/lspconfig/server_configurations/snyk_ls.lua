9b61b0b08955ea205f071ff139e69894e6a0dd38
Peter Benjamin <135638833+pbnj-dragon@users.noreply.github.com>
2024-04-18 23:10:17 -0700

    fix(snyk_ls): update root_dir & supported filetypes (#3112)

--- lua/lspconfig/server_configurations/snyk_ls.lua
+++ lua/lspconfig/server_configurations/snyk_ls.lua
@@ -3,7 +3,8 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { 'snyk-ls' },
-    root_dir = util.root_pattern '.git',
+    root_dir = util.root_pattern('.git', '.snyk'),
+    filetypes = { 'go', 'gomod', 'javascript', 'typescript', 'json', 'python', 'requirements', 'helm', 'yaml' },
     single_file_support = true,
     settings = {},
     init_options = {
@@ -17,7 +18,7 @@ https://github.com/snyk/snyk-ls
 LSP for Snyk Open Source, Snyk Infrastructure as Code, and Snyk Code.
 ]],
     default_config = {
-      root_dir = [[root_pattern(".git")]],
+      root_dir = [[root_pattern(".git", ".snyk")]],
       init_options = 'Configuration from https://github.com/snyk/snyk-ls#configuration-1',
     },
   },
