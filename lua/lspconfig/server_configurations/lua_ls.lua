a8b027ba9179e146a37c3362f8c269be7caf3267
Michael Härtl <haertl.mike@gmail.com>
2024-05-02 19:13:10 +0200

    docs(lua_ls) Fix broken example setup when workspace_folders are not used

--- lua/lspconfig/server_configurations/lua_ls.lua
+++ lua/lspconfig/server_configurations/lua_ls.lua
@@ -45,9 +45,11 @@ settings.
 ```lua
 require'lspconfig'.lua_ls.setup {
   on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-      return
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+        return
+      end
     end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
