b2ad2d48ceea17cc8326842590bec1b12d590a6c
Felix Zeller <felixazeller@gmail.com>
2024-05-19 19:34:42 -0400

    feat: add daily note commands for markdown-oxide

--- lua/lspconfig/server_configurations/markdown_oxide.lua
+++ lua/lspconfig/server_configurations/markdown_oxide.lua
@@ -19,4 +19,25 @@ system for software enthusiasts.
 Check the readme to see how to properly setup nvim-cmp.
     ]],
   },
+  commands = {
+    Today = {
+      function()
+        vim.lsp.buf.execute_command({command="jump", arguments={"today"}})
+      end,
+      description = "Open today's daily note"
+    },
+    Tomorrow = {
+      function()
+        vim.lsp.buf.execute_command({command="jump", arguments={"tomorrow"}})
+      end,
+      description = "Open tomorrow's daily note"
+    },
+    Yesterday = {
+      function()
+        vim.lsp.buf.execute_command({command="jump", arguments={"yesterday"}})
+      end,
+      description = "Open yesterday's daily note"
+    },
+  }
+}
 }
