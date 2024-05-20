9bda20fb967075355f253911bc066a8b5a03c77e
Felix Zeller <felixazeller@gmail.com>
2024-05-20 01:57:39 -0400

    feat: add daily note commands for markdown-oxide (#3164)
    * feat: add daily note commands for markdown-oxide
    
    * fix: update description
    
    * fix: syntax
    
    * fix: formatting
    
    * fix: formatting
    
    * fix: formatting

--- lua/lspconfig/server_configurations/markdown_oxide.lua
+++ lua/lspconfig/server_configurations/markdown_oxide.lua
@@ -11,12 +11,32 @@ return {
     description = [[
 https://github.com/Feel-ix-343/markdown-oxide
 
-Let's record your consciousness! Bring your own text editor!
-Implemented as a language server compatible with any text
-editor, Markdown-Oxide is attempting to be the best PKM
-system for software enthusiasts.
+Editor Agnostic PKM: you bring the text editor and we
+bring the PKM.
 
-Check the readme to see how to properly setup nvim-cmp.
+Inspired by and compatible with Obsidian.
+
+Check the readme to see how to properly setup.
     ]],
+  },
+  commands = {
+    Today = {
+      function()
+        vim.lsp.buf.execute_command { command = 'jump', arguments = { 'today' } }
+      end,
+      description = "Open today's daily note",
+    },
+    Tomorrow = {
+      function()
+        vim.lsp.buf.execute_command { command = 'jump', arguments = { 'tomorrow' } }
+      end,
+      description = "Open tomorrow's daily note",
+    },
+    Yesterday = {
+      function()
+        vim.lsp.buf.execute_command { command = 'jump', arguments = { 'yesterday' } }
+      end,
+      description = "Open yesterday's daily note",
+    },
   },
 }
