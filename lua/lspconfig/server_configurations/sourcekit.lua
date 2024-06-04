c02275af70a112194c85c378332a7de859faeec8
Gonzalo <gonzalorehu@gmail.com>
2024-06-04 14:56:46 -0500

    fix(sourcekit-lsp): use git as last resort for sourcekit-lsp

--- lua/lspconfig/server_configurations/sourcekit.lua
+++ lua/lspconfig/server_configurations/sourcekit.lua
@@ -7,9 +7,9 @@ return {
     root_dir = function(filename, _)
       return util.root_pattern 'buildServer.json'(filename)
         or util.root_pattern('*.xcodeproj', '*.xcworkspace')(filename)
-        or util.find_git_ancestor(filename)
         -- better to keep it at the end, because some modularized apps contain multiple Package.swift files
         or util.root_pattern('compile_commands.json', 'Package.swift')(filename)
+        or util.find_git_ancestor(filename)
     end,
   },
   docs = {
