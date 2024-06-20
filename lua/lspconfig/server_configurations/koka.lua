1198e89181f95159c45b1ad83a0fe6161361cd2d
mangoiv <contact@mangoiv.com>
2024-06-20 13:44:38 +0200

    fix(koka): fix koka language server config

--- lua/lspconfig/server_configurations/koka.lua
+++ lua/lspconfig/server_configurations/koka.lua
@@ -1,36 +1,21 @@
 local util = require 'lspconfig.util'
 
-local root_files = {}
-
-local default_capabilities = {
-  textDocument = {
-    completion = {
-      editsNearCursor = true,
-    },
-  },
-  offsetEncoding = { 'utf-8' },
-}
-
 return {
   default_config = {
-    cmd = { 'koka', '--language-server' },
-    filetypes = { 'kk' },
-    root_dir = function(fname)
-      return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
-    end,
+    cmd = { 'koka', '--language-server', '--lsstdio' },
+    filetypes = { 'koka' },
     single_file_support = true,
-    capabilities = default_capabilities,
+    root_dir = util.find_git_ancestor,
   },
-  commands = {},
+
   docs = {
     description = [[
     https://koka-lang.github.io/koka/doc/index.html
 Koka is a functional programming language with effect types and handlers.
     ]],
     default_config = {
-      root_dir = [[
-      ]],
-      capabilities = [[default capabilities, with offsetEncoding utf-8]],
+      root_dir = [[git directory]],
+      capabilities = [[default capabilities]],
     },
   },
 }
