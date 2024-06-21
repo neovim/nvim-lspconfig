d9025737339f2d68f23222e7c124156321736254
Micah Halter <micah@mehalter.com>
2024-06-21 09:16:22 -0400

    fix(hyprls): autostart with hyprlang filetype

--- lua/lspconfig/server_configurations/hyprls.lua
+++ lua/lspconfig/server_configurations/hyprls.lua
@@ -3,7 +3,7 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { 'hyprls', '--stdio' },
-    filetypes = { '*.hl', 'hypr*.conf', '.config/hypr/*.conf' },
+    filetypes = { 'hyprlang', '*.hl', 'hypr*.conf', '.config/hypr/*.conf' },
     root_dir = util.find_git_ancestor,
     single_file_support = true,
   },
