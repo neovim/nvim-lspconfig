9b991ddfa5b617af700f4d282990638a63add752
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2024-08-04 22:13:19 +0200

    feat: implement ZkList command for zk
    Surfaces the `zk.list` LSP command in the same way as `zk.new` and
    `zk.index`.

--- lua/lspconfig/server_configurations/zk.lua
+++ lua/lspconfig/server_configurations/zk.lua
@@ -1,5 +1,13 @@
 local util = require 'lspconfig.util'
 
+local function find_zk_root(startpath)
+  for dir in vim.fs.parents(startpath) do
+    if vim.fn.isdirectory(vim.fs.joinpath(dir, '.zk')) == 1 then
+      return dir
+    end
+  end
+end
+
 return {
   default_config = {
     cmd = { 'zk', 'lsp' },
@@ -15,6 +23,29 @@ return {
         }
       end,
       description = 'ZkIndex',
+    },
+    ZkList = {
+      function()
+        local bufpath = vim.api.nvim_buf_get_name(0)
+        local root = find_zk_root(bufpath)
+
+        vim.lsp.buf_request(0, 'workspace/executeCommand', {
+          command = 'zk.list',
+          arguments = { root, { select = { 'path' } } },
+        }, function(_, result, _, _)
+          if not result then
+            return
+          end
+          local paths = vim.tbl_map(function(item)
+            return item.path
+          end, result)
+          vim.ui.select(paths, {}, function(choice)
+            vim.cmd('edit ' .. choice)
+          end)
+        end)
+      end,
+
+      description = 'ZkList',
     },
     ZkNew = {
       function(...)
