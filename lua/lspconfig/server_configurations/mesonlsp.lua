94f0fca3153cd2adfacfd9f2e5f4ef0c0122545c
George Thomadakis <gthomadakis@protonmail.com>
2024-08-10 09:12:20 +0300

    fix(mesonlsp): update root directory pattern (#3265)
    Add pattern for meson.build files in the 'root_pattern' functions
    of the 'default_config' and 'doc' tables for the 'mesonlsp'
    and 'swift_mesonls' servers.
    
    Signed-off-by: George Thomadakis <gthomadakis@protonmail.com>

--- lua/lspconfig/server_configurations/mesonlsp.lua
+++ lua/lspconfig/server_configurations/mesonlsp.lua
@@ -4,7 +4,7 @@ return {
   default_config = {
     cmd = { 'mesonlsp', '--lsp' },
     filetypes = { 'meson' },
-    root_dir = util.root_pattern('meson_options.txt', 'meson.options', '.git'),
+    root_dir = util.root_pattern('meson.build', 'meson_options.txt', 'meson.options', '.git'),
   },
   docs = {
     description = [[
@@ -13,7 +13,7 @@ https://github.com/JCWasmx86/mesonlsp
 An unofficial, unendorsed language server for meson written in C++
 ]],
     default_config = {
-      root_dir = [[util.root_pattern("meson_options.txt", "meson.options", ".git")]],
+      root_dir = [[util.root_pattern("meson.build", "meson_options.txt", "meson.options", ".git")]],
     },
   },
 }
