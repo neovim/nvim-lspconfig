9ba0a25402e764f3c69ef8d79352044fbd27167a
Nicholas Ciechanowski <nicholasaciechanowski@gmail.com>
2024-08-14 11:48:26 +0000

    feat(htmx): add more filetypes

--- lua/lspconfig/server_configurations/htmx.lua
+++ lua/lspconfig/server_configurations/htmx.lua
@@ -3,7 +3,54 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { 'htmx-lsp' },
-    filetypes = { 'html', 'templ' },
+    filetypes = { -- filetypes copied and adjusted from tailwindcss-intellisense
+      -- html
+      'aspnetcorerazor',
+      'astro',
+      'astro-markdown',
+      'blade',
+      'clojure',
+      'django-html',
+      'htmldjango',
+      'edge',
+      'eelixir', -- vim ft
+      'elixir',
+      'ejs',
+      'erb',
+      'eruby', -- vim ft
+      'gohtml',
+      'gohtmltmpl',
+      'haml',
+      'handlebars',
+      'hbs',
+      'html',
+      'htmlangular',
+      'html-eex',
+      'heex',
+      'jade',
+      'leaf',
+      'liquid',
+      'markdown',
+      'mdx',
+      'mustache',
+      'njk',
+      'nunjucks',
+      'php',
+      'razor',
+      'slim',
+      'twig',
+      -- js
+      'javascript',
+      'javascriptreact',
+      'reason',
+      'rescript',
+      'typescript',
+      'typescriptreact',
+      -- mixed
+      'vue',
+      'svelte',
+      'templ',
+    },
     single_file_support = true,
     root_dir = function(fname)
       return util.find_git_ancestor(fname)
