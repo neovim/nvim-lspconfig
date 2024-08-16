1fe26090d22412b95f7b5c34330086bafc3be44e
Adam Daniels <adam@mediadrive.ca>
2024-08-16 02:09:17 -0400

    Revert "feat: ruby_lsp works inside eruby files (#3266)" (#3269)
    This reverts commit 4cd29ab1b326f5b04f4b982fae05934ac64c779b.

--- lua/lspconfig/server_configurations/ruby_lsp.lua
+++ lua/lspconfig/server_configurations/ruby_lsp.lua
@@ -3,7 +3,7 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { 'ruby-lsp' },
-    filetypes = { 'ruby', 'eruby' },
+    filetypes = { 'ruby' },
     root_dir = util.root_pattern('Gemfile', '.git'),
     init_options = {
       formatter = 'auto',
