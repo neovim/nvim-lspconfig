ca0d444a1f39199637a3ffab7d4bfa48a8f19f8a
ufo5260987423 <ufo5260987423@163.com>
2024-08-22 15:26:36 +0800

    fix(scheme-langserver): remove bin_name

--- lua/lspconfig/server_configurations/scheme_langserver.lua
+++ lua/lspconfig/server_configurations/scheme_langserver.lua
@@ -1,6 +1,5 @@
 local util = require 'lspconfig.util'
-local bin_name = 'scheme-langserver'
-local cmd = { bin_name, '~/.scheme-langserver.log', 'enable', 'disable' }
+local cmd = { 'scheme-langserver', '~/.scheme-langserver.log', 'enable', 'disable' }
 local root_files = {
   'Akku.manifest',
   '.git',
