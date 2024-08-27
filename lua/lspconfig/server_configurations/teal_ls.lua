a05f8d1e2c5d7a0e71e44b3a28e2b6bd311bd01b
Steve Vermeulen <sfvermeulen@gmail.com>
2024-08-27 21:07:50 +0800

    fix: tea-leaves was renamed and replaced teal-language-server (#3285)

--- lua/lspconfig/server_configurations/teal_ls.lua
+++ lua/lspconfig/server_configurations/teal_ls.lua
@@ -4,14 +4,11 @@ return {
   default_config = {
     cmd = {
       'teal-language-server',
-      -- use this to enable logging in /tmp/teal-language-server.log
-      -- "logging=on",
     },
     filetypes = {
       'teal',
-      -- "lua", -- Also works for lua, but you may get type errors that cannot be resolved within lua itself
     },
-    root_dir = util.root_pattern('tlconfig.lua', '.git'),
+    root_dir = util.root_pattern 'tlconfig.lua',
   },
   docs = {
     description = [[
@@ -19,11 +16,16 @@ https://github.com/teal-language/teal-language-server
 
 Install with:
 ```
-luarocks install --dev teal-language-server
+luarocks install teal-language-server
 ```
+
+Optional Command Args:
+* "--log-mode=by_date" - Enable logging in $HOME/.cache/teal-language-server. Log name will be date + pid of process
+* "--log-mode=by_proj_path" - Enable logging in $HOME/.cache/teal-language-server. Log name will be project path + pid of process
+* "--verbose=true" - Increases log level.  Does nothing unless log-mode is set
 ]],
     default_config = {
-      root_dir = [[root_pattern("tlconfig.lua", ".git")]],
+      root_dir = [[root_pattern("tlconfig.lua")]],
     },
   },
 }
