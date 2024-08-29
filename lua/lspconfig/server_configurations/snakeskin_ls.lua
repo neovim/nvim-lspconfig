7845af5c09e9c35c0010e414fce27087a760f7d0
Maksim Sinelnikov <sinelnikovweb@yandex-team.ru>
2024-08-29 11:13:03 +0500

    feat: add snakeskin_ls server configuration

---
+++ lua/lspconfig/server_configurations/snakeskin_ls.lua
@@ -0,0 +1,19 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { '@snakeskin/cli', 'lsp', '--stdio' },
+    filetypes = { 'ss' },
+    root_dir = util.root_pattern 'package.json',
+  },
+  docs = {
+    description = [[
+https://www.npmjs.com/package/@snakeskin/cli
+
+`snakeskin cli` can be installed via `npm`:
+```sh
+npm install -g @snakeskin/cli
+```
+]],
+  },
+}
