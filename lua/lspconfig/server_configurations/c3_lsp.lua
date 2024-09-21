149c66a3d94cc943815838ca3544d8eef2bfbabc
id3nom <45928251+id3nom@users.noreply.github.com>
2024-09-21 08:06:23 -0400

    fix(c3): update c3 root pattern (#3309)
    * fix: add C3 library support
    fix(c3_lsp): update root directory to be able to handle project in the
    following order - a C3 project 'project.json', a library '.c3l' dir,
    and ancestor 'git' repo.
    
    ---------
    
    Co-authored-by: Justin M. Keyes <justinkz@gmail.com>
    Co-authored-by: glepnir <glephunter@gmail.com>

--- lua/lspconfig/server_configurations/c3_lsp.lua
+++ lua/lspconfig/server_configurations/c3_lsp.lua
@@ -4,7 +4,7 @@ return {
   default_config = {
     cmd = { 'c3lsp' },
     root_dir = function(fname)
-      return util.root_pattern { 'project.json', '.git' }(fname)
+      return util.root_patttern { 'project.json', 'manifest.json', '.git' }(fname)
     end,
     filetypes = { 'c3', 'c3i' },
   },
@@ -13,9 +13,6 @@ return {
 https://github.com/pherrymason/c3-lsp
 
 Language Server for c3.
-    ]],
-    default_config = {
-      root_dir = [[root_pattern("project.json", ".git")]],
-    },
+]],
   },
 }
