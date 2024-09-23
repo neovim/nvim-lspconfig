75e8b331168f5ecb33720df9e74ca6544ca218a2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-23 13:01:47 +0100

    feat(unocss): update filtypes based on the official vscode extention

--- lua/lspconfig/configs/unocss.lua
+++ lua/lspconfig/configs/unocss.lua
@@ -3,13 +3,31 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { 'unocss-language-server', '--stdio' },
+    -- copied from https://github.com/unocss/unocss/blob/35297359bf61917bda499db86e3728a7ebd05d6c/packages/vscode/src/autocomplete.ts#L12-L35
     filetypes = {
+      'erb',
+      'haml',
+      'hbs',
       'html',
+      'css',
+      'postcss',
+      'javascript',
       'javascriptreact',
-      'rescript',
-      'typescriptreact',
-      'vue',
+      'markdown',
+      'ejs',
+      'php',
       'svelte',
+      'typescript',
+      'typescriptreact',
+      'vue-html',
+      'vue',
+      'sass',
+      'scss',
+      'less',
+      'stylus',
+      'astro',
+      'rescript',
+      'rust',
     },
     root_dir = function(fname)
       return util.root_pattern('unocss.config.js', 'unocss.config.ts', 'uno.config.js', 'uno.config.ts')(fname)
