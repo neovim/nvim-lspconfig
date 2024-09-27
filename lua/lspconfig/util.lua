014b33f086237f9e287fa7a4c33737fc4c850560
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-09-27 20:12:23 -0300

    fix(util.root_path): search all patterns per dir
    util.root_path used to search each pattern on all parent directories,
      with this change it will look for all patterns on each parent dir,
      thus finding closest matchers first.

--- lua/lspconfig/util.lua
+++ lua/lspconfig/util.lua
@@ -276,18 +276,18 @@ function M.root_pattern(...)
   local patterns = M.tbl_flatten { ... }
   return function(startpath)
     startpath = M.strip_archive_subpath(startpath)
-    for _, pattern in ipairs(patterns) do
-      local match = M.search_ancestors(startpath, function(path)
+    local match = M.search_ancestors(startpath, function(path)
+      for _, pattern in ipairs(patterns) do
         for _, p in ipairs(vim.fn.glob(M.path.join(M.path.escape_wildcards(path), pattern), true, true)) do
           if M.path.exists(p) then
             return path
           end
         end
-      end)
-
-      if match ~= nil then
-        return match
       end
+    end)
+
+    if match ~= nil then
+      return match
     end
   end
 end
