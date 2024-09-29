57e4e57257ee903c5875e66e75465cf750732af8
Emma <emma@guugle.com>
2024-09-28 20:40:56 -0400

    fix(fennel-ls): use closest cfg as root directory and fallback to git repo root

--- lua/lspconfig/configs/fennel_ls.lua
+++ lua/lspconfig/configs/fennel_ls.lua
@@ -5,7 +5,10 @@ return {
     cmd = { 'fennel-ls' },
     filetypes = { 'fennel' },
     root_dir = function(dir)
-      return util.find_git_ancestor(dir)
+      local has_fls_project_cfg = function(path)
+        return util.path.is_file(vim.fs.joinpath(path, 'flsproject.fnl'))
+      end
+      return util.search_ancestors(dir, has_fls_project_cfg) or vim.fs.root(0, '.git')
     end,
     settings = {},
     capabilities = {
@@ -17,6 +20,9 @@ return {
 https://sr.ht/~xerool/fennel-ls/
 
 A language server for fennel.
+
+fennel-ls is configured using the closest file to your working directory named `flsproject.fnl`.
+All fennel-ls configuration options [can be found here](https://git.sr.ht/~xerool/fennel-ls/tree/HEAD/docs/manual.md#configuration).
 ]],
   },
 }
