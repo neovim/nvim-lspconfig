a000176945990ad2e4086cc590f5ed5008084924
Justin M. Keyes <justinkz@gmail.com>
2024-10-01 13:49:16 +0200

    fix(ci): allow "vim.fs.dirname"

--- .github/ci/run_sanitizer.sh
+++ .github/ci/run_sanitizer.sh
@@ -5,7 +5,7 @@ REF_BRANCH="$1"
 PR_BRANCH="$2"
 
 # checks for added lines that contain search pattern and prints them
-SEARCH_PATTERN="(dirname|fn\.cwd)"
+SEARCH_PATTERN="(path\.dirname|fn\.cwd)"
 
 if git diff --pickaxe-all -U0 -G "${SEARCH_PATTERN}" "${REF_BRANCH}" "${PR_BRANCH}" -- '*.lua' | grep -Ev '(configs|utils)\.lua$' | grep -E "^\+.*${SEARCH_PATTERN}" ; then
   echo
