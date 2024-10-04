6a439a8c973aea47b8e24e0e6ea8614da9801e8f
Binh Tran <binhtran432k@gmail.com>
2024-10-04 13:37:55 +0700

    feat: add ungrammar-languageserver support (#3312)

---
+++ lua/lspconfig/configs/ungrammar_languageserver.lua
@@ -0,0 +1,34 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'ungrammar-languageserver', '--stdio' },
+    filetypes = { 'ungrammar' },
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+    settings = {
+      ungrammar = {
+        validate = {
+          enable = true,
+        },
+        format = {
+          enable = true,
+        },
+      },
+    },
+  },
+  docs = {
+    description = [[
+https://github.com/binhtran432k/ungrammar-language-features
+Language Server for Ungrammar.
+
+Ungrammar Language Server can be installed via npm:
+```sh
+npm i ungrammar-languageserver -g
+```
+    ]],
+    default_config = {
+      root_dir = [[util.find_git_ancestor]],
+    },
+  },
+}
