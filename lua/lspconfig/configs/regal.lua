269c596eb15d8055fafc4e0a56e35ea6b5dccbf4
Rintaro Okamura <rintaro.okamura@gmail.com>
2024-10-05 13:15:29 +0900

    feat(regal): add regal/startDebugging handler
    to handle `Debug` codelens to launch dap session
    
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- lua/lspconfig/configs/regal.lua
+++ lua/lspconfig/configs/regal.lua
@@ -8,6 +8,26 @@ return {
       return util.root_pattern '*.rego'(fname) or util.find_git_ancestor(fname)
     end,
     single_file_support = true,
+    handlers = {
+      ['regal/startDebugging'] = function(_, result)
+        if not result then
+          return nil, vim.lsp.rpc.rpc_response_error(vim.lsp.protocol.ErrorCodes.InvalidRequest)
+        end
+
+        local ok, dap = pcall(require, "dap")
+        if not ok then
+          return nil, vim.lsp.rpc.rpc_response_error(vim.lsp.protocol.ErrorCodes.InvalidRequest, "nvim-dap is not installed")
+        end
+
+        if not dap.session() == nil then
+          return nil, vim.lsp.rpc.rpc_response_error(vim.lsp.protocol.ErrorCodes.InvalidRequest, "active debug session exists")
+        end
+
+        dap.run(vim.tbl_deep_extend("force", result, { bundlePaths = {"${workspaceFolder}"}}))
+
+        return {ok = true}
+      end,
+    },
   },
   docs = {
     description = [[
