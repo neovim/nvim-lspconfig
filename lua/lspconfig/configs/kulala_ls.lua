3aca6d491eda5c23542b57c861de75b72dda2ff9
Marco Kellershoff <1384938+gorillamoe@users.noreply.github.com>
2024-10-07 07:17:44 +0200

    feat(configs): add kulala_ls (#3360)
    A minimal language server for HTTP syntax.
    
    See: https://github.com/mistweaverco/kulala-ls

---
+++ lua/lspconfig/configs/kulala_ls.lua
@@ -0,0 +1,23 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'kulala-ls', '--stdio' },
+    filetypes = { 'http' },
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+https://github.com/mistweaverco/kulala-ls
+
+A minimal language server for HTTP syntax.
+]],
+    default_config = {
+      cmd = { 'kulala-ls', '--stdio' },
+      filetypes = { 'http' },
+      root_dir = [[root_pattern('.git')]],
+      single_file_support = true,
+    },
+  },
+}
