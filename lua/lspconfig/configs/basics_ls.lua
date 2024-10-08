8516d653fbc106c89eea0c14f67ea379e01743a5
Anton Kastritskii <halloy52@gmail.com>
2024-10-09 00:02:36 +0100

    fix(basics_ls): lints

--- lua/lspconfig/configs/basics_ls.lua
+++ lua/lspconfig/configs/basics_ls.lua
@@ -5,26 +5,26 @@ return {
     settings = {
       buffer = {
         enable = true,
-        minCompletionLength = 4
+        minCompletionLength = 4,
       },
       path = {
         enable = true,
       },
       snippet = {
         enable = false,
-        sources = {}
-      }
-    }
+        sources = {},
+      },
+    },
   },
   docs = {
     description = [[
 https://github.com/antonk52/basics-language-server/
-    
+
 Buffer, path, and snippet completion
 
 ```sh
 npm install -g basics-language-server
 ```
 ]],
-  }
+  },
 }
