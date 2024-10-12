00834feb0dc55cfe04b2134a5e6d9966611f2f1d
Rachel <lhearachel@proton.me>
2024-10-11 19:20:30 -0700

    feat: poryscript-pls

---
+++ lua/lspconfig/configs/poryscript_pls.lua
@@ -0,0 +1,20 @@
+local util = require('lspconfig.util')
+
+return {
+  default_config = {
+    cmd = { 'poryscript-pls' },
+    filetypes = { 'pory' },
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+https://github.com/huderlem/poryscript-pls
+
+Language server for poryscript (a high level scripting language for GBA-era Pokémon decompilation projects)
+    ]],
+    default_config = {
+      root_dir = [[util.find_git_ancestor]],
+    },
+  },
+}
