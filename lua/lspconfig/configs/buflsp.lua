6ee2b25a08d9b1a6731bfd4084b340586b17c946
gogamid <36050790+gogamid@users.noreply.github.com>
2024-11-02 23:05:51 +0100

    feat(buf): set timeout and log-format

--- lua/lspconfig/configs/buflsp.lua
+++ lua/lspconfig/configs/buflsp.lua
@@ -1,7 +1,7 @@
 return {
   default_config = {
 
-    cmd = { 'buf', 'beta', 'lsp' },
+    cmd = { 'buf', 'beta', 'lsp', '--timeout=0', '--log-format=text' },
     filetypes = { 'proto' },
     root_dir = require('lspconfig.util').root_pattern('buf.yaml', '.git'),
   },
