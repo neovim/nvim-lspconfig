31cc46ef3e5e9a1a11df4a5a7c23ea20fcd22a9c
glepnir <glephunter@gmail.com>
2024-11-03 09:52:08 +0800

    fix(buf): update lua/lspconfig/configs/buflsp.lua

--- lua/lspconfig/configs/buflsp.lua
+++ lua/lspconfig/configs/buflsp.lua
@@ -1,6 +1,5 @@
 return {
   default_config = {
-
     cmd = { 'buf', 'beta', 'lsp', '--timeout=0', '--log-format=text' },
     filetypes = { 'proto' },
     root_dir = require('lspconfig.util').root_pattern('buf.yaml', '.git'),
