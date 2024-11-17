ddf319a03086bfae0b6a17a744bddc9674ac2824
Daniel Everts <me@deverts.co>
2024-11-17 17:55:14 +0100

    feat: add lsp for Pkl
    LSP for the Pkl configuration language

---
+++ lua/lspconfig/configs/pkl_lsp.lua
@@ -0,0 +1,23 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'java', '-jar', 'path/to/pkl-lsp.jar' },
+    filetypes = { 'pkl' },
+    root_dir = util.root_pattern('.git'),
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+https://github.com/apple/pkl-lsp.git
+
+Retrieve or build the `pkl-lsp` jar from the Git repo. You can then configure `pkl-lsp` like so:
+
+```lua
+require('lspconfig').pkl_lsp.setup{
+    cmd = { 'java', '-jar', 'path/to/pkl-lsp.jar' },
+}
+```
+    ]],
+  },
+}
