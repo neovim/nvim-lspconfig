4b5ee59b11443712415203590df588da5d1df5f2
Antonin Godard <antoningodard@pm.me>
2024-11-17 13:27:40 +0100

    feat(vale_ls): add reStructuredText to filetypes
    Vale supports parsing reStructuredText files, as documented here:
    https://vale.sh/docs/topics/scoping/#restructuredtext
    
    It uses rst2html in the background.
    
    Signed-off-by: Antonin Godard <antoningodard@pm.me>

--- lua/lspconfig/configs/vale_ls.lua
+++ lua/lspconfig/configs/vale_ls.lua
@@ -3,7 +3,7 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { 'vale-ls' },
-    filetypes = { 'markdown', 'text', 'tex' },
+    filetypes = { 'markdown', 'text', 'tex', 'rst' },
     root_dir = util.root_pattern '.vale.ini',
     single_file_support = true,
   },
