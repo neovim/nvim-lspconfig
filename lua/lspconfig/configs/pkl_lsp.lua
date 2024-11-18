b3bf71baf1a3b7eec33317604e20e5ee5b02595a
Daniel Everts <me@deverts.co>
2024-11-18 07:37:40 +0100

    style(pkl-lsp): updated path to remove unneccessary prefix

--- lua/lspconfig/configs/pkl_lsp.lua
+++ lua/lspconfig/configs/pkl_lsp.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 
 return {
   default_config = {
-    cmd = { 'java', '-jar', 'path/to/pkl-lsp.jar' },
+    cmd = { 'java', '-jar', 'pkl-lsp.jar' },
     filetypes = { 'pkl' },
     root_dir = util.root_pattern('.git'),
     single_file_support = true,
@@ -15,7 +15,7 @@ Retrieve or build the `pkl-lsp` jar from the Git repo. You can then configure `p
 
 ```lua
 require('lspconfig').pkl_lsp.setup{
-    cmd = { 'java', '-jar', 'path/to/pkl-lsp.jar' },
+    cmd = { 'java', '-jar', '/path/to/pkl-lsp.jar' },
 }
 ```
     ]],
