6f992df33fbd254177fec7d69a3ff061e9c33003
fishBone000 <FishBoneEK@outlook.com>
2024-11-18 17:08:12 +0000

    fix: add pylyzer dependenncy info (erg) (#3433)
    Problem: Pylyzer needs Erg as dependency, such info is missing in
    configs.md
    Solution: Add such info in doc, and add `ergPath` setup option

--- lua/lspconfig/configs/pylyzer.lua
+++ lua/lspconfig/configs/pylyzer.lua
@@ -23,12 +23,25 @@ return {
         checkOnType = false,
       },
     },
+    ergPath = vim.fn.getenv('HOME') .. '/.erg',
+    on_new_config = function(new_config, _)
+      if new_config.ergPath then
+        new_config.cmd_env.ERG_PATH = new_config.ergPath
+      end
+    end,
   },
   docs = {
     description = [[
   https://github.com/mtshiba/pylyzer
 
   `pylyzer`, a fast static code analyzer & language server for Python.
+
+  `pylyzer` requires Erg as dependency, and finds it via `ERG_PATH` environment variable.
+  By default `ERG_PATH` is set to `~/.erg` when `lspconfig` runs `pylyzer`,
+  pass `ergPath = "/path/to/erg"` if you want to change it.
+
+  To install Erg, simply extract tarball/zip from [Erg releases](https://github.com/erg-lang/erg/releases/latest)
+  to the the path where you want to install it, e.g. `~/.erg`.
     ]],
   },
 }
