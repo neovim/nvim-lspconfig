7e3ac7c6162e6345ff51fc93e2740d8fa956f7ac
Stefano Volpe <foxy@teapot.ovh>
2024-11-19 06:08:43 +0100

    feat: ltex-ls-plus

---
+++ lua/lspconfig/configs/ltex_plus.lua
@@ -0,0 +1,90 @@
+local util = require 'lspconfig.util'
+
+local language_id_mapping = {
+  bib = 'bibtex',
+  plaintex = 'tex',
+  rnoweb = 'rsweave',
+  rst = 'restructuredtext',
+  tex = 'latex',
+  pandoc = 'markdown',
+  text = 'plaintext',
+}
+
+local filetypes = {
+  'bib',
+  'gitcommit',
+  'markdown',
+  'org',
+  'plaintex',
+  'rst',
+  'rnoweb',
+  'tex',
+  'pandoc',
+  'quarto',
+  'rmd',
+  'context',
+  'html',
+  'xhtml',
+  'mail',
+  'text',
+}
+
+local function get_language_id(_, filetype)
+  local language_id = language_id_mapping[filetype]
+  if language_id then
+    return language_id
+  else
+    return filetype
+  end
+end
+local enabled_ids = {}
+do
+  local enabled_keys = {}
+  for _, ft in ipairs(filetypes) do
+    local id = get_language_id({}, ft)
+    if not enabled_keys[id] then
+      enabled_keys[id] = true
+      table.insert(enabled_ids, id)
+    end
+  end
+end
+
+return {
+  default_config = {
+    cmd = { 'ltex-ls-plus' },
+    filetypes = filetypes,
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+    get_language_id = get_language_id,
+    settings = {
+      ltex = {
+        enabled = enabled_ids,
+      },
+    },
+  },
+  docs = {
+    description = [=[
+https://github.com/ltex-plus/ltex-ls-plus
+
+LTeX Language Server: LSP language server for LanguageTool 🔍✔️ with support for LaTeX 🎓, Markdown 📝, and others
+
+To install, download the latest [release](https://github.com/ltex-plus/ltex-ls-plus) and ensure `ltex-ls-plus` is on your path.
+
+This server accepts configuration via the `settings` key.
+
+```lua
+  settings = {
+		ltex = {
+			language = "en-GB",
+		},
+	},
+```
+
+To support org files or R sweave, users can define a custom filetype autocommand (or use a plugin which defines these filetypes):
+
+```lua
+vim.cmd [[ autocmd BufRead,BufNewFile *.org set filetype=org ]]
+```
+]=],
+  },
+}
