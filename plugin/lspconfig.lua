687fcb939b7de052a3ba6c300348fcd5eb128e83
dundargoc <gocdundar@gmail.com>
2024-12-01 15:41:11 +0100

    refactor: remove outdated code
    Lspconfig requires neovim version 0.9 at the time of writing this, so
    discard any version checks and code for older versions.

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -6,17 +6,6 @@ vim.g.lspconfig = 1
 local api, lsp = vim.api, vim.lsp
 local util = require('lspconfig.util')
 
-if vim.fn.has 'nvim-0.8' ~= 1 then
-  local version_info = vim.version()
-  local warning_str = string.format(
-    '[lspconfig] requires neovim 0.8 or later. Detected neovim version: 0.%s.%s',
-    version_info.minor,
-    version_info.patch
-  )
-  vim.notify_once(warning_str)
-  return
-end
-
 local completion_sort = function(items)
   table.sort(items)
   return items
