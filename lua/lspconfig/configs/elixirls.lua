a4ed125210f5642b6fbe4bc5f8d7b3e28a4772ed
Justin M. Keyes <justinkz@gmail.com>
2024-12-02 17:22:44 +0100

    docs: broken elixir-ls URL
    fix #3488

--- lua/lspconfig/configs/elixirls.lua
+++ lua/lspconfig/configs/elixirls.lua
@@ -16,12 +16,13 @@ https://github.com/elixir-lsp/elixir-ls
 
 `elixir-ls` can be installed by following the instructions [here](https://github.com/elixir-lsp/elixir-ls#building-and-running).
 
-```bash
-curl -fLO https://github.com/elixir-lsp/elixir-ls/releases/latest/download/elixir-ls.zip
-unzip elixir-ls.zip -d /path/to/elixir-ls
-# Unix
-chmod +x /path/to/elixir-ls/language_server.sh
-```
+1. Download the zip from https://github.com/elixir-lsp/elixir-ls/releases/latest/
+2. Unzip it and make it executable.
+   ```bash
+   unzip elixir-ls.zip -d /path/to/elixir-ls
+   # Unix
+   chmod +x /path/to/elixir-ls/language_server.sh
+   ```
 
 **By default, elixir-ls doesn't have a `cmd` set.** This is because nvim-lspconfig does not make assumptions about your path. You must add the following to your init.vim or init.lua to set `cmd` to the absolute path ($HOME and ~ are not expanded) of your unzipped elixir-ls.
 
