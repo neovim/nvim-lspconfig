10b50434776ba7fd6b21549e13e939e6b3b66bed
Stefano Volpe <foxy@teapot.ovh>
2024-12-05 03:38:56 +0100

    style: apply review suggestions

--- lua/lspconfig/configs/ltex_plus.lua
+++ lua/lspconfig/configs/ltex_plus.lua
@@ -30,24 +30,25 @@ local filetypes = {
 }
 
 local function get_language_id(_, filetype)
-  local language_id = language_id_mapping[filetype]
-  if language_id then
-    return language_id
-  else
-    return filetype
-  end
+  return language_id_mapping[filetype] or filetype
 end
-local enabled_ids = {}
-do
-  local enabled_keys = {}
-  for _, ft in ipairs(filetypes) do
-    local id = get_language_id({}, ft)
-    if not enabled_keys[id] then
-      enabled_keys[id] = true
-      table.insert(enabled_ids, id)
-    end
-  end
-end
+
+local enabled_ids = {
+  'bibtex',
+  'context',
+  'gitcommit',
+  'html',
+  'mail',
+  'markdown',
+  'org',
+  'quarto',
+  'restructuredtext',
+  'rmd',
+  'rsweave',
+  'tex',
+  'text',
+  'xhtml',
+}
 
 return {
   default_config = {
