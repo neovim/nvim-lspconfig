8a595c04013d6139d09c831c4c24474c1973b0cb
Parsifa1 <li.aldric@gmail.com>
2024-12-09 16:56:25 +0800

    fix: duplicate node_modules in astro && volar lsp
    fix #3496

--- lua/lspconfig/configs/astro.lua
+++ lua/lspconfig/configs/astro.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 
 local function get_typescript_server_path(root_dir)
   local project_root = vim.fs.find('node_modules', { path = root_dir, upward = true })[1]
-  return project_root and (util.path.join(project_root, 'node_modules', 'typescript', 'lib')) or ''
+  return project_root and (util.path.join(project_root, 'typescript', 'lib')) or ''
 end
 
 return {
