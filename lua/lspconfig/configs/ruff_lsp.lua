ffa0ad98b11aca585ceff79928421d1fffc265de
Apostoli Geyer <ajgeyer@uwaterloo.ca>
2024-12-14 15:03:16 -0500

    fix(ruff, ruff_lsp): error returning the function returned by util.root_pattern on lsp start

--- lua/lspconfig/configs/ruff_lsp.lua
+++ lua/lspconfig/configs/ruff_lsp.lua
@@ -5,7 +5,7 @@ return {
     cmd = { 'ruff-lsp' },
     filetypes = { 'python' },
     root_dir = function(fname)
-      return util.root_pattern('pyproject.toml', 'ruff.toml')
+      return util.root_pattern('pyproject.toml', 'ruff.toml')(fname)
         or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
     end,
     single_file_support = true,
