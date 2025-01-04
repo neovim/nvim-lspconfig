324865b49ebb57a4fbed9896b3e3446b22676c2e
Grant Lemons <grantlemons@aol.com>
2025-01-04 06:53:07 -0600

    feat: update Harper supported languages #3534

--- lua/lspconfig/configs/harper_ls.lua
+++ lua/lspconfig/configs/harper_ls.lua
@@ -20,6 +20,8 @@ return {
       'toml',
       'typescript',
       'typescriptreact',
+      'haskell',
+      'cmake',
     },
     root_dir = function(fname)
       return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
