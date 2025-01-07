d8aebad098e30a347cd2a5ff03b59999aa94bbc6
Guilherme Felipe <guilherme.felipe@nubank.com.br>
2025-01-07 10:10:26 -0300

    fix(clojure_lsp): adds single_file_support for clojure
    This allow navigation within dependecies

--- lua/lspconfig/configs/clojure_lsp.lua
+++ lua/lspconfig/configs/clojure_lsp.lua
@@ -5,6 +5,7 @@ return {
     cmd = { 'clojure-lsp' },
     filetypes = { 'clojure', 'edn' },
     root_dir = util.root_pattern('project.clj', 'deps.edn', 'build.boot', 'shadow-cljs.edn', '.git', 'bb.edn'),
+    single_file_support = true,
   },
   docs = {
     description = [[
