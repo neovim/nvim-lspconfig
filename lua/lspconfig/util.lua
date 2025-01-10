2ec5c3f1fd044dfd0bffcdfac58c1299f9f76178
Yanguk <yanguk@duck.com>
2025-01-11 00:22:32 +0900

    refactor(util): change traversal method for root_pattern

--- lua/lspconfig/util.lua
+++ lua/lspconfig/util.lua
@@ -175,18 +175,19 @@ function M.root_pattern(...)
   local patterns = M.tbl_flatten { ... }
   return function(startpath)
     startpath = M.strip_archive_subpath(startpath)
-    for _, pattern in ipairs(patterns) do
-      local match = M.search_ancestors(startpath, function(path)
+
+    local match = M.search_ancestors(startpath, function(path)
+      for _, pattern in ipairs(patterns) do
         for _, p in ipairs(vim.fn.glob(table.concat({ escape_wildcards(path), pattern }, '/'), true, true)) do
           if vim.loop.fs_stat(p) then
             return path
           end
         end
-      end)
-
-      if match ~= nil then
-        return match
       end
+    end)
+
+    if match ~= nil then
+      return match
     end
   end
 end
