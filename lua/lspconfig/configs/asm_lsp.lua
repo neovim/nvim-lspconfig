a4c9d5f4a78854c5230d21984e1a4cc19b1db7bf
iguanacucumber <you@example.com>
2025-01-17 02:39:34 +0400

    feat(asm_lsp): add NASM support and single_file support

--- lua/lspconfig/configs/asm_lsp.lua
+++ lua/lspconfig/configs/asm_lsp.lua
@@ -1,16 +1,20 @@
+local util = require 'lspconfig.util'
+
 return {
   default_config = {
     cmd = { 'asm-lsp' },
     filetypes = { 'asm', 'vmasm' },
+    single_file_support = true,
     root_dir = function(fname)
-      return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+      return util.root_pattern('.asm-lsp.toml')(fname)
+        or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
     end,
   },
   docs = {
     description = [[
 https://github.com/bergercookie/asm-lsp
 
-Language Server for GAS/GO Assembly
+Language Server for NASM/GAS/GO Assembly
 
 `asm-lsp` can be installed via cargo:
 cargo install asm-lsp
