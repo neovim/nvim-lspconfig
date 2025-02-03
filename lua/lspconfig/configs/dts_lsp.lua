b092d5e94fa1452d1a27a217e1470b3f2851b348
Sergey Golovin <golovinengineer@icloud.com>
2025-02-03 10:27:33 +0200

    fix(dts-lsp): add links to devicetree.org and zephyrproject

--- lua/lspconfig/configs/dts_lsp.lua
+++ lua/lspconfig/configs/dts_lsp.lua
@@ -10,13 +10,18 @@ return {
   },
   docs = {
     description = [[
-`dts-lsp` is an LSP for DTS files built on top of tree-sitter-devicetree grammar.
+`dts-lsp` is an LSP for Devicetree files built on top of tree-sitter-devicetree grammar.
 Language servers can be used in many editors, such as Visual Studio Code, Emacs
 or Vim
 
 Install `dts-lsp` from https://github.com/igor-prusov/dts-lsp and add it to path
 
 `dts-lsp` doesn't require any configuration.
+
+More about Devicetree:
+https://www.devicetree.org/
+https://docs.zephyrproject.org/latest/build/dts/index.html
+
 ]],
   },
 }
