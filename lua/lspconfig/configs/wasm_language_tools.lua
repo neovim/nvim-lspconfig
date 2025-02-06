4ff005d1dce5154a0fb82680c4c138af0524a815
Pig Fang <g-plane@hotmail.com>
2025-02-06 13:49:33 +0800

    feat: add wasm-language-tools (#3564)
    * feat: add wasm-language-tools
    
    * chore: remove unused `util`

---
+++ lua/lspconfig/configs/wasm_language_tools.lua
@@ -0,0 +1,15 @@
+return {
+  default_config = {
+    cmd = { 'wat_server' },
+    filetypes = { 'wat' },
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+https://github.com/g-plane/wasm-language-tools
+
+WebAssembly Language Tools aims to provide and improve the editing experience of WebAssembly Text Format.
+It also provides an out-of-the-box formatter (a.k.a. pretty printer) for WebAssembly Text Format.
+]],
+  },
+}
