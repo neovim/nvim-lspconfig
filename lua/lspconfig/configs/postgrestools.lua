dff8e7e5457609943d6a35ec34339f5558080d1b
psteinroe <philipp@steinroetter.com>
2025-03-21 08:27:16 +0100

    feat: postgrestools

---
+++ lua/lspconfig/configs/postgrestools.lua
@@ -0,0 +1,19 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'postgrestools', 'lsp-proxy' },
+    filetypes = {
+      'sql',
+    },
+    root_dir = util.root_pattern('postgrestools.jsonc'),
+    single_file_support = false,
+  },
+  docs = {
+    description = [[
+https://pgtools.dev
+
+A collection of language tools and a Language Server Protocol (LSP) implementation for Postgres, focusing on developer experience and reliable SQL tooling..
+]],
+  },
+}
