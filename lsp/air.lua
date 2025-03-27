dfd3bcc77bd61350bba056e071063139bb117420
TheRealLorenz <lollobellina@gmail.com>
2025-03-27 19:00:59 +0100

    feat(air): port config for neovim 0.11

---
+++ lsp/air.lua
@@ -0,0 +1,12 @@
+---@brief
+---
+---https://github.com/posit-dev/air
+--
+-- Air is an R formatter and language server, written in Rust.
+--
+-- Refer to the [documentation](https://posit-dev.github.io/air/editors.html) for more details.
+return {
+  cmd = { 'air', 'language-server' },
+  filetypes = { 'r' },
+  root_markers = { 'air.toml', '.air.toml', '.git' },
+}
