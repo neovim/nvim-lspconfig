4efb9b0a9b9770ab530a282514624824e9b0271a
TheRealLorenz <lollobellina@gmail.com>
2025-03-27 12:01:55 +0100

    feat(tinymist): port config for neovim 0.11

---
+++ lsp/tinymist.lua
@@ -0,0 +1,9 @@
+---@brief
+---
+---https://github.com/Myriad-Dreamin/tinymist
+-- An integrated language service for Typst [taɪpst]. You can also call it "微霭" [wēi ǎi] in Chinese.
+return {
+  cmd = { 'tinymist' },
+  filetypes = { 'typst' },
+  root_markers = { '.git' },
+}
