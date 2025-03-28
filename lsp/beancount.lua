5520282dafa65db56733ee184674ce808ecc7279
TheRealLorenz <lollobellina@gmail.com>
2025-03-28 15:27:57 +0100

    feat(beancount): port config for neovim 0.11

---
+++ lsp/beancount.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+---https://github.com/polarmutex/beancount-language-server#installation
+--
+-- See https://github.com/polarmutex/beancount-language-server#configuration for configuration options
+return {
+  cmd = { 'beancount-language-server', '--stdio' },
+  filetypes = { 'beancount', 'bean' },
+  root_markers = { '.git' },
+  init_options = {},
+}
