4b1e9f2d3eddc54b994587d4a40822565ff8a2ef
TheRealLorenz <lollobellina@gmail.com>
2025-03-28 15:26:35 +0100

    feat(basics): port config for neovim 0.11

---
+++ lsp/basics_ls.lua
@@ -0,0 +1,25 @@
+---@brief
+---
+---https://github.com/antonk52/basics-language-server/
+--
+-- Buffer, path, and snippet completion
+--
+-- ```sh
+-- npm install -g basics-language-server
+-- ```
+return {
+  cmd = { 'basics-language-server' },
+  settings = {
+    buffer = {
+      enable = true,
+      minCompletionLength = 4,
+    },
+    path = {
+      enable = true,
+    },
+    snippet = {
+      enable = false,
+      sources = {},
+    },
+  },
+}
