f2c03e86504c9224829948e29b093c24e3b930da
TheRealLorenz <lollobellina@gmail.com>
2025-03-28 14:27:31 +0100

    feat(autotools_ls): port config for neovim 0.11

---
+++ lsp/autotools_ls.lua
@@ -0,0 +1,22 @@
+local util = require 'lspconfig.util'
+
+local root_files = { 'configure.ac', 'Makefile', 'Makefile.am', '*.mk' }
+
+---@brief
+---
+---https://github.com/Freed-Wu/autotools-language-server
+--
+-- `autotools-language-server` can be installed via `pip`:
+-- ```sh
+-- pip install autotools-language-server
+-- ```
+--
+-- Language server for autoconf, automake and make using tree sitter in python.
+return {
+  cmd = { 'autotools-language-server' },
+  filetypes = { 'config', 'automake', 'make' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    done_callback(util.root_pattern(unpack(root_files))(fname))
+  end,
+}
