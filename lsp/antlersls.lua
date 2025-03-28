b0aad7c7c1846c57df747e33cb978e77cfc2ff86
TheRealLorenz <lollobellina@gmail.com>
2025-03-28 14:21:57 +0100

    feat(antlersls): port config for neovim 0.11

---
+++ lsp/antlersls.lua
@@ -0,0 +1,13 @@
+---@brief
+---
+---https://www.npmjs.com/package/antlers-language-server
+--
+-- `antlersls` can be installed via `npm`:
+-- ```sh
+-- npm install -g antlers-language-server
+-- ```
+return {
+  cmd = { 'antlersls', '--stdio' },
+  filetypes = { 'html', 'antlers' },
+  root_markers = { 'composer.json' },
+}
