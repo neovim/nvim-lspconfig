33566e2787c462b43ca15fe1c77cf4d1725d8d0d
TheRealLorenz <lollobellina@gmail.com>
2025-03-28 14:26:33 +0100

    feat(autohotkey_lsp): port config for neovim 0.11

---
+++ lsp/autohotkey_lsp.lua
@@ -0,0 +1,48 @@
+-- NOTE: AutoHotkey is used only on windows
+local function get_autohotkey_path()
+  local path = vim.fn.exepath('autohotkey.exe')
+  return #path > 0 and path or ''
+end
+
+---@brief
+---
+---https://github.com/thqby/vscode-autohotkey2-lsp
+--
+-- AutoHotkey v2.0 LSP implementation
+return {
+  cmd = { 'autohotkey_lsp', '--stdio' },
+  filetypes = { 'autohotkey' },
+  root_markers = { 'package.json' },
+  flags = { debounce_text_changes = 500 },
+  --capabilities = capabilities,
+  --on_attach = custom_attach,
+  init_options = {
+    locale = 'en-us',
+    InterpreterPath = get_autohotkey_path(),
+    AutoLibInclude = 'All',
+    CommentTags = '^;;\\s*(?<tag>.+)',
+    CompleteFunctionParens = false,
+    SymbolFoldinFromOpenBrace = false,
+    Diagnostics = {
+      ClassStaticMemberCheck = true,
+      ParamsCheck = true,
+    },
+    ActionWhenV1IsDetected = 'Continue',
+    FormatOptions = {
+      array_style = 'expand',
+      break_chained_methods = false,
+      ignore_comment = false,
+      indent_string = '\t',
+      max_preserve_newlines = 2,
+      brace_style = 'One True Brace',
+      object_style = 'none',
+      preserve_newlines = true,
+      space_after_double_colon = true,
+      space_before_conditional = true,
+      space_in_empty_paren = false,
+      space_in_other = true,
+      space_in_paren = false,
+      wrap_line_length = 0,
+    },
+  },
+}
