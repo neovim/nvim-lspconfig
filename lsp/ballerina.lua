271d4befbc75e8eda0b7ea29531f94a4a49440c0
TheRealLorenz <lollobellina@gmail.com>
2025-03-28 15:25:16 +0100

    feat(ballerina): port config for neovim 0.11

---
+++ lsp/ballerina.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+---Ballerina language server
+--
+-- The Ballerina language's CLI tool comes with its own language server implementation.
+-- The `bal` command line tool must be installed and available in your system's PATH.
+return {
+  cmd = { 'bal', 'start-language-server' },
+  filetypes = { 'ballerina' },
+  root_markers = { 'Ballerina.toml' },
+}
