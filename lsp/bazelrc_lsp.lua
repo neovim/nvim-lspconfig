e492cac1ab218ad4bc622c23c7283a82b0a661fc
TheRealLorenz <lollobellina@gmail.com>
2025-03-28 15:27:30 +0100

    feat(bazelrc_lsp): port config for neovim 0.11

---
+++ lsp/bazelrc_lsp.lua
@@ -0,0 +1,20 @@
+---@brief
+---
+---https://github.com/salesforce-misc/bazelrc-lsp
+--
+-- `bazelrc-lsp` is a LSP for `.bazelrc` configuration files.
+--
+-- The `.bazelrc` file type is not detected automatically, you can register it manually (see below) or override the filetypes:
+--
+-- ```lua
+-- vim.filetype.add {
+--   pattern = {
+--     ['.*.bazelrc'] = 'bazelrc',
+--   },
+-- }
+-- ```
+return {
+  cmd = { 'bazelrc-lsp', 'lsp' },
+  filetypes = { 'bazelrc' },
+  root_markers = { 'WORKSPACE', 'WORKSPACE.bazel', 'MODULE.bazel' },
+}
