77c95373fea0f17491d77a4a41dababb18b60d80
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:14:38 +0100

    feat(fsharp_language_server): port config for neovim 0.11

---
+++ lsp/fsharp_language_server.lua
@@ -0,0 +1,28 @@
+local util = require 'lspconfig.util'
+
+---@brief
+---
+---F# Language Server
+-- https://github.com/faldor20/fsharp-language-server
+--
+-- An implementation of the language server protocol using the F# Compiler Service.
+--
+-- Build the project from source and override the command path to location of DLL.
+--
+-- If filetype determination is not already performed by an available plugin ([PhilT/vim-fsharp](https://github.com/PhilT/vim-fsharp), [fsharp/vim-fsharp](https://github.com/fsharp/vim-fsharp), and [adelarsq/neofsharp.vim](https://github.com/adelarsq/neofsharp.vim).
+-- ), then the following must be added to initialization configuration:
+--
+--
+-- `autocmd BufNewFile,BufRead *.fs,*.fsx,*.fsi set filetype=fsharp`
+return {
+  cmd = { 'dotnet', 'FSharpLanguageServer.dll' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    done_callback(util.root_pattern('*.sln', '*.fsproj', '.git')(fname))
+  end,
+  filetypes = { 'fsharp' },
+  init_options = {
+    AutomaticWorkspaceInit = true,
+  },
+  settings = {},
+}
