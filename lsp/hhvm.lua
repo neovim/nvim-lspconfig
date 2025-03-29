788f54235c9f6abf2eedadf836a899a9cf310b1d
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:21:33 +0100

    feat(hhvm): port config for neovim 0.11

---
+++ lsp/hhvm.lua
@@ -0,0 +1,12 @@
+---@brief
+---
+---Language server for programs written in Hack
+-- https://hhvm.com/
+-- https://github.com/facebook/hhvm
+-- See below for how to setup HHVM & typechecker:
+-- https://docs.hhvm.com/hhvm/getting-started/getting-started
+return {
+  cmd = { 'hh_client', 'lsp' },
+  filetypes = { 'php', 'hack' },
+  root_markers = { '.hhconfig' },
+}
