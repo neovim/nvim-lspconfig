93cfed7b0cdf0996d727576834d636a52d18d308
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:18:16 +0100

    feat(grammarly): port config for neovim 0.11

---
+++ lsp/grammarly.lua
@@ -0,0 +1,24 @@
+---@brief
+---
+---https://github.com/znck/grammarly
+--
+-- `grammarly-languageserver` can be installed via `npm`:
+--
+-- ```sh
+-- npm i -g grammarly-languageserver
+-- ```
+--
+-- WARNING: Since this language server uses Grammarly's API, any document you open with it running is shared with them. Please evaluate their [privacy policy](https://www.grammarly.com/privacy-policy) before using this.
+return {
+  cmd = { 'grammarly-languageserver', '--stdio' },
+  filetypes = { 'markdown' },
+  root_markers = { '.git' },
+  handlers = {
+    ['$/updateDocumentState'] = function()
+      return ''
+    end,
+  },
+  init_options = {
+    clientId = 'client_BaDkMgx4X19X9UxxYRCXZo',
+  },
+}
