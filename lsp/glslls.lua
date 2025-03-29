4dfced922b72b2f0c810bcbbac58a631d91a3874
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:17:32 +0100

    feat(glslls): port config for neovim 0.11

---
+++ lsp/glslls.lua
@@ -0,0 +1,21 @@
+---@brief
+---
+---https://github.com/svenstaro/glsl-language-server
+--
+-- Language server implementation for GLSL
+--
+-- `glslls` can be compiled and installed manually, or, if your distribution has access to the AUR,
+-- via the `glsl-language-server` AUR package
+return {
+  cmd = { 'glslls', '--stdin' },
+  filetypes = { 'glsl', 'vert', 'tesc', 'tese', 'frag', 'geom', 'comp' },
+  root_markers = { '.git' },
+  capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { 'utf-8', 'utf-16' },
+  },
+}
