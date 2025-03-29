7f041212b13b4a10721861f3ff7f91018db2afa4
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:14:51 +0100

    feat(fstar): port config for neovim 0.11

---
+++ lsp/fstar.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/FStarLang/FStar
+--
+-- LSP support is included in FStar. Make sure `fstar.exe` is in your PATH.
+return {
+  cmd = { 'fstar.exe', '--lsp' },
+  filetypes = { 'fstar' },
+  root_markers = { '.git' },
+}
