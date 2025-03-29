3602c69b8ecddd81d100822ae7d11e4a73c208ac
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 12:44:04 +0100

    feat(djlsp): port config for neovim 0.11

---
+++ lsp/djlsp.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+---https://github.com/fourdigits/django-template-lsp
+
+-- `djlsp`, a language server for Django templates.
+return {
+  cmd = { 'djlsp' },
+  filetypes = { 'html', 'htmldjango' },
+  root_markers = { '.git' },
+  settings = {},
+}
