694fa848c583004e2a839a65a64ae6b104dc5882
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 12:47:22 +0100

    feat(elp): port config for neovim 0.11

---
+++ lsp/elp.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+---https://whatsapp.github.io/erlang-language-platform
+--
+-- ELP integrates Erlang into modern IDEs via the language server protocol and was
+-- inspired by rust-analyzer.
+return {
+  cmd = { 'elp', 'server' },
+  filetypes = { 'erlang' },
+  root_markers = { 'rebar.config', 'erlang.mk', '.git' },
+}
