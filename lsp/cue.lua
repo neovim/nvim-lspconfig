9dc38202901f4c8a3fc5bab7f0b0f542e93adb81
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 11:23:53 +0100

    feat(cue): port config for neovim 0.11

---
+++ lsp/cue.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/cue-lang/cue
+--
+-- CUE makes it easy to validate data, write schemas, and ensure configurations align with policies.
+return {
+  cmd = { 'cue', 'lsp' },
+  filetypes = { 'cue' },
+  root_markers = { 'cue.mod', '.git' },
+}
