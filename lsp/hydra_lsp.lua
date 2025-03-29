539900d1459f72f2e2019b3a48f9cc1a40b08813
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:24:32 +0100

    feat(hydra_lsp): port config for neovim 0.11

---
+++ lsp/hydra_lsp.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/Retsediv/hydra-lsp
+--
+-- LSP for Hydra Python package config files.
+return {
+  cmd = { 'hydra-lsp' },
+  filetypes = { 'yaml' },
+  root_markers = { '.git' },
+}
