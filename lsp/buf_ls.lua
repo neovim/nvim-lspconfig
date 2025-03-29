1ade9bdcc7e69353535406aaca1beae9ae18b4a1
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 11:14:35 +0100

    feat(buf_ls): port config for neovim 0.11

---
+++ lsp/buf_ls.lua
@@ -0,0 +1,10 @@
+-- https://github.com/bufbuild/buf
+--
+-- buf beta lsp included in the cli itself
+--
+-- buf beta lsp is a Protobuf language server compatible with Buf modules and workspaces
+return {
+  cmd = { 'buf', 'beta', 'lsp', '--timeout=0', '--log-format=text' },
+  filetypes = { 'proto' },
+  root_markers = { 'buf.yaml', '.git' },
+}
