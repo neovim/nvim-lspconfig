b42f9f3318d7e958bd0cb9073a56f7fac7e6da0d
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 12:46:14 +0100

    feat(ecsact): port config for neovim 0.11

---
+++ lsp/ecsact.lua
@@ -0,0 +1,13 @@
+---@brief
+---
+---https://github.com/ecsact-dev/ecsact_lsp_server
+--
+-- Language server for Ecsact.
+--
+-- The default cmd assumes `ecsact_lsp_server` is in your PATH. Typically from the
+-- Ecsact SDK: https://ecsact.dev/start
+return {
+  cmd = { 'ecsact_lsp_server', '--stdio' },
+  filetypes = { 'ecsact' },
+  root_markers = { '.git' },
+}
