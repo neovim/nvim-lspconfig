64fb85ad7fb9e714f081d3b616bc96cfebe04268
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 11:14:05 +0100

    feat(buddy_ls): port config for neovim 0.11

---
+++ lsp/buddy_ls.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+---https://github.com/buddy-compiler/buddy-mlir#buddy-lsp-server
+-- The Language Server for the buddy-mlir, a drop-in replacement for mlir-lsp-server,
+-- supporting new dialects defined in buddy-mlir.
+-- `buddy-lsp-server` can be installed at the buddy-mlir repository (buddy-compiler/buddy-mlir)
+return {
+  cmd = { 'buddy-lsp-server' },
+  filetypes = { 'mlir' },
+  root_markers = { '.git' },
+}
