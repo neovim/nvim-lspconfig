e389653fcae383986cb7cd7c322756e2f35d5253
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 11:21:55 +0100

    feat(crystalline): port config for neovim 0.11

---
+++ lsp/crystalline.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/elbywan/crystalline
+--
+-- Crystal language server.
+return {
+  cmd = { 'crystalline' },
+  filetypes = { 'crystal' },
+  root_markers = { 'shard.yml', '.git' },
+}
