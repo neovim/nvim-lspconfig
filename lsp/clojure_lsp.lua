229c6dd960918cf2b16cfb0a80adbae3565d5029
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 11:19:31 +0100

    feat(clojure_lsp): port config for neovim 0.11

---
+++ lsp/clojure_lsp.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/clojure-lsp/clojure-lsp
+--
+-- Clojure Language Server
+return {
+  cmd = { 'clojure-lsp' },
+  filetypes = { 'clojure', 'edn' },
+  root_markers = { 'project.clj', 'deps.edn', 'build.boot', 'shadow-cljs.edn', '.git', 'bb.edn' },
+}
