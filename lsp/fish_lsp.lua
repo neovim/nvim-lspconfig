10ef633016876b77f98ad45fa225db3053f81e25
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:13:19 +0100

    feat(fish_lsp): port config for neovim 0.11

---
+++ lsp/fish_lsp.lua
@@ -0,0 +1,16 @@
+---@brief
+---
+---https://github.com/ndonfris/fish-lsp
+--
+-- A Language Server Protocol (LSP) tailored for the fish shell.
+-- This project aims to enhance the coding experience for fish,
+-- by introducing a suite of intelligent features like auto-completion,
+-- scope aware symbol analysis, per-token hover generation, and many others.
+--
+-- [homepage](https://www.fish-lsp.dev/)
+return {
+  cmd = { 'fish-lsp', 'start' },
+  cmd_env = { fish_lsp_show_client_popups = false },
+  filetypes = { 'fish' },
+  root_markers = { '.git' },
+}
