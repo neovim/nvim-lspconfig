5f29269eac97331e6787b4239dc21aa2c9e91daf
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:21:44 +0100

    feat(hie): port config for neovim 0.11

---
+++ lsp/hie.lua
@@ -0,0 +1,23 @@
+---@brief
+---
+---https://github.com/haskell/haskell-ide-engine
+--
+-- the following init_options are supported (see https://github.com/haskell/haskell-ide-engine#configuration):
+-- ```lua
+-- init_options = {
+--   languageServerHaskell = {
+--     hlintOn = bool;
+--     maxNumberOfProblems = number;
+--     diagnosticsDebounceDuration = number;
+--     liquidOn = bool (default false);
+--     completionSnippetsOn = bool (default true);
+--     formatOnImportOn = bool (default true);
+--     formattingProvider = string (default "brittany", alternate "floskell");
+--   }
+-- }
+-- ```
+return {
+  cmd = { 'hie-wrapper', '--lsp' },
+  filetypes = { 'haskell' },
+  root_markers = { 'stack.yaml', 'package.yaml', '.git' },
+}
