bb82749287d8e12c3fe3252b73c173ff843e7e70
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 11:18:22 +0100

    feat(cds_lsp): port config for neovim 0.11

---
+++ lsp/cds_lsp.lua
@@ -0,0 +1,18 @@
+---@brief
+---
+---https://cap.cloud.sap/docs/
+--
+-- `cds-lsp` can be installed via `npm`:
+--
+-- ```sh
+-- npm i -g @sap/cds-lsp
+-- ```
+return {
+  cmd = { 'cds-lsp', '--stdio' },
+  filetypes = { 'cds' },
+  -- init_options = { provideFormatter = true }, -- needed to enable formatting capabilities
+  root_markers = { 'package.json', 'db', 'srv' },
+  settings = {
+    cds = { validate = true },
+  },
+}
