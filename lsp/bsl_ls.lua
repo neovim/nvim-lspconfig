728cd7ba5ae85c69a35eecb561e06e315e9ff0fc
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 11:13:15 +0100

    feat(bsl_ls): port config for neovim 0.11

---
+++ lsp/bsl_ls.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/1c-syntax/bsl-language-server
+--
+-- Language Server Protocol implementation for 1C (BSL) - 1C:Enterprise 8 and OneScript languages.
+
+return {
+  filetypes = { 'bsl', 'os' },
+  root_markers = { '.git' },
+}
