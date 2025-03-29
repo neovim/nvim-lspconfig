426ca09a1f18b387f214e210057134bfcf3cad68
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 11:21:16 +0100

    feat(contextive): port config for neovim 0.11

---
+++ lsp/contextive.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://github.com/dev-cycles/contextive
+--
+-- Language Server for Contextive.
+--
+-- Contextive allows you to define terms in a central file and provides auto-completion suggestions and hover panels for these terms wherever they're used.
+--
+-- To install the language server, you need to download the appropriate [GitHub release asset](https://github.com/dev-cycles/contextive/releases/) for your operating system and architecture.
+--
+-- After the download unzip the Contextive.LanguageServer binary and copy the file into a folder that is included in your system's PATH.
+return {
+  cmd = { 'Contextive.LanguageServer' },
+  root_markers = { '.contextive', '.git' },
+}
