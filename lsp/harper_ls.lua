a651135ead3fd2f23444ee4086d3aaf784849407
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:20:55 +0100

    feat(harper_ls): port config for neovim 0.11

---
+++ lsp/harper_ls.lua
@@ -0,0 +1,47 @@
+---@brief
+---
+---https://github.com/automattic/harper
+--
+-- The language server for Harper, the slim, clean language checker for developers.
+--
+-- See our [documentation](https://writewithharper.com/docs/integrations/neovim) for more information on settings.
+--
+-- In short, they should look something like this:
+-- ```lua
+-- vim.lsp.config('harper_ls', {
+--   settings = {
+--     ["harper-ls"] = {
+--       userDictPath = "~/dict.txt"
+--     }
+--   },
+-- })
+-- ```
+return {
+  cmd = { 'harper-ls', '--stdio' },
+  filetypes = {
+    'c',
+    'cpp',
+    'cs',
+    'gitcommit',
+    'go',
+    'html',
+    'java',
+    'javascript',
+    'lua',
+    'markdown',
+    'nix',
+    'python',
+    'ruby',
+    'rust',
+    'swift',
+    'toml',
+    'typescript',
+    'typescriptreact',
+    'haskell',
+    'cmake',
+    'typst',
+    'php',
+    'dart',
+  },
+  root_markers = { '.git' },
+}
