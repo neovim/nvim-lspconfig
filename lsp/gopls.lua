63fb6e7de08d03f3e517e5e4398d1255f63c3157
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:17:57 +0100

    feat(gopls): port config for neovim 0.11

---
+++ lsp/gopls.lua
@@ -0,0 +1,43 @@
+local util = require 'lspconfig.util'
+local mod_cache = nil
+
+---@param fname string
+---@return string?
+local function get_root(fname)
+  if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
+    local clients = util.get_lsp_clients { name = 'gopls' }
+    if #clients > 0 then
+      return clients[#clients].config.root_dir
+    end
+  end
+  return vim.fs.root(fname, { 'go.work', 'go.mod', '.git' })
+end
+
+---@brief
+---
+---https://github.com/golang/tools/tree/master/gopls
+--
+-- Google's lsp server for golang.
+return {
+  cmd = { 'gopls' },
+  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    -- see: https://github.com/neovim/nvim-lspconfig/issues/804
+    if mod_cache then
+      done_callback(get_root(fname))
+      return
+    end
+    local cmd = { 'go', 'env', 'GOMODCACHE' }
+    vim.system(cmd, { text = true }, function(output)
+      if output.code == 0 then
+        if output.stdout then
+          mod_cache = vim.trim(output.stdout)
+        end
+        done_callback(get_root(fname))
+      else
+        vim.notify(('[gopls] cmd failed with code %d: %s\n%s'):format(output.code, cmd, output.stderr))
+      end
+    end)
+  end,
+}
