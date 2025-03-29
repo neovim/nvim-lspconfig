0ce3a14cfd9a98ad5b151f18fd2a83f1305966e4
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 12:47:45 +0100

    feat(emmet_ls): port config for neovim 0.11

---
+++ lsp/emmet_ls.lua
@@ -0,0 +1,28 @@
+---@brief
+---
+---https://github.com/aca/emmet-ls
+--
+-- Package can be installed via `npm`:
+-- ```sh
+-- npm install -g emmet-ls
+-- ```
+return {
+  cmd = { 'emmet-ls', '--stdio' },
+  filetypes = {
+    'astro',
+    'css',
+    'eruby',
+    'html',
+    'htmldjango',
+    'javascriptreact',
+    'less',
+    'pug',
+    'sass',
+    'scss',
+    'svelte',
+    'typescriptreact',
+    'vue',
+    'htmlangular',
+  },
+  root_markers = { '.git' },
+}
