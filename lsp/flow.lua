581ac722509fc61cd279e7b8c8652b63b705481c
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:13:28 +0100

    feat(flow): port config for neovim 0.11

---
+++ lsp/flow.lua
@@ -0,0 +1,18 @@
+---@brief
+---
+---https://flow.org/
+-- https://github.com/facebook/flow
+--
+-- See below for how to setup Flow itself.
+-- https://flow.org/en/docs/install/
+--
+-- See below for lsp command options.
+--
+-- ```sh
+-- npx flow lsp --help
+-- ```
+return {
+  cmd = { 'npx', '--no-install', 'flow', 'lsp' },
+  filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx' },
+  root_markers = { '.flowconfig' },
+}
