e9a26f7544bf07e9cb3ac2f67d8fbaf12bcca788
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:12:28 +0100

    feat(facility_language_server): port config for neovim 0.11

---
+++ lsp/facility_language_server.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/FacilityApi/FacilityLanguageServer
+--
+-- Facility language server protocol (LSP) support.
+return {
+  cmd = { 'facility-language-server' },
+  filetypes = { 'fsd' },
+  root_markers = { '.git' },
+}
