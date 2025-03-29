89d8c7641211fdd660f304f97e02c0b26f7932fa
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:16:08 +0100

    feat(ginko_ls): port config for neovim 0.11

---
+++ lsp/ginko_ls.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---`ginko_ls` is meant to be a feature-complete language server for device-trees.
+-- Language servers can be used in many editors, such as Visual Studio Code, Emacs
+-- or Vim
+--
+-- Install `ginko_ls` from https://github.com/Schottkyc137/ginko and add it to path
+--
+-- `ginko_ls` doesn't require any configuration.
+return {
+  cmd = { 'ginko_ls' },
+  filetypes = { 'dts' },
+  root_markers = { '.git' },
+  settings = {},
+}
