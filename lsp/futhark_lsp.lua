882965905848039bbabbfeeb508f5c8756b6036f
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:14:59 +0100

    feat(futhark_lsp): port config for neovim 0.11

---
+++ lsp/futhark_lsp.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://github.com/diku-dk/futhark
+--
+-- Futhark Language Server
+--
+-- This language server comes with the futhark compiler and is run with the command
+-- ```
+-- futhark lsp
+-- ```
+return {
+  cmd = { 'futhark', 'lsp' },
+  filetypes = { 'futhark', 'fut' },
+  root_markers = { '.git' },
+}
