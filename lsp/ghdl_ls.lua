a8888aeb9433d7dae997e68b481b273023948aa2
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:15:46 +0100

    feat(ghdl_ls): port config for neovim 0.11

---
+++ lsp/ghdl_ls.lua
@@ -0,0 +1,13 @@
+---@brief
+---
+---https://github.com/ghdl/ghdl-language-server
+--
+-- A language server for VHDL, using ghdl as its backend.
+--
+-- `ghdl-ls` is part of pyghdl, for installation instructions see
+-- [the upstream README](https://github.com/ghdl/ghdl/tree/master/pyGHDL/lsp).
+return {
+  cmd = { 'ghdl-ls' },
+  filetypes = { 'vhdl' },
+  root_markers = { 'hdl-prj.json', '.git' },
+}
