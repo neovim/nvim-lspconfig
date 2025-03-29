0fabc65afcde2f3ef4e6fd671745cb1956ec1854
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:13:00 +0100

    feat(fennel_ls): port config for neovim 0.11

---
+++ lsp/fennel_ls.lua
@@ -0,0 +1,26 @@
+local util = require 'lspconfig.util'
+
+---@brief
+---
+---https://sr.ht/~xerool/fennel-ls/
+--
+-- A language server for fennel.
+--
+-- fennel-ls is configured using the closest file to your working directory named `flsproject.fnl`.
+-- All fennel-ls configuration options [can be found here](https://git.sr.ht/~xerool/fennel-ls/tree/HEAD/docs/manual.md#configuration).
+return {
+  cmd = { 'fennel-ls' },
+  filetypes = { 'fennel' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local has_fls_project_cfg = function(path)
+      local fnlpath = vim.fs.joinpath(path, 'flsproject.fnl')
+      return (vim.uv.fs_stat(fnlpath) or {}).type == 'file'
+    end
+    done_callback(util.search_ancestors(fname, has_fls_project_cfg) or vim.fs.root(0, '.git'))
+  end,
+  settings = {},
+  capabilities = {
+    offsetEncoding = { 'utf-8', 'utf-16' },
+  },
+}
