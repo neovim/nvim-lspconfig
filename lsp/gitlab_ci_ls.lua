f427fc077a9a1ad42078c3fc5e984e79afd553e3
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:16:32 +0100

    feat(gitlab_ci_ls): port config for neovim 0.11

---
+++ lsp/gitlab_ci_ls.lua
@@ -0,0 +1,24 @@
+local util = require 'lspconfig.util'
+
+local cache_dir = vim.uv.os_homedir() .. '/.cache/gitlab-ci-ls/'
+
+---@brief
+---
+---https://github.com/alesbrelih/gitlab-ci-ls
+--
+-- Language Server for Gitlab CI
+--
+-- `gitlab-ci-ls` can be installed via cargo:
+-- cargo install gitlab-ci-ls
+return {
+  cmd = { 'gitlab-ci-ls' },
+  filetypes = { 'yaml.gitlab' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    done_callback(util.root_pattern('.gitlab*', '.git')(fname))
+  end,
+  init_options = {
+    cache_path = cache_dir,
+    log_path = cache_dir .. '/log/gitlab-ci-ls.log',
+  },
+}
