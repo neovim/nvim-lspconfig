68a2904939f667ec4d75b4e0e3fb446c16c96f45
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 20:19:19 +0100

    feat(graphql): port config for neovim 0.11

---
+++ lsp/graphql.lua
@@ -0,0 +1,21 @@
+local util = require 'lspconfig.util'
+
+---@brief
+---
+---https://github.com/graphql/graphiql/tree/main/packages/graphql-language-service-cli
+--
+-- `graphql-lsp` can be installed via `npm`:
+--
+-- ```sh
+-- npm install -g graphql-language-service-cli
+-- ```
+--
+-- Note that you must also have [the graphql package](https://github.com/graphql/graphql-js) installed within your project and create a [GraphQL config file](https://the-guild.dev/graphql/config/docs).
+return {
+  cmd = { 'graphql-lsp', 'server', '-m', 'stream' },
+  filetypes = { 'graphql', 'typescriptreact', 'javascriptreact' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    done_callback(util.root_pattern('.graphqlrc*', '.graphql.config.*', 'graphql.config.*')(fname))
+  end,
+}
