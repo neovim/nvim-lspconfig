3191d1e56f65587e60afe909315e1acf71ad1176
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 12:45:05 +0100

    feat(dotls): port config for neovim 0.11

---
+++ lsp/dotls.lua
@@ -0,0 +1,13 @@
+---@brief
+---
+---https://github.com/nikeee/dot-language-server
+--
+-- `dot-language-server` can be installed via `npm`:
+-- ```sh
+-- npm install -g dot-language-server
+-- ```
+return {
+  cmd = { 'dot-language-server', '--stdio' },
+  filetypes = { 'dot' },
+  root_markers = { '.git' },
+}
