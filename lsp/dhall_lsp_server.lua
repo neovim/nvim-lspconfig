5b661938addc6e1aa7cdb950cc69789eeb93ce8f
TheRealLorenz <lollobellina@gmail.com>
2025-03-29 12:43:25 +0100

    feat(dhall_lsp_server): port config for neovim 0.11

---
+++ lsp/dhall_lsp_server.lua
@@ -0,0 +1,16 @@
+---@brief
+---
+---https://github.com/dhall-lang/dhall-haskell/tree/master/dhall-lsp-server
+--
+-- language server for dhall
+--
+-- `dhall-lsp-server` can be installed via cabal:
+-- ```sh
+-- cabal install dhall-lsp-server
+-- ```
+-- prebuilt binaries can be found [here](https://github.com/dhall-lang/dhall-haskell/releases).
+return {
+  cmd = { 'dhall-lsp-server' },
+  filetypes = { 'dhall' },
+  root_markers = { '.git' },
+}
