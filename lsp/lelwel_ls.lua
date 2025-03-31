45469ef70d566fe059a07bafc6ad890cc6b56370
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 18:53:41 +0200

    feat(lelwel_ls): port config for neovim 0.11

---
+++ lsp/lelwel_ls.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://github.com/0x2a-42/lelwel
+--
+-- Language server for lelwel grammars.
+--
+-- You can install `lelwel-ls` via cargo:
+-- ```sh
+-- cargo install --features="lsp" lelwel
+-- ```
+return {
+  cmd = { 'lelwel-ls' },
+  filetypes = { 'llw' },
+  root_markers = { '.git' },
+}
