a99b42871da3ef3c8c1d38d11eee36e3301398cb
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 19:13:38 +0200

    feat(marksman): port config for neovim 0.11

---
+++ lsp/marksman.lua
@@ -0,0 +1,17 @@
+local bin_name = 'marksman'
+local cmd = { bin_name, 'server' }
+
+---@brief
+---
+---https://github.com/artempyanykh/marksman
+--
+-- Marksman is a Markdown LSP server providing completion, cross-references, diagnostics, and more.
+--
+-- Marksman works on MacOS, Linux, and Windows and is distributed as a self-contained binary for each OS.
+--
+-- Pre-built binaries can be downloaded from https://github.com/artempyanykh/marksman/releases
+return {
+  cmd = cmd,
+  filetypes = { 'markdown', 'markdown.mdx' },
+  root_markers = { '.marksman.toml', '.git' },
+}
