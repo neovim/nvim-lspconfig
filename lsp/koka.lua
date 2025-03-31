0ffab09bbf6ed4b6aab2840ab151cc226e686068
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 18:51:54 +0200

    feat(koka): port config for neovim 0.11

---
+++ lsp/koka.lua
@@ -0,0 +1,9 @@
+---@brief
+---
+---https://koka-lang.github.io/koka/doc/index.html
+-- Koka is a functional programming language with effect types and handlers.
+return {
+  cmd = { 'koka', '--language-server', '--lsstdio' },
+  filetypes = { 'koka' },
+  root_markers = { '.git' },
+}
