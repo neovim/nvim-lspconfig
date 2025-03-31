596d5c9aa03a36a7851e60b52790932fd394741b
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 19:13:28 +0200

    feat(marko-js): port config for neovim 0.11

---
+++ lsp/marko-js.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://github.com/marko-js/language-server
+
+-- Using the Language Server Protocol to improve Marko's developer experience.
+
+-- Can be installed via npm:
+-- ```
+-- npm i -g @marko/language-server
+-- ```
+return {
+  cmd = { 'marko-language-server', '--stdio' },
+  filetypes = { 'marko' },
+  root_markers = { '.git' },
+}
