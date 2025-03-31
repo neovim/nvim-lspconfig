c215ca62ef8116ef3f9a206f47d6565c85a58346
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 18:55:40 +0200

    feat(luau_lsp): port config for neovim 0.11

---
+++ lsp/luau_lsp.lua
@@ -0,0 +1,18 @@
+---@brief
+---
+---https://github.com/JohnnyMorganz/luau-lsp
+--
+-- Language server for the [Luau](https://luau-lang.org/) language.
+--
+-- `luau-lsp` can be installed by downloading one of the release assets available at https://github.com/JohnnyMorganz/luau-lsp.
+--
+-- You might also have to set up automatic filetype detection for Luau files, for example like so:
+--
+-- ```vim
+-- autocmd BufRead,BufNewFile *.luau setf luau
+-- ```
+return {
+  cmd = { 'luau-lsp', 'lsp' },
+  filetypes = { 'luau' },
+  root_markers = { '.git' },
+}
