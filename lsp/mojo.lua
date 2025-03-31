57e0a28a62f3370251f93b6b163efab5a846770c
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 19:15:19 +0200

    feat(mojo): port config for neovim 0.11

---
+++ lsp/mojo.lua
@@ -0,0 +1,12 @@
+---@brief
+---
+---https://github.com/modularml/mojo
+--
+-- `mojo-lsp-server` can be installed [via Modular](https://developer.modular.com/download)
+--
+-- Mojo is a new programming language that bridges the gap between research and production by combining Python syntax and ecosystem with systems programming and metaprogramming features.
+return {
+  cmd = { 'mojo-lsp-server' },
+  filetypes = { 'mojo' },
+  root_markers = { '.git' },
+}
