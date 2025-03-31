aadfdeb0a19c1ef5a98533993d994a72fa57e18a
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 17:25:18 +0200

    feat(jedi_language_server): port config for neovim 0.11

---
+++ lsp/jedi_language_server.lua
@@ -0,0 +1,17 @@
+---@brief
+---
+---https://github.com/pappasam/jedi-language-server
+--
+-- `jedi-language-server`, a language server for Python, built on top of jedi
+return {
+  cmd = { 'jedi-language-server' },
+  filetypes = { 'python' },
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    '.git',
+  },
+}
