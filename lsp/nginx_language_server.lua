56ffd1ab72a761985b39f0e0b7cf1d19817292f1
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 19:33:40 +0200

    feat(nginx_language_server): port config for neovim 0.11

---
+++ lsp/nginx_language_server.lua
@@ -0,0 +1,14 @@
+---@brief
+---
+---https://pypi.org/project/nginx-language-server/
+--
+-- `nginx-language-server` can be installed via pip:
+--
+-- ```sh
+-- pip install -U nginx-language-server
+-- ```
+return {
+  cmd = { 'nginx-language-server' },
+  filetypes = { 'nginx' },
+  root_markers = { 'nginx.conf', '.git' },
+}
