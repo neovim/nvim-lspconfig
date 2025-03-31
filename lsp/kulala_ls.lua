30024f0cf6c40ec7533c4e4045f24cffa0d76cc9
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 18:52:16 +0200

    feat(kulala_ls): port config for neovim 0.11

---
+++ lsp/kulala_ls.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/mistweaverco/kulala-ls
+--
+-- A minimal language server for HTTP syntax.
+return {
+  cmd = { 'kulala-ls', '--stdio' },
+  filetypes = { 'http' },
+  root_markers = { '.git' },
+}
