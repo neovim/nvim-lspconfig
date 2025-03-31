c6c6b0d55cd1c2298441aaa0eafcd8f1212d011b
TheRealLorenz <lollobellina@gmail.com>
2025-03-31 19:35:43 +0200

    feat(nxls): port config for neovim 0.11

---
+++ lsp/nxls.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://github.com/nrwl/nx-console/tree/master/apps/nxls
+--
+-- nxls, a language server for Nx Workspaces
+--
+-- `nxls` can be installed via `npm`:
+-- ```sh
+-- npm i -g nxls
+-- ```
+return {
+  cmd = { 'nxls', '--stdio' },
+  filetypes = { 'json', 'jsonc' },
+  root_markers = { 'nx.json', '.git' },
+}
