7ed762ca04bab4fc6feb7ab1ba3cd92f93af5b96
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(phan): port config for neovim 0.11

---
+++ lsp/phan.lua
@@ -0,0 +1,32 @@
+local util = require 'lspconfig.util'
+
+local cmd = {
+  'phan',
+  '-m',
+  'json',
+  '--no-color',
+  '--no-progress-bar',
+  '-x',
+  '-u',
+  '-S',
+  '--language-server-on-stdin',
+  '--allow-polyfill-parser',
+}
+
+---@brief
+---
+---https://github.com/phan/phan
+--
+-- Installation: https://github.com/phan/phan#getting-started
+return {
+  cmd = cmd,
+  filetypes = { 'php' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local cwd = vim.uv.cwd()
+    local root = util.root_pattern('composer.json', '.git')(fname)
+
+    -- prefer cwd if root is a descendant
+    done_callback(util.path.is_descendant(cwd, root) and cwd or root)
+  end,
+}
