6904d316fecdc2d53b4d763d3147c9a8bef75512
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:36:05 +0200

    feat(roc_ls): port config for neovim 0.11

---
+++ lsp/roc_ls.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+---https://github.com/roc-lang/roc/tree/main/crates/language_server#roc_language_server
+--
+-- The built-in language server for the Roc programming language.
+-- [Installation](https://github.com/roc-lang/roc/tree/main/crates/language_server#installing)
+return {
+  cmd = { 'roc_language_server' },
+  filetypes = { 'roc' },
+  root_markers = { '.git' },
+}
