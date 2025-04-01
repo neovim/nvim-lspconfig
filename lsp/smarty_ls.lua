f5f7406225a49054edcce128a05810c7151ee575
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(smarty_ls): port config for neovim 0.11

---
+++ lsp/smarty_ls.lua
@@ -0,0 +1,36 @@
+local util = require 'lspconfig.util'
+
+---@brief
+---
+---https://github.com/landeaux/vscode-smarty-langserver-extracted
+--
+-- Language server for Smarty.
+--
+-- `smarty-language-server` can be installed via `npm`:
+--
+-- ```sh
+-- npm i -g vscode-smarty-langserver-extracted
+-- ```
+return {
+  cmd = { 'smarty-language-server', '--stdio' },
+  filetypes = { 'smarty' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local cwd = vim.uv.cwd()
+    local root = util.root_pattern('composer.json', '.git')(fname)
+
+    -- prefer cwd if root is a descendant
+    done_callback(util.path.is_descendant(cwd, root) and cwd or root)
+  end,
+  settings = {
+    smarty = {
+      pluginDirs = {},
+    },
+    css = {
+      validate = true,
+    },
+  },
+  init_options = {
+    storageDir = nil,
+  },
+}
