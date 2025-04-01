30d36dcb433e8a46c07ca329f8b7a10d9a24e720
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:36:05 +0200

    feat(rescriptls): port config for neovim 0.11

---
+++ lsp/rescriptls.lua
@@ -0,0 +1,45 @@
+---@brief
+---
+---https://github.com/rescript-lang/rescript-vscode/tree/master/server
+--
+-- ReScript Language Server can be installed via npm:
+-- ```sh
+-- npm install -g @rescript/language-server
+-- ```
+--
+-- See [package.json](https://github.com/rescript-lang/rescript-vscode/blob/master/package.json#L139)
+-- for init_options supported.
+--
+-- For example, in order to disable the `inlayHints` option:
+-- ```lua
+-- vim.lsp.config('rescriptls', {
+--   settings = {
+--     rescript = {
+--       settings = {
+--         inlayHints = { enable = false },
+--       },
+--     },
+--   })
+-- }
+-- ```
+return {
+  cmd = { 'rescript-language-server', '--stdio' },
+  filetypes = { 'rescript' },
+  root_markers = { 'bsconfig.json', 'rescript.json', '.git' },
+  settings = {},
+  init_options = {
+    extensionConfiguration = {
+      -- buggy, prompts much too often, superseded by incrementalTypechecking, below
+      askToStartBuild = false,
+
+      allowBuiltInFormatter = true, -- lower latency
+      incrementalTypechecking = { -- removes the need for external build process
+        enabled = true,
+        acrossFiles = true,
+      },
+      cache = { projectConfig = { enabled = true } }, -- speed up latency dramatically
+      codeLens = true,
+      inlayHints = { enable = true },
+    },
+  },
+}
