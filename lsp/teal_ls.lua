94a88903b20d571b6253bcb6fc2c4dfb69f86b78
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:40:55 +0200

    feat(teal_ls): port config for neovim 0.11

---
+++ lsp/teal_ls.lua
@@ -0,0 +1,22 @@
+---@brief
+---
+---https://github.com/teal-language/teal-language-server
+--
+-- Install with:
+-- ```
+-- luarocks install teal-language-server
+-- ```
+--
+-- Optional Command Args:
+-- * "--log-mode=by_date" - Enable logging in $HOME/.cache/teal-language-server. Log name will be date + pid of process
+-- * "--log-mode=by_proj_path" - Enable logging in $HOME/.cache/teal-language-server. Log name will be project path + pid of process
+-- * "--verbose=true" - Increases log level.  Does nothing unless log-mode is set
+return {
+  cmd = {
+    'teal-language-server',
+  },
+  filetypes = {
+    'teal',
+  },
+  root_markers = { 'tlconfig.lua' },
+}
