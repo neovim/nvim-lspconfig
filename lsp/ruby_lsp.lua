03dba7eec8068a9e39ab2203aae215a478287e7c
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:36:05 +0200

    feat(ruby_lsp): port config for neovim 0.11

---
+++ lsp/ruby_lsp.lua
@@ -0,0 +1,21 @@
+---@brief
+---
+---https://shopify.github.io/ruby-lsp/
+--
+-- This gem is an implementation of the language server protocol specification for
+-- Ruby, used to improve editor features.
+--
+-- Install the gem. There's no need to require it, since the server is used as a
+-- standalone executable.
+--
+-- ```sh
+-- gem install ruby-lsp
+-- ```
+return {
+  cmd = { 'ruby-lsp' },
+  filetypes = { 'ruby', 'eruby' },
+  root_markers = { 'Gemfile', '.git' },
+  init_options = {
+    formatter = 'auto',
+  },
+}
