8ac3c301a766ec7754be121a10fbe58b34511026
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:40:55 +0200

    feat(typst_lsp): port config for neovim 0.11

---
+++ lsp/typst_lsp.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/nvarner/typst-lsp
+--
+-- Language server for Typst.
+return {
+  cmd = { 'typst-lsp' },
+  filetypes = { 'typst' },
+  root_markers = { '.git' },
+}
