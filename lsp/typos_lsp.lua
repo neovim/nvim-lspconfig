e0500eae18d8891ca8978fbd78ca79f1c31687b7
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:40:55 +0200

    feat(typos_lsp): port config for neovim 0.11

---
+++ lsp/typos_lsp.lua
@@ -0,0 +1,13 @@
+---@brief
+---
+---https://github.com/crate-ci/typos
+-- https://github.com/tekumara/typos-lsp
+--
+-- A Language Server Protocol implementation for Typos, a low false-positive
+-- source code spell checker, written in Rust. Download it from the releases page
+-- on GitHub: https://github.com/tekumara/typos-lsp/releases
+return {
+  cmd = { 'typos-lsp' },
+  root_markers = { 'typos.toml', '_typos.toml', '.typos.toml' },
+  settings = {},
+}
