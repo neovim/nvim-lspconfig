0a9c9508c9b4bffc60593667d382c9771d8d197f
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(systemd_ls): port config for neovim 0.11

---
+++ lsp/systemd_ls.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://github.com/psacawa/systemd-language-server
+--
+-- `systemd-language-server` can be installed via `pip`:
+-- ```sh
+-- pip install systemd-language-server
+-- ```
+--
+-- Language Server for Systemd unit files
+return {
+  cmd = { 'systemd-language-server' },
+  filetypes = { 'systemd' },
+  root_markers = { '.git' },
+}
