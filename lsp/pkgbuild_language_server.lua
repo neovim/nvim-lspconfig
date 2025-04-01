a50a7a64d0ecd3bb3473a4b8baf06e486753e450
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(pkgbuild_language_server): port config for neovim 0.11

---
+++ lsp/pkgbuild_language_server.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/Freed-Wu/pkgbuild-language-server
+--
+-- Language server for ArchLinux/Windows Msys2's PKGBUILD.
+return {
+  cmd = { 'pkgbuild-language-server' },
+  filetypes = { 'PKGBUILD' },
+  root_markers = { '.git' },
+}
