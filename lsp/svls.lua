419ff895178b95c975d0ad7ddde0140352a2bc94
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(svls): port config for neovim 0.11

---
+++ lsp/svls.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://github.com/dalance/svls
+--
+-- Language server for verilog and SystemVerilog
+--
+-- `svls` can be installed via `cargo`:
+--  ```sh
+--  cargo install svls
+--  ```
+return {
+  cmd = { 'svls' },
+  filetypes = { 'verilog', 'systemverilog' },
+  root_markers = { '.git' },
+}
