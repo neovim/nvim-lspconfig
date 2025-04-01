a5242e7212d9c66cf3a257575ce6d37f41d91f53
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:40:55 +0200

    feat(tflint): port config for neovim 0.11

---
+++ lsp/tflint.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+---https://github.com/terraform-linters/tflint
+--
+-- A pluggable Terraform linter that can act as lsp server.
+-- Installation instructions can be found in https://github.com/terraform-linters/tflint#installation.
+return {
+  cmd = { 'tflint', '--langserver' },
+  filetypes = { 'terraform' },
+  root_markers = { '.terraform', '.git', '.tflint.hcl' },
+}
