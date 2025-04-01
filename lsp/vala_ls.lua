b5941a32082e346e8938dd467821e37f9c5cfc7e
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:43:09 +0200

    feat(vala_ls): port config for neovim 0.11

---
+++ lsp/vala_ls.lua
@@ -0,0 +1,35 @@
+local util = require 'lspconfig.util'
+
+local meson_matcher = function(path)
+  local pattern = 'meson.build'
+  local f = vim.fn.glob(table.concat({ path, pattern }, '/'))
+  if f == '' then
+    return nil
+  end
+  for line in io.lines(f) do
+    -- skip meson comments
+    if not line:match '^%s*#.*' then
+      local str = line:gsub('%s+', '')
+      if str ~= '' then
+        if str:match '^project%(' then
+          return path
+        else
+          break
+        end
+      end
+    end
+  end
+end
+
+---@brief
+---
+---https://github.com/Prince781/vala-language-server
+return {
+  cmd = { 'vala-language-server' },
+  filetypes = { 'vala', 'genie' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local root = util.search_ancestors(fname, meson_matcher)
+    done_callback(root or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1]))
+  end,
+}
