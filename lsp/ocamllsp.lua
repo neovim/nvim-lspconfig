255291c106ba7a0d922a9077db0cb071f242db64
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 12:29:21 +0200

    feat(ocamllsp): port config for neovim 0.11

---
+++ lsp/ocamllsp.lua
@@ -0,0 +1,36 @@
+local util = require 'lspconfig.util'
+
+local language_id_of = {
+  menhir = 'ocaml.menhir',
+  ocaml = 'ocaml',
+  ocamlinterface = 'ocaml.interface',
+  ocamllex = 'ocaml.ocamllex',
+  reason = 'reason',
+  dune = 'dune',
+}
+
+local get_language_id = function(_, ftype)
+  return language_id_of[ftype]
+end
+
+---@brief
+---
+---https://github.com/ocaml/ocaml-lsp
+--
+-- `ocaml-lsp` can be installed as described in [installation guide](https://github.com/ocaml/ocaml-lsp#installation).
+--
+-- To install the lsp server in a particular opam switch:
+-- ```sh
+-- opam install ocaml-lsp-server
+-- ```
+return {
+  cmd = { 'ocamllsp' },
+  filetypes = { 'ocaml', 'menhir', 'ocamlinterface', 'ocamllex', 'reason', 'dune' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    done_callback(
+      util.root_pattern('*.opam', 'esy.json', 'package.json', '.git', 'dune-project', 'dune-workspace')(fname)
+    )
+  end,
+  get_language_id = get_language_id,
+}
