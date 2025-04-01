1c4c40b625372749a964214a71749efc172df88c
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(sqls): port config for neovim 0.11

---
+++ lsp/sqls.lua
@@ -0,0 +1,17 @@
+---@brief
+---
+---https://github.com/sqls-server/sqls
+--
+-- ```lua
+-- vim.lsp.config('sqls', {
+--   cmd = {"path/to/command", "-config", "path/to/config.yml"};
+--   ...
+-- })
+-- ```
+-- Sqls can be installed via `go get github.com/sqls-server/sqls`. Instructions for compiling Sqls from the source can be found at [sqls-server/sqls](https://github.com/sqls-server/sqls).
+return {
+  cmd = { 'sqls' },
+  filetypes = { 'sql', 'mysql' },
+  root_markers = { 'config.yml' },
+  settings = {},
+}
