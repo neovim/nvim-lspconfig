02fb40fb571ce60d39285f51a2b77caca8d89c73
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(perlls): port config for neovim 0.11

---
+++ lsp/perlls.lua
@@ -0,0 +1,28 @@
+---@brief
+---
+---https://github.com/richterger/Perl-LanguageServer/tree/master/clients/vscode/perl
+--
+-- `Perl-LanguageServer`, a language server for Perl.
+--
+-- To use the language server, ensure that you have Perl::LanguageServer installed and perl command is on your path.
+return {
+  cmd = {
+    'perl',
+    '-MPerl::LanguageServer',
+    '-e',
+    'Perl::LanguageServer::run',
+    '--',
+    '--port 13603',
+    '--nostdio 0',
+  },
+  settings = {
+    perl = {
+      perlCmd = 'perl',
+      perlInc = ' ',
+      fileFilter = { '.pm', '.pl' },
+      ignoreDirs = '.git',
+    },
+  },
+  filetypes = { 'perl' },
+  root_markers = { '.git' },
+}
