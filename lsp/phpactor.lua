615a536545ab00293c3b08d63d9e29e23bdd79b4
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(phpactor): port config for neovim 0.11

---
+++ lsp/phpactor.lua
@@ -0,0 +1,19 @@
+local util = require 'lspconfig.util'
+
+---@brief
+---
+---https://github.com/phpactor/phpactor
+--
+-- Installation: https://phpactor.readthedocs.io/en/master/usage/standalone.html#global-installation
+return {
+  cmd = { 'phpactor', 'language-server' },
+  filetypes = { 'php' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local cwd = vim.uv.cwd()
+    local root = util.root_pattern('composer.json', '.git', '.phpactor.json', '.phpactor.yml')(fname)
+
+    -- prefer cwd if root is a descendant
+    done_callback(util.path.is_descendant(cwd, root) and cwd or root)
+  end,
+}
