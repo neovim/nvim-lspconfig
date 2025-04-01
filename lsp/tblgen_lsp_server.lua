2f5099aa647579ae081dfa404950cca768c3877e
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:40:55 +0200

    feat(tblgen_lsp_server): port config for neovim 0.11

---
+++ lsp/tblgen_lsp_server.lua
@@ -0,0 +1,23 @@
+local function get_command()
+  local cmd = { 'tblgen-lsp-server' }
+  local files = vim.fs.find('tablegen_compile_commands.yml', { path = vim.fn.expand('%:p:h'), upward = true })
+  if #files > 0 then
+    local file = files[1]
+    table.insert(cmd, '--tablegen-compilation-database=' .. vim.fs.dirname(file) .. '/tablegen_compile_commands.yml')
+  end
+
+  return cmd
+end
+
+---@brief
+---
+---https://mlir.llvm.org/docs/Tools/MLIRLSP/#tablegen-lsp-language-server--tblgen-lsp-server
+--
+-- The Language Server for the LLVM TableGen language
+--
+-- `tblgen-lsp-server` can be installed at the llvm-project repository (https://github.com/llvm/llvm-project)
+return {
+  cmd = get_command(),
+  filetypes = { 'tablegen' },
+  root_markers = { 'tablegen_compile_commands.yml', '.git' },
+}
