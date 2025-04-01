a8b32873fb865fc1a770561f943e39119f199da5
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(snakeskin_ls): port config for neovim 0.11

---
+++ lsp/snakeskin_ls.lua
@@ -0,0 +1,13 @@
+---@brief
+---
+---https://www.npmjs.com/package/@snakeskin/cli
+--
+-- `snakeskin cli` can be installed via `npm`:
+-- ```sh
+-- npm install -g @snakeskin/cli
+-- ```
+return {
+  cmd = { 'snakeskin-cli', 'lsp', '--stdio' },
+  filetypes = { 'ss' },
+  root_markers = { 'package.json' },
+}
