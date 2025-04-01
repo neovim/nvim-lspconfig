5acabb8cf377c247fa634c972ff1c7fa3e15c30c
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(pyre): port config for neovim 0.11

---
+++ lsp/pyre.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://pyre-check.org/
+--
+-- `pyre` a static type checker for Python 3.
+--
+-- `pyre` offers an extremely limited featureset. It currently only supports diagnostics,
+-- which are triggered on save.
+--
+-- Do not report issues for missing features in `pyre` to `lspconfig`.
+return {
+  cmd = { 'pyre', 'persistent' },
+  filetypes = { 'python' },
+  root_markers = { '.pyre_configuration' },
+}
