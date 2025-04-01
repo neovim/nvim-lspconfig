ec857b176cd2b907982ea330eae5a8519e998102
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(prosemd_lsp): port config for neovim 0.11

---
+++ lsp/prosemd_lsp.lua
@@ -0,0 +1,12 @@
+---@brief
+---
+---https://github.com/kitten/prosemd-lsp
+--
+-- An experimental LSP for Markdown.
+--
+-- Please see the manual installation instructions: https://github.com/kitten/prosemd-lsp#manual-installation
+return {
+  cmd = { 'prosemd-lsp', '--stdio' },
+  filetypes = { 'markdown' },
+  root_markers = { '.git' },
+}
