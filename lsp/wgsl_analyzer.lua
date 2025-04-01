4b9d1b9f9cdb85a5ba054b4fe1fe926ec1012bb6
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:43:24 +0200

    feat(wgsl_analyzer): port config for neovim 0.11

---
+++ lsp/wgsl_analyzer.lua
@@ -0,0 +1,14 @@
+---@brief
+---
+---https://github.com/wgsl-analyzer/wgsl-analyzer
+--
+-- `wgsl-analyzer` can be installed via `cargo`:
+-- ```sh
+-- cargo install --git https://github.com/wgsl-analyzer/wgsl-analyzer wgsl-analyzer
+-- ```
+return {
+  cmd = { 'wgsl-analyzer' },
+  filetypes = { 'wgsl' },
+  root_markers = { '.git' },
+  settings = {},
+}
