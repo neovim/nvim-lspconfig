32f0728d1a5e18f29b9266ef705ea2759c4e6124
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(perlpls): port config for neovim 0.11

---
+++ lsp/perlpls.lua
@@ -0,0 +1,19 @@
+---@brief
+---
+---https://github.com/FractalBoy/perl-language-server
+-- https://metacpan.org/pod/PLS
+--
+-- `PLS`, another language server for Perl.
+--
+-- To use the language server, ensure that you have PLS installed and that it is in your path
+return {
+  cmd = { 'pls' },
+  settings = {
+    perl = {
+      perlcritic = { enabled = false },
+      syntax = { enabled = true },
+    },
+  },
+  filetypes = { 'perl' },
+  root_markers = { '.git' },
+}
