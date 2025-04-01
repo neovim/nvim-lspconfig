9fb85e8818663e9272358e8e3bf20adcbc59c4b6
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:58 +0200

    feat(salt_ls): port config for neovim 0.11

---
+++ lsp/salt_ls.lua
@@ -0,0 +1,14 @@
+---@brief
+---
+---Language server for Salt configuration files.
+-- https://github.com/dcermak/salt-lsp
+--
+-- The language server can be installed with `pip`:
+-- ```sh
+-- pip install salt-lsp
+-- ```
+return {
+  cmd = { 'salt_lsp_server' },
+  filetypes = { 'sls' },
+  root_markers = { '.git' },
+}
