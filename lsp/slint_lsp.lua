9e83898606c04e8b47c9ea70dd42a2806102195a
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(slint_lsp): port config for neovim 0.11

---
+++ lsp/slint_lsp.lua
@@ -0,0 +1,22 @@
+---@brief
+---
+---https://github.com/slint-ui/slint
+-- `Slint`'s language server
+--
+-- You can build and install `slint-lsp` binary with `cargo`:
+-- ```sh
+-- cargo install slint-lsp
+-- ```
+--
+-- Vim does not have built-in syntax for the `slint` filetype at this time.
+--
+-- This can be added via an autocmd:
+--
+-- ```lua
+-- vim.cmd [[ autocmd BufRead,BufNewFile *.slint set filetype=slint ]]
+-- ```
+return {
+  cmd = { 'slint-lsp' },
+  filetypes = { 'slint' },
+  root_markers = { '.git' },
+}
