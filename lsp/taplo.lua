00070048c0b9059fe41951ac68a8e0a686526a27
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:40:55 +0200

    feat(taplo): port config for neovim 0.11

---
+++ lsp/taplo.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://taplo.tamasfe.dev/cli/usage/language-server.html
+--
+-- Language server for Taplo, a TOML toolkit.
+--
+-- `taplo-cli` can be installed via `cargo`:
+-- ```sh
+-- cargo install --features lsp --locked taplo-cli
+-- ```
+return {
+  cmd = { 'taplo', 'lsp', 'stdio' },
+  filetypes = { 'toml' },
+  root_markers = { '.git' },
+}
