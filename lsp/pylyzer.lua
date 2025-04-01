b87e5d2f345a17aa0a3d88ceb529571147581266
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(pylyzer): port config for neovim 0.11

---
+++ lsp/pylyzer.lua
@@ -0,0 +1,25 @@
+---@brief
+---
+---https://github.com/mtshiba/pylyzer
+--
+-- `pylyzer`, a fast static code analyzer & language server for Python.
+return {
+  cmd = { 'pylyzer', '--server' },
+  filetypes = { 'python' },
+  root_markers = {
+    'setup.py',
+    'tox.ini',
+    'requirements.txt',
+    'Pipfile',
+    'pyproject.toml',
+    '.git',
+  },
+  settings = {
+    python = {
+      diagnostics = true,
+      inlayHints = true,
+      smartCompletion = true,
+      checkOnType = false,
+    },
+  },
+}
