3f766837ed587ade98803678e4a60fb27e37cffb
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:58 +0200

    feat(scheme_langserver): port config for neovim 0.11

---
+++ lsp/scheme_langserver.lua
@@ -0,0 +1,15 @@
+local cmd = { 'scheme-langserver', '~/.scheme-langserver.log', 'enable', 'disable' }
+
+---@brief
+---
+---https://github.com/ufo5260987423/scheme-langserver
+-- `scheme-langserver`, a language server protocol implementation for scheme.
+-- And for nvim user, please add .sls to scheme file extension list.
+return {
+  cmd = cmd,
+  filetypes = { 'scheme' },
+  root_markers = {
+    'Akku.manifest',
+    '.git',
+  },
+}
