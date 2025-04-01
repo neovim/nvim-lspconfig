0010990721053905765b8d10780693b1d7f895c7
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:43:59 +0200

    feat(yang_lsp): port config for neovim 0.11

---
+++ lsp/yang_lsp.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/TypeFox/yang-lsp
+--
+-- A Language Server for the YANG data modeling language.
+return {
+  cmd = { 'yang-language-server' },
+  filetypes = { 'yang' },
+  root_markers = { '.git' },
+}
