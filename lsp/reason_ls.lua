252959e6450f06f89ddb1044dfeb9d48c5c7ec71
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:36:05 +0200

    feat(reason_ls): port config for neovim 0.11

---
+++ lsp/reason_ls.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---Reason language server
+--
+-- You can install reason language server from [reason-language-server](https://github.com/jaredly/reason-language-server) repository.
+return {
+  cmd = { 'reason-language-server' },
+  filetypes = { 'reason' },
+  root_markers = { 'bsconfig.json', '.git' },
+}
