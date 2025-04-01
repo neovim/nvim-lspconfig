27d765a047188ea3af3f1ab9f5032d7f929517b8
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(stylelint_lsp): port config for neovim 0.11

---
+++ lsp/stylelint_lsp.lua
@@ -0,0 +1,53 @@
+local util = require 'lspconfig.util'
+
+local root_file = {
+  '.stylelintrc',
+  '.stylelintrc.mjs',
+  '.stylelintrc.cjs',
+  '.stylelintrc.js',
+  '.stylelintrc.json',
+  '.stylelintrc.yaml',
+  '.stylelintrc.yml',
+  'stylelint.config.mjs',
+  'stylelint.config.cjs',
+  'stylelint.config.js',
+}
+
+root_file = util.insert_package_json(root_file, 'stylelint')
+
+---@brief
+---
+---https://github.com/bmatcuk/stylelint-lsp
+--
+-- `stylelint-lsp` can be installed via `npm`:
+--
+-- ```sh
+-- npm i -g stylelint-lsp
+-- ```
+--
+-- Can be configured by passing a `settings.stylelintplus` object to `stylelint_lsp.setup`:
+--
+-- ```lua
+-- vim.lsp.config('stylelint_lsp', {
+--   settings = {
+--     stylelintplus = {
+--       -- see available options in stylelint-lsp documentation
+--     }
+--   }
+-- })
+-- ```
+return {
+  cmd = { 'stylelint-lsp', '--stdio' },
+  filetypes = {
+    'astro',
+    'css',
+    'html',
+    'less',
+    'scss',
+    'sugarss',
+    'vue',
+    'wxss',
+  },
+  root_markers = root_file,
+  settings = {},
+}
