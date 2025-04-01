856a7e2c1ea5d91038629702612784e2ef489562
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:41:30 +0200

    feat(uvls): port config for neovim 0.11

---
+++ lsp/uvls.lua
@@ -0,0 +1,20 @@
+---@brief
+---
+---https://codeberg.org/caradhras/uvls
+-- Language server for UVL, written using tree sitter and rust.
+-- You can install the server easily using cargo:
+-- ```sh
+-- git clone https://codeberg.org/caradhras/uvls
+-- cd  uvls
+-- cargo install --path .
+-- ```
+-- Note: To activate properly nvim needs to know the uvl filetype.
+-- You can add it via:
+-- ```lua
+-- vim.cmd([[au BufRead,BufNewFile *.uvl setfiletype uvl]])
+-- ```
+return {
+  cmd = { 'uvls' },
+  filetypes = { 'uvl' },
+  root_markers = { '.git' },
+}
