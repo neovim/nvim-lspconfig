b5226d4f098a9fad7a1c1ebb5afbad630de46c0e
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:43:09 +0200

    feat(v_analyzer): port config for neovim 0.11

---
+++ lsp/v_analyzer.lua
@@ -0,0 +1,12 @@
+---@brief
+---
+---https://github.com/vlang/v-analyzer
+--
+-- V language server.
+--
+-- `v-analyzer` can be installed by following the instructions [here](https://github.com/vlang/v-analyzer#installation).
+return {
+  cmd = { 'v-analyzer' },
+  filetypes = { 'v', 'vsh', 'vv' },
+  root_markers = { 'v.mod', '.git' },
+}
