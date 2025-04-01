7ae785def90e31aa5353628e43e3bdc737de456d
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(starpls): port config for neovim 0.11

---
+++ lsp/starpls.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/withered-magic/starpls
+--
+-- `starpls` is an LSP implementation for Starlark. Installation instructions can be found in the project's README.
+return {
+  cmd = { 'starpls' },
+  filetypes = { 'bzl' },
+  root_markers = { 'WORKSPACE', 'WORKSPACE.bazel', 'MODULE.bazel' },
+}
