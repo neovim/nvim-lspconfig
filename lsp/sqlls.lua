2c3e131612fc28f1fba2d6f14a7975392afa44dd
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(sqlls): port config for neovim 0.11

---
+++ lsp/sqlls.lua
@@ -0,0 +1,12 @@
+---@brief
+---
+---https://github.com/joe-re/sql-language-server
+--
+-- This LSP can be installed via  `npm`. Find further instructions on manual installation of the sql-language-server at [joe-re/sql-language-server](https://github.com/joe-re/sql-language-server).
+-- <br>
+return {
+  cmd = { 'sql-language-server', 'up', '--method', 'stdio' },
+  filetypes = { 'sql', 'mysql' },
+  root_markers = { '.sqllsrc.json' },
+  settings = {},
+}
