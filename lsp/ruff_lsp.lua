e6a36a5bc5f881e3f24f4b53fb59adb198650047
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:36:05 +0200

    feat(ruff_lsp): port config for neovim 0.11

---
+++ lsp/ruff_lsp.lua
@@ -0,0 +1,28 @@
+---@brief
+---
+---https://github.com/astral-sh/ruff-lsp
+--
+-- A Language Server Protocol implementation for Ruff, an extremely fast Python linter and code transformation tool, written in Rust. It can be installed via pip.
+--
+-- ```sh
+-- pip install ruff-lsp
+-- ```
+--
+-- Extra CLI arguments for `ruff` can be provided via
+--
+-- ```lua
+-- vim.lsp.config('ruff_lsp', {
+--   init_options = {
+--     settings = {
+--       -- Any extra CLI arguments for `ruff` go here.
+--       args = {},
+--     }
+--   }
+-- })
+-- ```
+return {
+  cmd = { 'ruff-lsp' },
+  filetypes = { 'python' },
+  root_markers = { 'pyproject.toml', 'ruff.toml', '.git' },
+  settings = {},
+}
