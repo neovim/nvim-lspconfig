592115221035f246c7911cc81e70b850ee47763c
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:40:55 +0200

    feat(tilt_ls): port config for neovim 0.11

---
+++ lsp/tilt_ls.lua
@@ -0,0 +1,16 @@
+---@brief
+---
+---https://github.com/tilt-dev/tilt
+--
+-- Tilt language server.
+--
+-- You might need to add filetype detection manually:
+--
+-- ```vim
+-- autocmd BufRead Tiltfile setf=tiltfile
+-- ```
+return {
+  cmd = { 'tilt', 'lsp', 'start' },
+  filetypes = { 'tiltfile' },
+  root_markers = { '.git' },
+}
