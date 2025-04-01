f7cb98f47379d8c4ab3149991ee4045802cc281a
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 22:38:59 +0200

    feat(standardrb): port config for neovim 0.11

---
+++ lsp/standardrb.lua
@@ -0,0 +1,10 @@
+---@brief
+---
+---https://github.com/testdouble/standard
+--
+-- Ruby Style Guide, with linter & automatic code fixer.
+return {
+  cmd = { 'standardrb', '--lsp' },
+  filetypes = { 'ruby' },
+  root_markers = { 'Gemfile', '.git' },
+}
