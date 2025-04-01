c8d0f10685581f998743bb3318493b379950304b
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(puppet): port config for neovim 0.11

---
+++ lsp/puppet.lua
@@ -0,0 +1,26 @@
+---@brief
+---
+---LSP server for Puppet.
+--
+-- Installation:
+--
+-- - Clone the editor-services repository:
+--     https://github.com/puppetlabs/puppet-editor-services
+--
+-- - Navigate into that directory and run: `bundle install`
+--
+-- - Install the 'puppet-lint' gem: `gem install puppet-lint`
+--
+-- - Add that repository to $PATH.
+--
+-- - Ensure you can run `puppet-languageserver` from outside the editor-services directory.
+return {
+  cmd = { 'puppet-languageserver', '--stdio' },
+  filetypes = { 'puppet' },
+  root_markers = {
+    'manifests',
+    '.puppet-lint.rc',
+    'hiera.yaml',
+    '.git',
+  },
+}
