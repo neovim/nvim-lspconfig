226a4e934b4c2fa2fea0569b0fb50a2468e909c1
TheRealLorenz <lollobellina@gmail.com>
2025-04-01 18:27:55 +0200

    feat(prismals): port config for neovim 0.11

---
+++ lsp/prismals.lua
@@ -0,0 +1,18 @@
+---@brief
+---
+---Language Server for the Prisma JavaScript and TypeScript ORM
+--
+-- `@prisma/language-server` can be installed via npm
+-- ```sh
+-- npm install -g @prisma/language-server
+-- ```
+return {
+  cmd = { 'prisma-language-server', '--stdio' },
+  filetypes = { 'prisma' },
+  settings = {
+    prisma = {
+      prismaFmtBinPath = '',
+    },
+  },
+  root_markers = { '.git', 'package.json' },
+}
