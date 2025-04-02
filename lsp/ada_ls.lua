62831b87bd1554f5808bb16eb41f142052551113
Elie Richa <eliericha@users.noreply.github.com>
2025-04-02 15:39:18 +0200

    feat(ada_ls): alire.toml #3667

---
+++ lsp/ada_ls.lua
@@ -0,0 +1,29 @@
+local util = require 'lspconfig.util'
+
+---@brief
+---
+---https://github.com/AdaCore/ada_language_server
+--
+-- Installation instructions can be found [here](https://github.com/AdaCore/ada_language_server#Install).
+--
+-- Workspace-specific [settings](https://github.com/AdaCore/ada_language_server/blob/master/doc/settings.md) such as `projectFile` can be provided in a `.als.json` file at the root of the workspace.
+-- Alternatively, configuration may be passed as a "settings" object to `vim.lsp.config('ada_ls', {})`:
+--
+-- ```lua
+-- vim.lsp.config('ada_ls', {
+--     settings = {
+--       ada = {
+--         projectFile = "project.gpr";
+--         scenarioVariables = { ... };
+--       }
+--     }
+-- })
+-- ```
+return {
+  cmd = { 'ada_language_server' },
+  filetypes = { 'ada' },
+  root_dir = function(bufnr, done_callback)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    done_callback(util.root_pattern('Makefile', '.git', 'alire.toml', '*.gpr', '*.adc')(fname))
+  end,
+}
