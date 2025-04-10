e85989992536c2cbb8305c7756508cdb4119c258
TheRealLorenz <lollobellina@gmail.com>
2025-04-10 10:11:02 +0200

    feat(bqls): port config for neovim 0.11

---
+++ lsp/bqls.lua
@@ -0,0 +1,15 @@
+---@brief
+---
+---https://github.com/kitagry/bqls
+--
+-- The `bqls` BigQuery language server can be installed by running:
+--
+-- ```sh
+-- $ go install github.com/kitagry/bqls@latest
+-- ```
+return {
+  cmd = { 'bqls' },
+  filetypes = { 'sql' },
+  root_markers = { '.git' },
+  settings = {},
+}
