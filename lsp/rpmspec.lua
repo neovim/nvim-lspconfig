4dbc066802cba9ed08cd7f4c0c12a9901a7e61bf
TheRealLorenz <lollobellina@gmail.com>
2025-04-10 10:11:42 +0200

    feat(rpmspec): port config for neovim 0.11

---
+++ lsp/rpmspec.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+---https://github.com/dcermak/rpm-spec-language-server
+--
+-- Language server protocol (LSP) support for RPM Spec files.
+return {
+  cmd = { 'python3', '-mrpm_lsp_server', '--stdio' },
+  filetypes = { 'spec' },
+  root_markers = { '.git' },
+  settings = {},
+}
