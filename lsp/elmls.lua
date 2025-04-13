f122f0923bb44686d998b7ae3a1645d229aa2a13
TheRealLorenz <lollobellina@gmail.com>
2025-04-13 15:02:48 +0200

    feat(elmls): do not use `lspconfig.util`
    And fix a deprecation warning

--- lsp/elmls.lua
+++ lsp/elmls.lua
@@ -1,8 +1,3 @@
-local util = require 'lspconfig.util'
-local api = vim.api
-
-local elm_root_pattern = util.root_pattern 'elm.json'
-
 ---@brief
 ---
 -- https://github.com/elm-tooling/elm-language-server#installation
@@ -16,10 +11,10 @@ return {
   -- TODO(ashkan) if we comment this out, it will allow elmls to operate on elm.json. It seems like it could do that, but no other editor allows it right now.
   filetypes = { 'elm' },
   root_dir = function(bufnr, on_dir)
-    local fname = api.nvim_buf_get_name(bufnr)
-    local filetype = api.nvim_buf_get_option(0, 'filetype')
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local filetype = vim.api.nvim_get_option_value('filetype', { buf = 0 })
     if filetype == 'elm' or (filetype == 'json' and fname:match 'elm%.json$') then
-      on_dir(elm_root_pattern(fname))
+      on_dir(vim.fs.root(bufnr, 'elm.json'))
       return
     end
     on_dir(nil)
