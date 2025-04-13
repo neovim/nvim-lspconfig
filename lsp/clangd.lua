c040056bab994d0f65e7ac2fad97ef8a522f0806
notfirefox <138280734+notfirefox@users.noreply.github.com>
2025-04-13 13:50:42 +0200

    fix(clangd): improve indentation of snippets

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -88,4 +88,29 @@ return {
       symbol_info()
     end, { desc = 'Show symbol info' })
   end,
+  on_init = function(client)
+    local request = client.rpc.request
+    function client.rpc.request(method, params, handler, ...)
+      if method ~= 'textDocument/completion' then
+        return request(method, params, handler, ...)
+      end
+      local new_handler = function(...)
+        local err, result = ...
+        if err or not result then
+          return handler(...)
+        end
+        local items = result.items or result
+        for _, item in ipairs(items) do
+          local kind = vim.lsp.protocol.CompletionItemKind
+          if item.kind == kind.Snippet then
+            local text = item.textEdit.newText
+            text = text:gsub('{\n', '{\n\t')
+            item.textEdit.newText = text
+          end
+        end
+        return handler(...)
+      end
+      return request(method, params, new_handler, ...)
+    end
+  end,
 }
