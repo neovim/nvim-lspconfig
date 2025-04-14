5cc066e9c05e8a3d79a88479ca79fd232e0febaa
Vo Quang Chien <voquangchien.dev@proton.me>
2025-04-14 23:07:00 +0700

    feat: sqruff

---
+++ lsp/sqruff.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+-- https://github.com/quarylabs/sqruff
+--
+-- `sqruff` can be installed by following the instructions [here](https://github.com/quarylabs/sqruff?tab=readme-ov-file#installation)
+--
+return {
+  cmd = { 'sqruff', 'lsp' },
+  filetypes = { 'sql' },
+  root_markers = { '.sqruff', '.git' },
+}
