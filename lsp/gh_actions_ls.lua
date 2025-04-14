aa49d068ae52e8e8ea4ee8908e5ae35c3c28b3c3
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-04-14 16:43:17 +0200

    fix(gh_actions_ls): workspace_required=true #3722
    see https://github.com/neovim/nvim-lspconfig/pull/3713#issuecomment-2801407962
    (Note that `workspace_required` requires nightly or the upcoming nvim 0.11.1)

--- lsp/gh_actions_ls.lua
+++ lsp/gh_actions_ls.lua
@@ -15,12 +15,14 @@
 -- ```
 return {
   cmd = { 'gh-actions-language-server', '--stdio' },
-  filetypes = { 'yaml' }, -- the `root_markers` prevent attaching to every yaml file
+  -- the `root_markers` with `workspace_required` prevent attaching to every yaml file
+  filetypes = { 'yaml' },
   root_markers = {
     '.github/workflows',
     '.forgejo/workflows',
     '.gitea/workflows',
   },
+  workspace_required = true,
   capabilities = {
     workspace = {
       didChangeWorkspaceFolders = {
