9c4e003d069be766ac56531a97377b895c57c6b6
TheRealLorenz <lollobellina@gmail.com>
2025-04-14 15:01:51 +0200

    feat(ols): do not use `lspconfig.util`

--- lsp/ols.lua
+++ lsp/ols.lua
@@ -4,13 +4,18 @@
 ---
 --- `Odin Language Server`.
 
-local util = require 'lspconfig.util'
-
 return {
   cmd = { 'ols' },
   filetypes = { 'odin' },
   root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(util.root_pattern('ols.json', '.git', '*.odin')(fname))
+    on_dir(vim.fs.root(bufnr, function(name, _)
+      local patterns = { 'ols.json', '.git', '*.odin' }
+      for _, pattern in ipairs(patterns) do
+        if vim.glob.to_lpeg(pattern):match(name) ~= nil then
+          return true
+        end
+      end
+      return false
+    end))
   end,
 }
