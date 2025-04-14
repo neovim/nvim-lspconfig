a7303d0978a3dc7dcb5be3070bc6384b95b05808
TheRealLorenz <lollobellina@gmail.com>
2025-04-14 15:01:51 +0200

    feat(solc): do not use `lspconfig.util`

--- lsp/solc.lua
+++ lsp/solc.lua
@@ -4,13 +4,18 @@
 ---
 --- solc is the native language server for the Solidity language.
 
-local util = require 'lspconfig.util'
-
 return {
   cmd = { 'solc', '--lsp' },
   filetypes = { 'solidity' },
   root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(util.root_pattern('hardhat.config.*', '.git')(fname))
+    on_dir(vim.fs.root(bufnr, function(name, _)
+      local patterns = { 'hardhat.config.*', '.git' }
+      for _, pattern in ipairs(patterns) do
+        if vim.glob.to_lpeg(pattern):match(name) ~= nil then
+          return true
+        end
+      end
+      return false
+    end))
   end,
 }
