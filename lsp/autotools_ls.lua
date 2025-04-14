dfdbec82987d2a4b37e199d0b9ba07fcbeff830b
TheRealLorenz <lollobellina@gmail.com>
2025-04-14 15:01:51 +0200

    feat(autotools_ls): do not use `lspconfig.util`

--- lsp/autotools_ls.lua
+++ lsp/autotools_ls.lua
@@ -9,15 +9,19 @@
 ---
 --- Language server for autoconf, automake and make using tree sitter in python.
 
-local util = require 'lspconfig.util'
-
 local root_files = { 'configure.ac', 'Makefile', 'Makefile.am', '*.mk' }
 
 return {
   cmd = { 'autotools-language-server' },
   filetypes = { 'config', 'automake', 'make' },
   root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(util.root_pattern(unpack(root_files))(fname))
+    on_dir(vim.fs.root(bufnr, function(name, _)
+      for _, pattern in ipairs(root_files) do
+        if vim.glob.to_lpeg(pattern):match(name) ~= nil then
+          return true
+        end
+      end
+      return false
+    end))
   end,
 }
