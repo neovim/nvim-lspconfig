8fa35082f4723c5b73198643fc3b50c856271a16
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-04-15 15:16:25 +0200

    feat(biome): vim.lsp.config support, w/ workspace_required & root_dir

---
+++ lsp/biome.lua
@@ -0,0 +1,36 @@
+---@brief
+-- https://biomejs.dev
+--
+-- Toolchain of the web. [Successor of Rome](https://biomejs.dev/blog/annoucing-biome).
+--
+-- ```sh
+-- npm install [-g] @biomejs/biome
+-- ```
+
+local util = require 'lspconfig.util'
+
+return {
+  cmd = { 'biome', 'lsp-proxy' },
+  filetypes = {
+    'astro',
+    'css',
+    'graphql',
+    'javascript',
+    'javascriptreact',
+    'json',
+    'jsonc',
+    'svelte',
+    'typescript',
+    'typescript.tsx',
+    'typescriptreact',
+    'vue',
+  },
+  workspace_required = true,
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local root_files = { 'biome.json', 'biome.jsonc' }
+    root_files = util.insert_package_json(root_files, 'biome', fname)
+    local root_dir = vim.fs.dirname(vim.fs.find(root_files, { path = fname, upward = true })[1])
+    on_dir(root_dir)
+  end,
+}
