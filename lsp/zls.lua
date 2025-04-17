1afe49af77ba4e1f0dd6afc46a447e90516ccf02
cbackas <git@zac.gg>
2025-04-17 10:43:08 -0500

    chore: remove .git root_markers from newly created lsp/astro.lua and lsp/zls.lua

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -6,6 +6,6 @@
 return {
   cmd = { 'zls' },
   filetypes = { 'zig', 'zir' },
-  root_markers = { 'zls.json', 'build.zig', '.git' },
+  root_markers = { 'zls.json', 'build.zig' },
   workspace_required = false,
 }
