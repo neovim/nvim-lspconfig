8ec13272adff99d3d2249f669853d78c7e973d3a
glepnir <glephunter@gmail.com>
2025-04-17 14:09:55 +0800

    fix(astro): using vim.fs.jointhpath concat path (#3741)
    Problem: hardcord path symbol in get_typescript_server_path
    
    Solution: using vim.fs.joinpath to concat path

--- lsp/astro.lua
+++ lsp/astro.lua
@@ -1,6 +1,6 @@
 local function get_typescript_server_path(root_dir)
   local project_root = vim.fs.dirname(vim.fs.find('node_modules', { path = root_dir, upward = true })[1])
-  return project_root and (project_root .. '/node_modules/typescript/lib') or ''
+  return project_root and vim.fs.joinpath(project_root, 'node_modules', 'typescript', 'lib') or ''
 end
 
 ---@brief
