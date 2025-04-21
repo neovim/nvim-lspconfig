dd389cd82e87e3031ff048c56bf48e54882635c5
Alexej Kowalew <616b2f@gmail.com>
2025-04-21 13:30:52 +0200

    refactor(roslyn_ls): config

--- lsp/roslyn_ls.lua
+++ lsp/roslyn_ls.lua
@@ -1,4 +1,9 @@
 --- borrowed some source from: https://github.com/seblj/roslyn.nvim
+-- config that activates keymaps and enables snippet support
+---comment
+
+local uv = vim.uv
+local fs = vim.fs
 
 ---@param client vim.lsp.Client
 ---@param target string
@@ -26,91 +31,8 @@ local function on_init_project(client, project_files)
   end)
 end
 
-local function on_attach(client, bufnr)
-  -- enable inlay hints if LSP server supports it
-  if client.supports_method("textDocument/inlayHint") then
-    vim.lsp.inlay_hint.enable(true, { bufnr=bufnr })
-  end
-
-  -- Set autocommands conditional on server_capabilities
-  if client.supports_method("textDocument/documentHighlight") then
-    vim.api.nvim_exec2([[
-    augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-    ]],
-    {output=true})
-  end
-
-  -- enable semantic tokens highligting hints
-  if client.supports_method("textDocument/semanticTokens") then
-    client.server_capabilities.semanticTokensProvider = true
-  end
-
-  local bufname = vim.api.nvim_buf_get_name(bufnr)
-  -- don't try to find sln or csproj for files from libraries
-  -- outside of the project
-  if not bufname:match("^/tmp/MetadataAsSource/") then
-    -- try find solutions root first
-    local root_dir = nil
-    root_dir = vim.fs.root(bufnr, function (name, path)
-      local match = name:match("%.sln$") ~= nil
-      if match then
-        local sln_file = vim.fs.joinpath(path, name)
-        on_init_sln(client, sln_file)
-      end
-      return match
-    end)
-
-    if not root_dir then
-      -- try find solutions root first
-      root_dir = vim.fs.root(bufnr, function (name, path)
-        local match = name:match("%.csproj$") ~= nil
-        if match then
-          local csproj_file = vim.fs.joinpath(path, name)
-          on_init_project(client, {csproj_file})
-        end
-        return match
-      end)
-    end
-
-    assert(root_dir, "no solution and no csproj found")
-
-    client.root_dir = root_dir
-  end
-end
-
-
-local function start_roslyn_server(pipe_name)
-    vim.system({
-      "./Microsoft.CodeAnalysis.LanguageServer",
-      -- "dotnet",
-      -- "Microsoft.CodeAnalysis.LanguageServer.dll",
-      "--logLevel",
-      "Trace",
-      "--extensionLogDirectory",
-      "/var/home/ak/devel/roslyn/logs/",
-      "--pipe",
-      pipe_name
-    },
-    { cwd = "/var/home/ak/devel/roslyn/artifacts/LanguageServer/Release/net8.0/linux-x64/" })
-    vim.uv.sleep(1500)
-end
-
 local function roslyn_handlers()
   return {
-        -- ["client/registerCapability"] = function(err, res, ctx)
-        --     if not roslyn_config.filewatching then
-        --         for _, reg in ipairs(res.registrations) do
-        --             if reg.method == "workspace/didChangeWatchedFiles" then
-        --                 reg.registerOptions.watchers = {}
-        --             end
-        --         end
-        --     end
-        --     return vim.lsp.handlers["client/registerCapability"](err, res, ctx)
-        -- end,
         ["workspace/projectInitializationComplete"] = function(_, _, ctx)
             vim.notify("Roslyn project initialization complete", vim.log.levels.INFO, { title = "roslyn.nvim" })
 
@@ -158,15 +80,69 @@ end
 
 ---@type vim.lsp.ClientConfig
 return {
-  name = "roslynlsp",
+  name = "roslyn_ls",
   offset_encoding = 'utf-8',
   ---@param dispatchers vim.lsp.rpc.Dispatchers
   cmd = function (dispatchers)
     local pipe_name = "/tmp/422df9c8340645ba8966061884b388aa.sock"
-    start_roslyn_server(pipe_name)
+    vim.system({
+        "roslyn_ls", -- or provide the location of dll manually "dotnet", "Microsoft.CodeAnalysis.LanguageServer.dll",
+        "--logLevel",
+        "Trace",
+        "--extensionLogDirectory", -- this property is required by the server
+        fs.joinpath(uv.os_tmpdir(), 'roslyn_ls/logs'),
+        "--pipe",
+        pipe_name
+      })
+    uv.sleep(1500)
     return vim.lsp.rpc.connect(pipe_name)(dispatchers)
   end,
   filetypes = { "cs" },
+  root_markers = { '.sln', '.csproj' },
+  handlers = roslyn_handlers(),
+  on_attach = function (client, bufnr)
+    -- enable inlay hints if LSP server supports it
+    if client.supports_method("textDocument/inlayHint") then
+      vim.lsp.inlay_hint.enable(true, { bufnr=bufnr })
+    end
+
+    -- enable semantic tokens highligting hints
+    if client.supports_method("textDocument/semanticTokens") then
+      client.server_capabilities.semanticTokensProvider = true
+    end
+
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    -- don't try to find sln or csproj for files from libraries
+    -- outside of the project
+    if not bufname:match("^" .. fs.joinpath("/tmp/MetadataAsSource/")) then
+      -- try find solutions root first
+      local root_dir = nil
+      root_dir = fs.root(bufnr, function (name, path)
+        local match = name:match("%.sln$") ~= nil
+        if match then
+          local sln_file = fs.joinpath(path, name)
+          on_init_sln(client, sln_file)
+        end
+        return match
+      end)
+
+      if not root_dir then
+        -- try find solutions root first
+        root_dir = fs.root(bufnr, function (name, path)
+          local match = name:match("%.csproj$") ~= nil
+          if match then
+            local csproj_file = fs.joinpath(path, name)
+            on_init_project(client, {csproj_file})
+          end
+          return match
+        end)
+      end
+
+      assert(root_dir, "no solution and no csproj found")
+
+      client.root_dir = root_dir
+    end
+  end,
   capabilities = {
       -- HACK: Enable filewatching to later just not watch any files
       -- This is to not make the server watch files and make everything super slow in certain situations
@@ -184,35 +160,6 @@ return {
           },
       },
   },
-  handlers = roslyn_handlers(),
-  on_attach = on_attach,
-  -- on_init = function(client, initialize_result)
-  --
-  --   -- vim.fs.dir(client.root_dir, )
-  --   -- local root_dir = nil
-  --   -- root_dir = vim.fs.root(0, function (name, _)
-  --   --   local match = name:match("%.sln$") ~= nil
-  --   --   if match then
-  --   --     on_init_sln(client, name)
-  --   --   end
-  --   --   return match
-  --   -- end)
-  --   --
-  --   -- if not root_dir then
-  --   --   -- try find solutions root first
-  --   --   root_dir = vim.fs.root(0, function (name, _)
-  --   --     local match = name:match("%.csproj$") ~= nil
-  --   --     if match then
-  --   --       on_init_project(client, {name})
-  --   --     end
-  --   --     return match
-  --   --   end)
-  --   -- end
-  --
-  --   -- local lsp_commands = require("roslyn.lsp_commands")
-  --   -- lsp_commands.fix_all_code_action(client)
-  --   -- lsp_commands.nested_code_action(client)
-  -- end,
   settings = {
     ["csharp|background_analysis"] = {
       dotnet_analyzer_diagnostics_scope = "fullSolution",
