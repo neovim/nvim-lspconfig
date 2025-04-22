373717c637cdd6c274a2affa8a5bda6671ee6ac1
glepnir <glephunter@gmail.com>
2025-04-22 19:50:07 +0800

    fix(mdx_analyzer): remove invalid field and update brief

--- lsp/mdx_analyzer.lua
+++ lsp/mdx_analyzer.lua
@@ -1,7 +1,7 @@
 ---@brief
----https://github.com/mdx-js/mdx-analyzer
+--- https://github.com/mdx-js/mdx-analyzer
 ---
----`mdx-analyzer`, a language server for MDX
+--- `mdx-analyzer`, a language server for MDX
 
 local function get_typescript_server_path(root_dir)
   local project_root = vim.fs.dirname(vim.fs.find('node_modules', { path = root_dir, upward = true })[1])
@@ -12,7 +12,6 @@ return {
   cmd = { 'mdx-language-server', '--stdio' },
   filetypes = { 'mdx' },
   root_markers = { 'package.json' },
-  single_file_support = true,
   settings = {},
   init_options = {
     typescript = {},
