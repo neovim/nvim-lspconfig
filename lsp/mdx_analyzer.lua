256a1afc577cdb2308c9a5726e607cf0de9cfdb1
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-04-22 17:49:46 +0600

    fix(mdx_analyzer): put brief on top of file and use workspace_required instead of single_file_support

--- lsp/mdx_analyzer.lua
+++ lsp/mdx_analyzer.lua
@@ -1,18 +1,18 @@
-local function get_typescript_server_path(root_dir)
-  local project_root = vim.fs.dirname(vim.fs.find('node_modules', { path = root_dir, upward = true })[1])
-  return project_root and vim.fs.joinpath(project_root, 'node_modules', 'typescript', 'lib') or ''
-end
-
 ---@brief
 ---https://github.com/mdx-js/mdx-analyzer
 ---
 ---`mdx-analyzer`, a language server for MDX
 
+local function get_typescript_server_path(root_dir)
+  local project_root = vim.fs.dirname(vim.fs.find('node_modules', { path = root_dir, upward = true })[1])
+  return project_root and vim.fs.joinpath(project_root, 'node_modules', 'typescript', 'lib') or ''
+end
+
 return {
   cmd = { 'mdx-language-server', '--stdio' },
   filetypes = { 'mdx' },
   root_markers = { 'package.json' },
-  single_file_support = true,
+  workspace_required = false,
   settings = {},
   init_options = {
     typescript = {},
