05dd2fa13c0fb39bc0188ae99d78dee6bf99d620
Justin M. Keyes <justinkz@gmail.com>
2025-04-27 01:00:16 +0200

    docs: cleanup

--- lsp/volar.lua
+++ lsp/volar.lua
@@ -64,10 +64,10 @@
 ---       tsdk = '/path/to/node_modules/typescript/lib'
 ---     }
 ---   },
----   on_new_config = function(new_config, new_root_dir)
+---   before_init = function(params, config)
 ---     local lib_path = vim.fs.find('node_modules/typescript/lib', { path = new_root_dir, upward = true })[1]
 ---     if lib_path then
----       new_config.init_options.typescript.tsdk = lib_path
+---       config.init_options.typescript.tsdk = lib_path
 ---     end
 ---   end
 --- })
