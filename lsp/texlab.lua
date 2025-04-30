3d69858b600c51131600d03c236a2afb40a64cdd
Philipp Joram <phijor@users.noreply.github.com>
2025-04-30 13:15:06 +0200

    fix(texlab): add "latexmkrc"  root marker for Latexmk projects #3810
    Problem:
    The root directory of Latexmk-based projects is not detected when the
    local configuration file is called `latexmkrc` (without leading `.`).
    
    Solution:
    Both `.latexmkrc` and `latexmkrc` are valid names for local Latexmk
    configuration files [1]. Add `latexmk` to the list of possible root
    markers.
    
    [1]: https://mgeier.github.io/latexmk.html#local-configuration-files

--- lsp/texlab.lua
+++ lsp/texlab.lua
@@ -164,7 +164,7 @@ end
 return {
   cmd = { 'texlab' },
   filetypes = { 'tex', 'plaintex', 'bib' },
-  root_markers = { '.git', '.latexmkrc', '.texlabroot', 'texlabroot', 'Tectonic.toml' },
+  root_markers = { '.git', '.latexmkrc', 'latexmkrc', '.texlabroot', 'texlabroot', 'Tectonic.toml' },
   settings = {
     texlab = {
       rootDirectory = nil,
