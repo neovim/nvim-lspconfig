26287d7c39a477a05b6b120c4dc0331349c9816a
Justin M. Keyes <justinkz@gmail.com>
2025-05-09 21:01:30 -0400

    refactor: generalize insert_package_json()

--- lsp/tailwindcss.lua
+++ lsp/tailwindcss.lua
@@ -117,7 +117,7 @@ return {
     }
     local fname = vim.api.nvim_buf_get_name(bufnr)
     root_files = util.insert_package_json(root_files, 'tailwindcss', fname)
-    root_files = util.insert_mix_exs(root_files, 'tailwind', fname)
+    root_files = util.root_markers_with_field(root_files, { 'mix.lock' }, 'tailwind', fname)
     on_dir(vim.fs.dirname(vim.fs.find(root_files, { path = fname, upward = true })[1]))
   end,
 }
