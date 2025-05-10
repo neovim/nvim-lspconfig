78e5df8ab217df8599ec8bf7180ee6c88b2078cd
Daniel Sinev <daniel.sinev@gmail.com>
2025-05-10 15:15:16 +0200

    feat(matlab_ls): vim.lsp.config support

---
+++ lsp/matlab_ls.lua
@@ -0,0 +1,25 @@
+---@brief
+---
+--- https://github.com/mathworks/MATLAB-language-server
+---
+--- MATLAB® language server implements the Microsoft® Language Server Protocol for the MATLAB language.
+return {
+  cmd = { 'matlab-language-server', '--stdio' },
+  filetypes = { 'matlab' },
+  root_dir = function(_, callback)
+    local root_dir = vim.fs.root(0, '.git')
+    if root_dir then
+      callback(root_dir)
+    else
+      callback(vim.env.PWD)
+    end
+  end,
+  settings = {
+    MATLAB = {
+      indexWorkspace = true,
+      installPath = '',
+      matlabConnectionTiming = 'onStart',
+      telemetry = true,
+    },
+  },
+}
