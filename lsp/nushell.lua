9df2b180169e5b3d95a40d4d8da2af4bc9ae7559
Bahex <Bahex@users.noreply.github.com>
2025-05-22 17:44:02 +0300

    refactor: update parameter name for consistency and readability

--- lsp/nushell.lua
+++ lsp/nushell.lua
@@ -6,7 +6,7 @@
 return {
   cmd = { 'nu', '--lsp' },
   filetypes = { 'nu' },
-  root_dir = function(bufnr, cb)
-    cb(vim.fs.root(bufnr, { '.git' }) or vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)))
+  root_dir = function(bufnr, on_dir)
+    on_dir(vim.fs.root(bufnr, { '.git' }) or vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)))
   end,
 }
