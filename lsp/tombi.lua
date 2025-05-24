b489e15706225fd15913dc258d1299f886f6271c
yassun7010 <yassun7010@outlook.com>
2025-05-25 03:04:58 +0900

    feat(lsp): add Tombi language server configuration

---
+++ lsp/tombi.lua
@@ -0,0 +1,11 @@
+---@brief
+---
+--- https://tombi-toml.github.io/tombi/
+---
+--- Language server for Tombi, a TOML toolkit.
+---
+return {
+  cmd = { 'tombi', 'lsp' },
+  filetypes = { 'toml' },
+  root_markers = { 'tombi.toml', '.git' },
+}
