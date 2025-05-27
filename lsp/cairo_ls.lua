d2782075adc632870e007317031701be66097025
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2025-05-27 14:36:56 +0200

    fix(cairo_ls): update command #3873

--- lsp/cairo_ls.lua
+++ lsp/cairo_ls.lua
@@ -12,7 +12,7 @@
 --- *cairo-language-server is still under active development, some features might not work yet !*
 return {
   init_options = { hostInfo = 'neovim' },
-  cmd = { 'scarb-cairo-language-server', '/C', '--node-ipc' },
+  cmd = { 'scarb', 'cairo-language-server', '/C', '--node-ipc' },
   filetypes = { 'cairo' },
   root_markers = { 'Scarb.toml', 'cairo_project.toml', '.git' },
 }
