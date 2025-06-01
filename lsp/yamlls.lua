c30a661a1f4c270f542eaf861f3eb726bb9baa69
qvalentin <36446499+qvalentin@users.noreply.github.com>
2025-06-01 20:31:19 +0200

    feat(helm_ls,yamlls): support custom filetype for helm-ls #3881
    this adds config for starting helm_ls and yamlls with the new filetype
    yaml.helm-values that will be added to the neovim helm_ls extension by
    https://github.com/qvalentin/helm-ls.nvim/pull/8
    
    Files of this type should have both yamlls and helm-ls running.
    This is required because helm-ls now supports editing values.yaml files
    but should only be active for certain yaml files.

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -60,7 +60,7 @@
 --- ```
 return {
   cmd = { 'yaml-language-server', '--stdio' },
-  filetypes = { 'yaml', 'yaml.docker-compose', 'yaml.gitlab' },
+  filetypes = { 'yaml', 'yaml.docker-compose', 'yaml.gitlab', 'yaml.helm-values' },
   root_markers = { '.git' },
   settings = {
     -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
