f859f3ae6dd97736f57959332c70b8958334b9a3
Bruce Wen <bruce.wen@ericsson.com>
2025-06-18 20:11:05 +0200

    fix(powershell_es): update LogLevel to Information

--- lsp/powershell_es.lua
+++ lsp/powershell_es.lua
@@ -44,7 +44,7 @@ return {
     local shell = vim.lsp.config.powershell_es.shell or 'pwsh'
 
     local command_fmt =
-      [[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Normal]]
+      [[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Information]]
     local command = command_fmt:format(bundle_path, bundle_path, temp_path, temp_path)
     local cmd = { shell, '-NoLogo', '-NoProfile', '-Command', command }
 
