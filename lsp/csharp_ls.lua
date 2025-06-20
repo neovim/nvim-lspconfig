f02f631f16ca72c1349caefdecbbb7e27d7edf56
Joseph Guenther <joseph@codingcool.ca>
2025-06-20 15:38:47 -0400

    fix(csharp_ls): add missing slnx root pattern

--- lsp/csharp_ls.lua
+++ lsp/csharp_ls.lua
@@ -14,7 +14,7 @@ return {
   cmd = { 'csharp-ls' },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(util.root_pattern '*.sln'(fname) or util.root_pattern '*.csproj'(fname))
+    on_dir(util.root_pattern '*.sln'(fname) or util.root_pattern '*.slnx'(fname) or util.root_pattern '*.csproj'(fname))
   end,
   filetypes = { 'cs' },
   init_options = {
