neovim/nvim-lspconfig

commit fa91d1c90433aaf68c6a2774d5ffce7319effa09

fix(intelephense): workspace not found
Problem:
user reports that "lsp/intelephense.lua doesn't recognize root directory
despite the project contains composer.json or .git".

Solution:
Drop the `root_dir`, use `root_markers`.

fix #3901

lsp/intelephense.lua
@@ -28,12 +28,5 @@
 return {
   cmd = { 'intelephense', '--stdio' },
   filetypes = { 'php' },
-  root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    local cwd = assert(vim.uv.cwd())
-    local root = vim.fs.root(fname, { 'composer.json', '.git' })
-
-    -- prefer cwd if root is a descendant
-    on_dir(root and vim.fs.relpath(cwd, root) and cwd)
-  end,
+  root_markers = { 'composer.json', '.git' },
 }

commit 74e8eff87973477de43a88b168ffbcf14b2a89b5

fix(intelephense): change root markers order
This solve a bug introduced in fa91d1c

lsp/intelephense.lua
@@ -28,5 +28,5 @@
 return {
   cmd = { 'intelephense', '--stdio' },
   filetypes = { 'php' },
-  root_markers = { 'composer.json', '.git' },
+  root_markers = { '.git', 'composer.json' },
 }