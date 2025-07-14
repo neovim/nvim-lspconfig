ac75e72017ab26fda03781c67834bc1cc7273366
aldur <aldur@users.noreply.github.com>
2025-07-14 18:47:47 +0200

    fix(clarinet)!: rename `clarity_lsp` to `clarinet` #3948

---
+++ lsp/clarinet.lua
@@ -0,0 +1,9 @@
+---@brief
+--- https://github.com/hirosystems/clarinet
+---
+--- Clarinet is the fastest way to build, test, and deploy smart contracts on the Stacks blockchain.
+return {
+  cmd = { 'clarinet', 'lsp' },
+  filetypes = { 'clar', 'clarity' },
+  root_markers = { '.git' },
+}
