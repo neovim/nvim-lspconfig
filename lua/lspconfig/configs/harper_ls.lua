6f0d8eefdf3ba5a82e57d55070bdd44a1443abe0
Jeriel Baptista Verissimo <jeriel.verissimo@protonmail.com>
2025-07-16 22:14:48 -0300

    style: add missing trailing comma

--- lua/lspconfig/configs/harper_ls.lua
+++ lua/lspconfig/configs/harper_ls.lua
@@ -25,7 +25,7 @@ return {
       'typst',
       'php',
       'dart',
-      'clojure'
+      'clojure',
     },
     root_dir = function(fname)
       return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
