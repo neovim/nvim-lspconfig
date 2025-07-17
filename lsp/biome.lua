695563ade17cf246a2a4548b1618cee021670c6d
Igor <igorlfs@ufmg.br>
2025-07-17 20:51:58 -0300

    feat(biome): use project local bin if available

--- lsp/biome.lua
+++ lsp/biome.lua
@@ -10,7 +10,14 @@
 local util = require 'lspconfig.util'
 
 return {
-  cmd = { 'biome', 'lsp-proxy' },
+  cmd = function(dispatchers, config)
+    local cmd = 'biome'
+    local local_cmd = config.root_dir and config.root_dir .. '/node_modules/.bin/biome'
+    if local_cmd and vim.fn.executable(local_cmd) == 1 then
+      cmd = local_cmd
+    end
+    return vim.lsp.rpc.start({ cmd, 'lsp-proxy' }, dispatchers)
+  end,
   filetypes = {
     'astro',
     'css',
