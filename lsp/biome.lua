2957a9a55ba98b8d321b653b26c68b23f547d0d0
Justin M. Keyes <justinkz@gmail.com>
2025-07-19 08:28:52 -0700

    Apply suggestion from @justinmk

--- lsp/biome.lua
+++ lsp/biome.lua
@@ -12,7 +12,7 @@ local util = require 'lspconfig.util'
 return {
   cmd = function(dispatchers, config)
     local cmd = 'biome'
-    local local_cmd = config and config.root_dir and config.root_dir .. '/node_modules/.bin/biome'
+    local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/biome'
     if local_cmd and vim.fn.executable(local_cmd) == 1 then
       cmd = local_cmd
     end
