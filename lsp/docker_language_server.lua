2e8722a39b1f14933b98d59ae4dc06dbfb2cb992
Justin M. Keyes <justinkz@gmail.com>
2025-07-31 20:26:30 -0700

    Apply suggestion from @justinmk

--- lsp/docker_language_server.lua
+++ lsp/docker_language_server.lua
@@ -8,7 +8,7 @@
 --- ```
 return {
   cmd = { 'docker-language-server', 'start', '--stdio' },
-  filetypes = { 'dockerfile', 'yaml.docker-compose', 'hcl', 'json' },
+  filetypes = { 'dockerfile', 'yaml.docker-compose' },
   root_markers = {
     'Dockerfile',
     'docker-compose.yaml',
