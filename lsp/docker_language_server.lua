a4122e534f30ca21e14476b9510cc81f63998f64
Michael Martin <flrgh@protonmail.com>
2025-08-08 12:13:31 -0700

    fix(docker_language_server): set language id for compose files
    This adds a `get_language_id()` handler to the docker language server
    config to correctly set the language ID to `dockercompose` as expected
    by the server. Before this change, the nvim filetype of
    `yaml.docker-compose` was passed to the server as the language ID,
    resulting in in a docker compose file being misinterpreted as a
    Dockerfile.
    
    See also: e3d837b938bd1d7e84c3eb0362b6761e4d5dc15d / #3982

--- lsp/docker_language_server.lua
+++ lsp/docker_language_server.lua
@@ -9,6 +9,13 @@
 return {
   cmd = { 'docker-language-server', 'start', '--stdio' },
   filetypes = { 'dockerfile', 'yaml.docker-compose' },
+  get_language_id = function(_, ftype)
+    if ftype == 'yaml.docker-compose' or ftype:lower():find('ya?ml') then
+      return 'dockercompose'
+    else
+      return ftype
+    end
+  end,
   root_markers = {
     'Dockerfile',
     'docker-compose.yaml',
