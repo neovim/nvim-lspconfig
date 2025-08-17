32f2939355fd37bd3d26fa666e863c3a7a0c7f8b
Alexis Tacnet <alexis@mistral.ai>
2025-08-17 19:09:17 +0200

    feat(biome): improve support for monorepos arch

--- lsp/biome.lua
+++ lsp/biome.lua
@@ -6,6 +6,10 @@
 --- ```sh
 --- npm install [-g] @biomejs/biome
 --- ```
+---
+--- ### Monorepo support
+---
+--- `biome` supports monorepos by default. It will automatically find the `biome.json` corresponding to the package you are working on, as described in the [documentation](https://biomejs.dev/guides/big-projects/#monorepo). This works without the need of spawning multiple instances of `biome`, saving memory.
 
 local util = require 'lspconfig.util'
 
@@ -28,10 +32,32 @@ return {
   },
   workspace_required = true,
   root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    local root_files = { 'biome.json', 'biome.jsonc' }
-    root_files = util.insert_package_json(root_files, 'biome', fname)
-    local root_dir = vim.fs.dirname(vim.fs.find(root_files, { path = fname, upward = true })[1])
-    on_dir(root_dir)
+    -- The project root is where the LSP can be started from
+    -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+    -- We select then from the project root, which is identified by the presence of a package
+    -- manager lock file.
+    local project_root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb' }
+    local project_root = vim.fs.root(bufnr, project_root_markers)
+    if not project_root then
+      return nil
+    end
+
+    -- We know that the buffer is using Biome if it has a config file
+    -- in its directory tree.
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+    local biome_config_files = { 'biome.json', 'biome.jsonc' }
+    biome_config_files = util.insert_package_json(biome_config_files, 'biome', filename)
+    local is_buffer_using_biome = vim.fs.find(biome_config_files, {
+      path = vim.fs.dirname(filename),
+      type = 'file',
+      limit = 1,
+      upward = true,
+      stop = project_root,
+    })[1]
+    if not is_buffer_using_biome then
+      return nil
+    end
+
+    on_dir(project_root)
   end,
 }
