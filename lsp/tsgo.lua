5f1c9a90c8db9c647da40ce6cf5be9e49ccbf0c7
Vinícius Weigert <52323602+ViniWei@users.noreply.github.com>
2025-08-19 12:25:52 -0300

    fix(ts_ls): server not starting if there is no typescript #4011

--- lsp/tsgo.lua
+++ lsp/tsgo.lua
@@ -35,20 +35,6 @@ return {
       return
     end
 
-    -- We know that the buffer is using Typescript if it has a config file
-    -- in its directory tree.
-    local ts_config_files = { 'tsconfig.json', 'jsconfig.json' }
-    local is_buffer_using_typescript = vim.fs.find(ts_config_files, {
-      path = vim.api.nvim_buf_get_name(bufnr),
-      type = 'file',
-      limit = 1,
-      upward = true,
-      stop = vim.fs.dirname(project_root),
-    })[1]
-    if not is_buffer_using_typescript then
-      return
-    end
-
     on_dir(project_root)
   end,
 }
