neovim/nvim-lspconfig

commit 63a016437e44283e4aa54802be3002d06f724253

feat(yamlls): document formatting support #4003

lsp/yamlls.lua
@@ -66,4 +66,8 @@ return {
     -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
     redhat = { telemetry = { enabled = false } },
   },
+  on_init = function(client)
+    -- https://github.com/redhat-developer/yaml-language-server/issues/486
+    client.server_capabilities.documentFormattingProvider = true
+  end,
 }

commit 66274c2ed7a45bc6d3f6b67f623184178500def0

refactor(yamlls): non-hacky way to enable formatting #4012
Faking capabilities isn't necessary to enable formatting support. It's a
dynamically registered capability that yamlls announces whenever yaml
formatting is enabled in settings, which it isn't by default.

See
https://github.com/redhat-developer/yaml-language-server/blob/3821411ee8c92e5b7e5ca88f84ab443ae3b2791a/src/yamlServerInit.ts#L128
https://github.com/redhat-developer/yaml-language-server/blob/3821411ee8c92e5b7e5ca88f84ab443ae3b2791a/src/languageserver/handlers/settingsHandlers.ts#L159-L174

Fixes: 63a016437e44 ("feat(yamlls): document formatting support #4003")

lsp/yamlls.lua
@@ -66,9 +66,7 @@ return {
   settings = {
     -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
     redhat = { telemetry = { enabled = false } },
+    -- formatting disabled by default in yaml-language-server; enable it
+    yaml = { format = { enable = true } },
   },
-  on_init = function(client)
-    -- https://github.com/redhat-developer/yaml-language-server/issues/486
-    client.server_capabilities.documentFormattingProvider = true
-  end,
 }

lua/lspconfig/configs/yamlls.lua
@@ -9,6 +9,8 @@ return {
     settings = {
       -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
       redhat = { telemetry = { enabled = false } },
+      -- formatting disabled by default in yaml-language-server; enable it
+      yaml = { format = { enable = true } },
     },
   },
   docs = {