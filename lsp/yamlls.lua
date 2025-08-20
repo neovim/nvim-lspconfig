592de750bf5b8c45fc48cebfde585721626ffd6d
Christopher Johnstone <61227735+ChrisPJohnstone@users.noreply.github.com>
2025-08-20 18:58:56 +0100

    feat(yamlls): force documentFormattingProvider=true #4016
    Problem:
    The current version of the config will let you do formatting manually
    `:lua vim.lsp.buf.format()` but the lsp doesn't have capabilities
    registered correctly so if you use auto formatting like outlined in
    [help pages](https://neovim.io/doc/user/lsp.html#lsp-attach)
    
        if not client:supports_method('textDocument/willSaveWaitUntil')
            and client:supports_method('textDocument/formatting') then
          vim.api.nvim_create_autocmd('BufWritePre', {
            group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
            buffer = args.buf,
            callback = function()
              vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
            end,
          })
        end
    
    , it won't enable for the yaml lsp because
    `client:supports_method('textDocument/formatting')` returns false.
    
    Solution:
    The `on_init` hook, while hacky, will mark the capability as true.

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -70,4 +70,11 @@ return {
     -- formatting disabled by default in yaml-language-server; enable it
     yaml = { format = { enable = true } },
   },
+  on_init = function(client)
+    --- https://github.com/neovim/nvim-lspconfig/pull/4016
+    --- Since formatting is disabled by default if you check `client:supports_method('textDocument/formatting')`
+    --- during `LspAttach` it will return `false`. This hack sets the capability to `true` to facilitate
+    --- autocmd's which check this capability
+    client.server_capabilities.documentFormattingProvider = true
+  end,
 }
