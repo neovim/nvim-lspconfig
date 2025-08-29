3d09c75c9338604fc0d5d2d96ba83134cb8ff947
Andy LeClair <andyleclair@gmail.com>
2025-08-28 21:28:32 -0400

    feat(expert): add config for expert lsp

---
+++ lsp/expert.lua
@@ -0,0 +1,12 @@
+---@brief
+---
+--- https://github.com/elixir-lang/expert
+---
+--- Expert is the official language server implementation for the Elixir programming language.
+
+---@type vim.lsp.Config
+return {
+  filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
+  root_markers = { 'mix.exs', '.git' },
+  cmd = { 'expert' },
+}
