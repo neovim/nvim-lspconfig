386231e6b746206147fc62d0243f09904a0a2f1f
Michele Sorcinelli <michelesr@users.noreply.github.com>
2025-08-31 01:50:16 +0100

    fix(pyright): command :LspPyrightOrganizeImports fails #3971
    Problem:
    The `pyright.organizeimports` is private, so client:exec_cmd()
    fails because it refuses to run commands that are not advertised
    as capabilities.
    
    Solution:
    Call client.request() to side-step the check in client:exec_cmd().
    YOLO

--- lsp/pyright.lua
+++ lsp/pyright.lua
@@ -43,10 +43,15 @@ return {
   },
   on_attach = function(client, bufnr)
     vim.api.nvim_buf_create_user_command(bufnr, 'LspPyrightOrganizeImports', function()
-      client:exec_cmd({
+      local params = {
         command = 'pyright.organizeimports',
         arguments = { vim.uri_from_bufnr(bufnr) },
-      })
+      }
+
+      -- Using client.request() directly because "pyright.organizeimports" is private
+      -- (not advertised via capabilities), which client:exec_cmd() refuses to call.
+      -- https://github.com/neovim/neovim/blob/c333d64663d3b6e0dd9aa440e433d346af4a3d81/runtime/lua/vim/lsp/client.lua#L1024-L1030
+      client.request('workspace/executeCommand', params, nil, bufnr)
     end, {
       desc = 'Organize Imports',
     })
