a9bbd7ae0e4bef90d2e701ec6c3ea32d6a6d7c20
Rasheed Atanda <atandarash@gmail.com>
2025-09-03 21:52:31 +0100

    fix(expert): implement root_dir instead

--- lsp/expert.lua
+++ lsp/expert.lua
@@ -3,10 +3,19 @@
 --- https://github.com/elixir-lang/expert
 ---
 --- Expert is the official language server implementation for the Elixir programming language.
+---
+--- 'root_dir' is chosen like this: if two or more directories containing `mix.exs` were found when searching directories upward, the second one (higher up) is chosen, with the assumption that it is the root of an umbrella app. Otherwise the directory containing the single mix.exs that was found is chosen.
 
 ---@type vim.lsp.Config
 return {
   filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
-  root_markers = { '.git', 'mix.exs' },
   cmd = { 'expert' },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local matches = vim.fs.find({ 'mix.exs' }, { upward = true, limit = 2, path = fname })
+    local child_or_root_path, maybe_umbrella_path = unpack(matches)
+    local root_dir = vim.fs.dirname(maybe_umbrella_path or child_or_root_path)
+
+    on_dir(root_dir)
+  end,
 }
