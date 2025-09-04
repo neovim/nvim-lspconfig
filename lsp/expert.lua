2934192eab0b17b1b37666ad8fa137141d7fd239
Justin M. Keyes <justinkz@gmail.com>
2025-09-03 20:50:35 -0400

    docs, lint

--- lsp/expert.lua
+++ lsp/expert.lua
@@ -15,8 +15,8 @@ return {
   cmd = { 'expert' },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
-    --- Elixir workspaces may have `mix.exs` files, e.g. for an "umbrella" layout or monorepo. So we
-    --- specify `limit=2` and treat the highest one (if any) as the root of an umbrella app.
+    --- Elixir workspaces may have multiple `mix.exs` files, for an "umbrella" layout or monorepo.
+    --- So we specify `limit=2` and treat the highest one (if any) as the root of an umbrella app.
     local matches = vim.fs.find({ 'mix.exs' }, { upward = true, limit = 2, path = fname })
     local child_or_root_path, maybe_umbrella_path = unpack(matches)
     local root_dir = vim.fs.dirname(maybe_umbrella_path or child_or_root_path)
