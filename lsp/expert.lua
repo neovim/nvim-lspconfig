a7fcd7518b0aa4dc5db31fddba19735c73e7f647
Justin M. Keyes <justinkz@gmail.com>
2025-09-03 17:34:44 -0700

    Update lsp/expert.lua

--- lsp/expert.lua
+++ lsp/expert.lua
@@ -4,7 +4,10 @@
 ---
 --- Expert is the official language server implementation for the Elixir programming language.
 ---
---- 'root_dir' is chosen like this: if two or more directories containing `mix.exs` were found when searching directories upward, the second one (higher up) is chosen, with the assumption that it is the root of an umbrella app. Otherwise the directory containing the single mix.exs that was found is chosen.
+--- 'root_dir' is chosen like this: if two or more directories containing `mix.exs` were found when
+--- searching directories upward, the second one (higher up) is chosen, with the assumption that it
+--- is the root of an umbrella app. Otherwise the directory containing the single mix.exs that was
+--- found is chosen.
 
 ---@type vim.lsp.Config
 return {
