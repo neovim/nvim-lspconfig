345560dbc7a6acd91607d182093d512f7ff23096
Bartłomiej Maryńczak <marynczak.bartlomiej@gmail.com>
2025-09-14 23:44:05 +0200

    feat: StyLua Lua formatter #4073

---
+++ lsp/stylua.lua
@@ -0,0 +1,12 @@
+---@brief
+---
+--- https://github.com/JohnnyMorganz/StyLua
+---
+--- A deterministic code formatter for Lua 5.1, 5.2, 5.3, 5.4, LuaJIT, Luau and CfxLua/FiveM Lua
+
+---@type vim.lsp.Config
+return {
+  cmd = { 'stylua', '--lsp' },
+  filetypes = { 'lua' },
+  root_markers = { '.stylua.toml', 'stylua.toml' },
+}
