d2d650799b556fd7508ab0cc3699dade1e0552e7
Kai Moschcau <kai.moschcau@blecon.de>
2025-09-17 08:12:43 +0200

    fix(biome,eslint,svelte,ts_ls,tsgo,vtsls): remove deno.lock root marker
    Problem:
    Adding `deno.lock` caused other servers to start in deno projects where
    they should not.
    
    Solution:
    Remove `deno.lock` root marker from these other server configurations.
    
    This reverts commit 7d5cf94d70b003385f4410746614b0c8e4d6b63d.

--- lsp/biome.lua
+++ lsp/biome.lua
@@ -44,7 +44,7 @@ return {
     -- As stated in the documentation above, this LSP supports monorepos and simple projects.
     -- We select then from the project root, which is identified by the presence of a package
     -- manager lock file.
-    local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', 'deno.lock' }
+    local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
     -- Give the root markers equal priority by wrapping them in a table
     root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers } or root_markers
     local project_root = vim.fs.root(bufnr, root_markers)
