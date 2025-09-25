903045b001a013a1b321b1121db711820f94e588
Rich Grundy <rich@grundy.io>
2025-09-25 21:29:51 +0100

    fix(ruby_lsp): set cmd_cwd to root_dir #4099
    Problem: When running from a directory other than root (i.e monorepo),
    ruby-lsp does not load correctly. Unsure why, previous lspconfig had
    cmd_cwd set to the same as root_dir.
    
    Solution:
    Set the cmd_cwd via reuse_client(), similar to ast_grep. See #3850, #4082 for more details.
    Tested both from root_dir and monorepo multi-root project.

--- lsp/ruby_lsp.lua
+++ lsp/ruby_lsp.lua
@@ -20,4 +20,8 @@ return {
   init_options = {
     formatter = 'auto',
   },
+  reuse_client = function(client, config)
+    config.cmd_cwd = config.root_dir
+    return client.config.cmd_cwd == config.cmd_cwd
+  end,
 }
