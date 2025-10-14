ebcf88c94dcace89e48b2c6445af26cecfaff31a
Pairofdice <keonfarrugia@gmail.com>
2025-10-14 15:56:43 +0200

    fix(OmniSharp):Changed config to vim.lsp.config
    Problem:Config uses deprecated lspconfig.util for finding root files.
    
    Solution:
    Replace root_dir with root_markers and remove the wildcards.

--- lsp/omnisharp.lua
+++ lsp/omnisharp.lua
@@ -14,8 +14,6 @@
 ---
 ---
 
-local util = require 'lspconfig.util'
-
 ---@type vim.lsp.Config
 return {
   cmd = {
@@ -29,15 +27,7 @@ return {
     '--languageserver',
   },
   filetypes = { 'cs', 'vb' },
-  root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(
-      util.root_pattern '*.sln'(fname)
-        or util.root_pattern '*.csproj'(fname)
-        or util.root_pattern 'omnisharp.json'(fname)
-        or util.root_pattern 'function.json'(fname)
-    )
-  end,
+  root_markers = {".sln",".csproj","omnisharp.json","function.json"},
   init_options = {},
   capabilities = {
     workspace = {
