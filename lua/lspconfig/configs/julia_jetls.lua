6789b1a7dfde27a9828c633577acf9a7f4ff8511
Ralph Kube <ralph_kube@gmx.net>
2025-10-18 08:50:35 -0700

    stylua check

--- lua/lspconfig/configs/julia_jetls.lua
+++ lua/lspconfig/configs/julia_jetls.lua
@@ -5,18 +5,18 @@
 --- `JETLS` is a new language server for Julia
 
 --- Define the directory where julia environments are usually located
-local julia_envs = os.getenv("HOME") .. ".julia/environments"
+local julia_envs = os.getenv('HOME') .. '.julia/environments'
 
 return {
   cmd = {
-        "julia",
-        "--startup-file=no",
-        "--history-file=no",
-        "--project=" .. julia_envs.. "/JETLS.jl",
-        julia_envs .. "/JETLS.jl/runserver.jl",
+    'julia',
+    '--startup-file=no',
+    '--history-file=no',
+    '--project=" .. julia_envs.. "/JETLS.jl',
+    julia_envs .. '/JETLS.jl/runserver.jl',
   },
-  filetypes = {"julia"},
-  root_markers = { "Project.toml", "JuliaProject.toml" },
+  filetypes = { 'julia' },
+  root_markers = { 'Project.toml', 'JuliaProject.toml' },
   docs = {
     description = [[
 A new language server for Julia.
@@ -30,6 +30,6 @@ git clone git@github.com:aviatesk/JETLS.jl.git
 cd JETLS.jl
 julia --project=. -e 'using Pkg; Pkg.instantiate()'
 ```
-    ]]
-  }
+    ]],
+  },
 }
