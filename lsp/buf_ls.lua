fe11eb4466f889cad91c8ea6bce499e2c01bbc2f
Edward McFarlane <emcfarlane@buf.build>
2025-10-20 18:35:45 +0100

    fix(buf_ls): update serve command for buf > v1.59.0
    This updates the buf_ls command post v1.59.0, where the command was
    promoted out of beta to general availability. The command was changed
    from buf beta lsp to buf lsp serve.

--- lsp/buf_ls.lua
+++ lsp/buf_ls.lua
@@ -7,7 +7,7 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'buf', 'beta', 'lsp', '--timeout=0', '--log-format=text' },
+  cmd = { 'buf', 'lsp', 'serve', '--timeout=0', '--log-format=text' },
   filetypes = { 'proto' },
   root_markers = { 'buf.yaml', '.git' },
 }
