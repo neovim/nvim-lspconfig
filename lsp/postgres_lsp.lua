a9f9f281830d0f438d7847156faa6c66022452bb
psteinroe <philipp@steinroetter.com>
2025-10-26 16:39:14 +0100

    chore(postgres_lsp): update config for postgres-language-server renaming
    Update references from postgrestools to postgres-language-server following
    the project's renaming. This includes the command, documentation URL, and
    root markers.

--- lsp/postgres_lsp.lua
+++ lsp/postgres_lsp.lua
@@ -1,14 +1,14 @@
 ---@brief
 ---
---- https://pgtools.dev
+--- https://pg-language-server.com
 ---
 --- A collection of language tools and a Language Server Protocol (LSP) implementation for Postgres, focusing on developer experience and reliable SQL tooling.
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'postgrestools', 'lsp-proxy' },
+  cmd = { 'postgres-language-server', 'lsp-proxy' },
   filetypes = {
     'sql',
   },
-  root_markers = { 'postgrestools.jsonc' },
+  root_markers = { 'postgres-language-server.jsonc' },
 }
