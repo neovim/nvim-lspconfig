0a3b8948caabb28532d25d33588de46463066e9a
TomIO <tom@termux.dev>
2025-10-28 16:00:05 +0100

    fix(termux_language_server): replace broken `filetypes` field
    Problem:
    `termux_language_server`'s `filetypes` field
    is set to the non-existent `PKGBUILD` filetype,
    thus the LSP never attaches to a target buffer.
    
    Solution:
    Replace the `filetypes` field with a `root_dir`
    function matching the filename pattern suggested
    by upstream.
    https://termux-language-server.readthedocs.io/en/latest/resources/configure.html#neovim

--- lsp/termux_language_server.lua
+++ lsp/termux_language_server.lua
@@ -4,9 +4,28 @@
 ---
 --- Language server for various bash scripts such as Arch PKGBUILD, Gentoo ebuild, Termux build.sh, etc.
 
+local util = require('lspconfig').util
+
 ---@type vim.lsp.Config
 return {
   cmd = { 'termux-language-server' },
-  filetypes = { 'PKGBUILD' },
-  root_markers = { '.git' },
+  root_dir = function(bufnr, on_dir)
+    local patterns = {
+      -- Termux
+      'build.sh',
+      '*.subpackage.sh',
+      -- Arch/MSYS2
+      'PKGBUILD',
+      'makepkg.conf',
+      '*.install',
+      -- Gentoo
+      'make.conf',
+      'color.map',
+      '*.ebuild',
+      '*.eclass',
+    }
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local match = util.root_pattern(patterns)(fname)
+    on_dir(match and (vim.fs.root(match, '.git') or match))
+  end,
 }
