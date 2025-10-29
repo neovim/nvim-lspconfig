1074ec3b3af1bad3cd04bb0d515f143aba2d3536
pogyomo <pogyomo@gmail.com>
2025-10-29 20:57:10 +0900

    feat(csharp_ls): runs csharp-ls where sln, slnx or csproj file exists

--- lsp/csharp_ls.lua
+++ lsp/csharp_ls.lua
@@ -13,6 +13,14 @@ local util = require 'lspconfig.util'
 ---@type vim.lsp.Config
 return {
   cmd = { 'csharp-ls' },
+  cmd_cwd = vim.fs.root(0, {
+    function(name, _)
+      return name:match '%.slnx?$' ~= nil
+    end,
+    function(name, _)
+      return name:match '%.csproj$' ~= nil
+    end,
+  }),
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
     on_dir(util.root_pattern '*.sln'(fname) or util.root_pattern '*.slnx'(fname) or util.root_pattern '*.csproj'(fname))
