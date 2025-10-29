neovim/nvim-lspconfig

commit 4d9d9d9c5e66dd175e2168bfddcb9e1a5b589004

fix(termux_language_server): broken `filetypes` field #4161
Problem:
`termux_language_server`'s `filetypes` field
is set to the non-existent `PKGBUILD` filetype,
thus the LSP never attaches to a target buffer.

Solution:
Replace the `filetypes` field with a `root_dir`
function matching the filename pattern suggested
by upstream.
https://termux-language-server.readthedocs.io/en/latest/resources/configure.html#neovim

lsp/termux_language_server.lua
@@ -4,9 +4,28 @@
 ---
 --- Language server for various bash scripts such as Arch PKGBUILD, Gentoo ebuild, Termux build.sh, etc.
 
+local util = require('lspconfig').util
+
 ---@type vim.lsp.Config
 return {
   cmd = { 'termux-language-server' },
-  filetypes = { 'PKGBUILD' },
-  root_markers = { '.git' },
+  root_dir = function(bufnr, on_dir)
+    local patterns = {
+      -- Termux
+      'build.sh',
+      '*.subpackage.sh',
+      -- Arch/MSYS2
+      'PKGBUILD',
+      'makepkg.conf',
+      '*.install',
+      -- Gentoo
+      'make.conf',
+      'color.map',
+      '*.ebuild',
+      '*.eclass',
+    }
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local match = util.root_pattern(patterns)(fname)
+    on_dir(match and (vim.fs.root(match, '.git') or match))
+  end,
 }

commit 73cc7981712e5ad73602ede3c983a36d3cefa243

fix(termux_language_server): do not call `on_dir()` unconditionally
Problem:
The `root_dir()` function for `termux_language_server`
introduced in #4161 calls `on_dir()` unconditionally.
Leading to `termux_language_server` attaching to all buffers.

Solution:
Make the call conditional.

lsp/termux_language_server.lua
@@ -26,6 +26,8 @@ return {
     }
     local fname = vim.api.nvim_buf_get_name(bufnr)
     local match = util.root_pattern(patterns)(fname)
-    on_dir(match and (vim.fs.root(match, '.git') or match))
+    if match then
+      on_dir(vim.fs.root(match, '.git') or match)
+    end
   end,
 }