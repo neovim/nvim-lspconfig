6ba70dce0724278cb8ab90013282a3294bfba630
Trevor <trevor@hauter.dev>
2025-10-30 19:47:32 -0400

    fix(basedpyright): update root marker order

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -25,12 +25,12 @@ return {
   cmd = { 'basedpyright-langserver', '--stdio' },
   filetypes = { 'python' },
   root_markers = {
+    'pyrightconfig.json',
     'pyproject.toml',
     'setup.py',
     'setup.cfg',
     'requirements.txt',
     'Pipfile',
-    'pyrightconfig.json',
     '.git',
   },
   settings = {
