b40e538a3c230e60e44a9149339cab581aa7eb0f
Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>
2025-11-12 17:53:11 +0100

    fix(buf_ls): reuse client across workspaces #4179

--- lsp/buf_ls.lua
+++ lsp/buf_ls.lua
@@ -10,4 +10,8 @@ return {
   cmd = { 'buf', 'lsp', 'serve', '--timeout=0', '--log-format=text' },
   filetypes = { 'proto' },
   root_markers = { 'buf.yaml', '.git' },
+  reuse_client = function()
+    -- `buf lsp serve` is meant to be used with multiple workspaces.
+    return true
+  end,
 }
