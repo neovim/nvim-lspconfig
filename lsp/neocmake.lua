b7730d1ade78d137e6d6503a574c7f7b4dbb37ee
idealseal <realidealseal@protonmail.com>
2025-11-21 18:14:40 +0100

    fix(neocmake): use subcommand instead of flag
    Old versions accept both, however starting with v0.9.0 only
    subcommands will be recognized.

--- lsp/neocmake.lua
+++ lsp/neocmake.lua
@@ -18,7 +18,7 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'neocmakelsp', '--stdio' },
+  cmd = { 'neocmakelsp', 'stdio' },
   filetypes = { 'cmake' },
   root_markers = { '.git', 'build', 'cmake' },
 }
