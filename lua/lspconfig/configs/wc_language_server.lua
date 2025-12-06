fa4ab80b0eb367e98ef01a2d1c588b4d90be063a
Burton Smith <31320098+break-stuff@users.noreply.github.com>
2025-12-06 05:05:10 +0000

    update formatting

--- lua/lspconfig/configs/wc_language_server.lua
+++ lua/lspconfig/configs/wc_language_server.lua
@@ -3,12 +3,30 @@ return {
   init_options = { hostInfo = 'neovim' },
   cmd = { 'wc-language-server', '--stdio' },
   filetypes = {
-    'html', 'javascriptreact', 'typescriptreact', 'astro', 'svelte', 'vue', 
-    'markdown', 'mdx', 'javascript', 'typescript', 'css', 'scss', 'less'
+    'html',
+    'javascriptreact',
+    'typescriptreact',
+    'astro',
+    'svelte',
+    'vue',
+    'markdown',
+    'mdx',
+    'javascript',
+    'typescript',
+    'css',
+    'scss',
+    'less',
   },
   root_dir = function(bufnr, on_dir)
-    local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 
-      'bun.lockb', 'tsconfig.json', 'jsconfig.json', 'wc.config.js' }
+    local root_markers = {
+      'package-lock.json',
+      'yarn.lock',
+      'pnpm-lock.yaml',
+      'bun.lockb',
+      'tsconfig.json',
+      'jsconfig.json',
+      'wc.config.js',
+    }
     local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
     on_dir(project_root)
   end,
