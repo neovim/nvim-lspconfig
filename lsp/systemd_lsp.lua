aab53575de6d3efa7fff8206114e21c8621b3db4
kris3713 <07e5297d5b@c0x0.com>
2026-01-02 15:35:52 -0700

    fix(systemd_lsp): Refactored comment documentation

--- lsp/systemd_lsp.lua
+++ lsp/systemd_lsp.lua
@@ -19,36 +19,27 @@
 --- > Systemd unit files, use the following settings:
 ---
 --- ```lua
---- local systemd_unit_extensions = { -- Credit to @magnuslarsen
----   -- systemd unit files
----   'service',
----   'socket',
----   'timer',
----   'mount',
----   'automount',
----   'swap',
----   'target',
----   'path',
----   'slice',
----   'scope',
----   'device',
----   -- Podman Quadlet files
----   'container',
----   'volume',
----   'network',
----   'kube',
----   'pod',
----   'build',
----   'image',
---- }
----
---- local extension_map = {}
---- for _, exts in ipairs(systemd_unit_extensions) do
----   extension_map[exts] = 'systemd'
---- end
----
 --- vim.filetype.add {
----   extension = extension_map
+---   -- systemd unit filetypes
+---   ['service'] = 'systemd',
+---   ['socket'] = 'systemd',
+---   ['timer'] = 'systemd',
+---   ['mount'] = 'systemd',
+---   ['automount'] = 'systemd',
+---   ['swap'] = 'systemd',
+---   ['target'] = 'systemd',
+---   ['path'] = 'systemd',
+---   ['slice'] = 'systemd',
+---   ['scope'] = 'systemd',
+---   ['device'] = 'systemd',
+---   -- Podman Quadlet filetypes
+---   ['container'] = 'systemd',
+---   ['volume'] = 'systemd',
+---   ['network'] = 'systemd',
+---   ['kube'] = 'systemd',
+---   ['pod'] = 'systemd',
+---   ['build'] = 'systemd',
+---   ['image'] = 'systemd',
 --- }
 --- ```
 
