859f4453081ba3411a698f1c97a6ac83f36aa234
Igor <igorlfs@ufmg.br>
2026-01-03 01:33:48 -0300

    fix: cleanup unknown filetypes

--- lsp/html.lua
+++ lsp/html.lua
@@ -25,7 +25,7 @@
 ---@type vim.lsp.Config
 return {
   cmd = { 'vscode-html-language-server', '--stdio' },
-  filetypes = { 'html', 'templ' },
+  filetypes = { 'html' },
   root_markers = { 'package.json', '.git' },
   settings = {},
   init_options = {
