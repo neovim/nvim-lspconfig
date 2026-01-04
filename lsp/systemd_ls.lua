6d30e64c0691e638f62fa107af4b2af38db7081f
Olivia Kinnear <git@superatomic.dev>
2026-01-04 12:59:04 -0600

    fix: show deprecation messages on_init

--- lsp/systemd_ls.lua
+++ lsp/systemd_ls.lua
@@ -2,7 +2,12 @@
 ---
 --- Renamed to [systemd_lsp](#systemd_lsp)
 
-vim.deprecate('systemd_ls', 'systemd_lsp', '2.0.0', 'nvim-lspconfig', false)
-
 ---@type vim.lsp.Config
-return vim.lsp.config.systemd_lsp
+return vim.tbl_extend('force', vim.lsp.config.systemd_lsp, {
+  on_init = function(...)
+    vim.deprecate('systemd_ls', 'systemd_lsp', '3.0.0', 'nvim-lspconfig', false)
+    if vim.lsp.config.systemd_lsp.on_init then
+      vim.lsp.config.systemd_lsp.on_init(...)
+    end
+  end,
+})
