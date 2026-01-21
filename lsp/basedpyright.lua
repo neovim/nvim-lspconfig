64cd73aa266348b1e2b9f8cd31e107ef65aa0c4a
lljbash <lljbash@gmail.com>
2026-01-21 19:13:43 +0800

    fix(basedpyright): remove explicit useLibraryCodeForTypes setting

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -38,8 +38,11 @@ return {
     basedpyright = {
       analysis = {
         autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
         diagnosticMode = 'openFilesOnly',
+        -- https://docs.basedpyright.com/latest/configuration/language-server-settings/
+        -- Explicitly setting `basedpyright.analysis.useLibraryCodeForTypes` is **discouraged** by the official docs.
+        -- Because it will override per-project configurations like `pyproject.toml`.
+        -- If left unset, its default value is `true`, and it can be correctly overridden by project config files.
       },
     },
   },
