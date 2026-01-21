34229c6e021f8d2b9c947222ccd03605040b81b5
zeertzjq <zeertzjq@outlook.com>
2026-01-22 06:53:51 +0800

    fix(lua_ls): adjust priorities (#4289)
    Give same priority for .emmyrc.json and .luarc.json, lower priority to
    other config files, and lowest priority to .git.

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -66,21 +66,25 @@
 --- * [Lua.workspace.library](https://luals.github.io/wiki/settings/#workspacelibrary)
 ---
 
+local root_markers1 = {
+  '.emmyrc.json',
+  '.luarc.json',
+  '.luarc.jsonc',
+}
+local root_markers2 = {
+  '.luacheckrc',
+  '.stylua.toml',
+  'stylua.toml',
+  'selene.toml',
+  'selene.yml',
+}
+
 ---@type vim.lsp.Config
 return {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
-  root_markers = {
-    '.emmyrc.json',
-    '.luarc.json',
-    '.luarc.jsonc',
-    '.luacheckrc',
-    '.stylua.toml',
-    'stylua.toml',
-    'selene.toml',
-    'selene.yml',
-    '.git',
-  },
+  root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers1, root_markers2, { '.git' } }
+    or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { '.git' }),
   settings = {
     Lua = {
       codeLens = { enable = true },
