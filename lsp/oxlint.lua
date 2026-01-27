e2bc4368282c7a94b8c2d49210d585baaf003153
Kris Maglione <maglione.k@gmail.com>
2026-01-26 19:59:09 -0800

    feat(oxlint): automatically enable type-aware linting for oxlint when necessary
    Problem:
    Type-aware linting is not enabled by default, and it isn't obvious at
    all how to do so in neovim based on the Oxlint documentation.
    
    Solution:
    Automatically enable type-aware linting if the necessary dependencies
    are installed and the Oxlint config file references TypeScript.

--- lsp/oxlint.lua
+++ lsp/oxlint.lua
@@ -11,11 +11,24 @@
 --- npm i -g oxlint
 --- ```
 ---
+--- Type-aware linting will automatically be enabled if `tsgolint` exists in your
+--- path and your `.oxlintrc.json` contains the string "typescript".
+---
 --- The default `on_attach` function provides an `:LspOxlintFixAll` command which
 --- can be used to fix all fixable diagnostics. See the `eslint` config entry for
 --- an example of how to use this to automatically fix all errors on write.
 
 local util = require 'lspconfig.util'
+
+local function oxlint_conf_mentions_typescript(root_dir)
+  local fn = vim.fs.joinpath(root_dir, '.oxlintrc.json')
+  for line in io.lines(fn) do
+    if line:find('typescript') then
+      return true
+    end
+  end
+  return false
+end
 
 ---@type vim.lsp.Config
 return {
@@ -52,15 +65,26 @@ return {
     local root_markers = util.insert_package_json({ '.oxlintrc.json' }, 'oxlint', fname)[1]
     on_dir(vim.fs.dirname(vim.fs.find(root_markers, { path = fname, upward = true })[1]))
   end,
-  init_options = {
-    settings = {
-      -- ['run'] = 'onType',
-      -- ['configPath'] = nil,
-      -- ['tsConfigPath'] = nil,
-      -- ['unusedDisableDirectives'] = 'allow',
-      -- ['typeAware'] = false,
-      -- ['disableNestedConfig'] = false,
-      -- ['fixKind'] = 'safe_fix',
-    },
+  settings = {
+    -- run = 'onType',
+    -- configPath = nil,
+    -- tsConfigPath = nil,
+    -- unusedDisableDirectives = 'allow',
+    -- typeAware = false,
+    -- disableNestedConfig = false,
+    -- fixKind = 'safe_fix',
   },
+  before_init = function(init_params, config)
+    local settings = config.settings or {}
+    if settings.typeAware == nil and vim.fn.executable('tsgolint') == 1 then
+      local ok, res = pcall(oxlint_conf_mentions_typescript, config.root_dir)
+      if ok and res then
+        settings = vim.tbl_extend('force', settings, { typeAware = true })
+      end
+    end
+    local init_options = config.init_options or {}
+    init_options.settings = vim.tbl_extend('force', init_options.settings or {} --[[@as table]], settings)
+
+    init_params.initializationOptions = init_options
+  end,
 }
