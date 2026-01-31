e852d6dde229f9410b63b735165d51d3b27dd483
Kris Maglione <maglione.k@gmail.com>
2026-01-30 17:13:50 -0800

    fix(oxlint): remove unknown filetypes
    Problem:
    The filetypes javascript.tsx and typescript.tsx are not valid and have
    been removed from other LSP configs recently.
    
    Solution:
    Remove them from the oxlint config.

--- lsp/oxlint.lua
+++ lsp/oxlint.lua
@@ -34,10 +34,8 @@ return {
   filetypes = {
     'javascript',
     'javascriptreact',
-    'javascript.jsx',
     'typescript',
     'typescriptreact',
-    'typescript.tsx',
     'vue',
     'svelte',
     'astro',
