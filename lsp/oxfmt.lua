27f722f35503d4b10c485398686ec508d5cd6e7e
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2026-02-24 18:35:21 +0800

    feat(oxlint, oxfmt): check for local binaries in node_modules

--- lsp/oxfmt.lua
+++ lsp/oxfmt.lua
@@ -15,7 +15,14 @@ local util = require 'lspconfig.util'
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'oxfmt', '--lsp' },
+  cmd = function(dispatchers, config)
+    local cmd = 'oxfmt'
+    local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/oxfmt'
+    if local_cmd and vim.fn.executable(local_cmd) == 1 then
+      cmd = local_cmd
+    end
+    return vim.lsp.rpc.start({ cmd, '--lsp' }, dispatchers)
+  end,
   filetypes = {
     'javascript',
     'javascriptreact',
--- lsp/oxlint.lua
+++ lsp/oxlint.lua
@@ -30,7 +30,14 @@ end
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'oxlint', '--lsp' },
+  cmd = function(dispatchers, config)
+    local cmd = 'oxlint'
+    local local_cmd = (config or {}).root_dir and config.root_dir .. '/node_modules/.bin/oxlint'
+    if local_cmd and vim.fn.executable(local_cmd) == 1 then
+      cmd = local_cmd
+    end
+    return vim.lsp.rpc.start({ cmd, '--lsp' }, dispatchers)
+  end,
   filetypes = {
     'javascript',
     'javascriptreact',
