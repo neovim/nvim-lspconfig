899c2556e32e8c585a69cb39c0e5b950c5b4c52f
Matthias Wahl <matthiaswahl@m7w3.de>
2026-02-27 20:53:00 +0100

    fix(pony_lsp)!: rename `pony_language_server` to `pony_lsp`

--- lsp/pony_language_server.lua
+++ lsp/pony_language_server.lua
@@ -1,12 +1,13 @@
 ---@brief
 ---
---- https://github.com/ponylang/pony-language-server
----
---- Language server for the Pony programming language
+--- Renamed to [pony_lsp](#pony_lsp)
 
 ---@type vim.lsp.Config
-return {
-  cmd = { 'pony-lsp' },
-  filetypes = { 'pony' },
-  root_markers = { 'corral.json', '.git' },
-}
+return vim.tbl_extend('force', vim.lsp.config.pony_lsp, {
+  on_init = function(...)
+    vim.deprecate('pony_language_server', 'pony_lsp', '3.0.0', 'nvim-lspconfig', false)
+    if vim.lsp.config.pony_lsp.on_init then
+      vim.lsp.config.pony_lsp.on_init(...)
+    end
+  end,
+})
