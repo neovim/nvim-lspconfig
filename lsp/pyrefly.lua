1d583ccec2a01c820850a8e1aef461b32741509e
skarrok <skarrok.h@gmail.com>
2026-03-05 22:51:40 +0300

    fix(pyrefly): schedule vim.notify in on_exit (#4336)

--- lsp/pyrefly.lua
+++ lsp/pyrefly.lua
@@ -21,6 +21,8 @@ return {
     '.git',
   },
   on_exit = function(code, _, _)
-    vim.notify('Closing Pyrefly LSP exited with code: ' .. code, vim.log.levels.INFO)
+    vim.schedule(function()
+      vim.notify('Closing Pyrefly LSP exited with code: ' .. code, vim.log.levels.INFO)
+    end)
   end,
 }
