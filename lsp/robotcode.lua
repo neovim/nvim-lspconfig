e6eb630210fb6b6a84f27c07ca2b0cb6bf33aa7f
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2026-04-03 22:01:32 +0700

    feat(robotcode): support virtual env #4367

--- lsp/robotcode.lua
+++ lsp/robotcode.lua
@@ -4,11 +4,14 @@
 ---
 --- RobotCode - Language Server Protocol implementation for Robot Framework.
 
+local venv = os.getenv('VIRTUAL_ENV')
+
 ---@type vim.lsp.Config
 return {
   cmd = { 'robotcode', 'language-server' },
   filetypes = { 'robot', 'resource' },
   root_markers = { 'robot.toml', 'pyproject.toml', 'Pipfile', '.git' },
+  cmd_env = venv and { PYTHONPATH = string.gsub(vim.fn.glob(venv .. '/lib/python*/site-packages'), '\n', ':') } or nil,
   get_language_id = function(_, _)
     return 'robotframework'
   end,
