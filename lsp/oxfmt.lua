dfbada1a311c4bf3369f8d5421369c9b6e8b888f
VI0L3TF0X <73484028+V1OL3TF0X@users.noreply.github.com>
2026-05-21 16:36:13 +0200

    fix(oxc): fix oxlint, oxfmt vite plus config search #4434

--- lsp/oxfmt.lua
+++ lsp/oxfmt.lua
@@ -58,7 +58,14 @@ return {
       { 'oxfmt', 'vite%-plus' },
       fname
     )
-    root_markers = util.root_markers_with_field(root_markers, { 'vite.config.ts' }, 'vite%-plus', fname)
+    -- find vite plus config with fmt field
+    root_markers = util.root_markers_with_field(
+      root_markers,
+      { 'vite.config.ts' },
+      { 'vite%-plus', 'fmt:' },
+      fname,
+      'all'
+    )
     on_dir(vim.fs.dirname(vim.fs.find(root_markers, { path = fname, upward = true })[1]))
   end,
 }
