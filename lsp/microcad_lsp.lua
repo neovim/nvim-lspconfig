26a10c48388e239ff878eb8ddb71a2b1859a52bd
Frederik Hansen <frederik@altofte.dk>
2026-06-18 18:01:27 +0200

    feat: added µcad lsp

---
+++ lsp/microcad_lsp.lua
@@ -0,0 +1,26 @@
+---@brief
+--- https://codeberg.org/microcad/microcad/src/branch/main/crates/lsp
+---
+--- An LSP for the µcad model description language
+---
+--- Install with
+--- ```sh
+--- cargo install microcad-lsp
+--- ```
+--- Neovim does not detect µcad filetype automatically, so you will need to add the following to your
+---
+--- ```lua
+--- vim.filetype.add {
+---   extension = {
+---     µcad = 'microcad',
+---   },
+--- }
+--- ```
+
+---@type vim.lsp.Config
+return {
+  name = "microcad_lsp",
+  cmd = { 'microcad-lsp', '--stdio' },
+  filetypes = { 'microcad' },
+  root_markers = { '.git' },
+}
