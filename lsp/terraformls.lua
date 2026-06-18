c2cf6ed3e8f34a9cfaf90085c540e2713e43677f
Gustav Eikaas <46537983+GustavEikaas@users.noreply.github.com>
2026-06-18 15:03:17 +0200

    feat(terraformls): enable codelens #4453
    ref https://github.com/hashicorp/terraform-ls/blob/84064cd19ffd0d6b356cd2725d81987111c94cb6/docs/language-clients.md#L162

--- lsp/terraformls.lua
+++ lsp/terraformls.lua
@@ -37,4 +37,12 @@ return {
   cmd = { 'terraform-ls', 'serve' },
   filetypes = { 'terraform', 'terraform-vars' },
   root_markers = { '.terraform', '.git' },
+  capabilities = {
+    experimental = {
+      showReferencesCommandId = 'client.showReferences',
+    },
+  },
+  on_attach = function(_, bufnr)
+    vim.lsp.codelens.enable(true, { bufnr = bufnr })
+  end,
 }
