ac5f8d8075e03c173b1715065411fec4e8ea7683
Harshit Tomar <harshitt345@gmail.com>
2026-07-01 21:59:55 +0530

    fix(ruff_lsp): deprecate ruff_lsp in favour of ruff
    Problem:
    ruff_lsp project is now archive and the tool deprecated
    
    Solution:
    add deprecation warning to the config description, also ensure that the
    user is warned when enabling the config using `vim.deprecate()`

--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -10,7 +10,7 @@
 ---
 --- **Available in Ruff `v0.4.5` in beta and stabilized in Ruff `v0.5.3`.**
 ---
---- This is the new built-in language server written in Rust. It supports the same feature set as `ruff-lsp`, but with superior performance and no installation required. Note that the `ruff-lsp` server will continue to be maintained until further notice.
+--- This is the new built-in language server written in Rust. It supports the same feature set as `ruff-lsp`, but with superior performance and no separate installation required.
 ---
 --- Server settings can be provided via:
 ---
