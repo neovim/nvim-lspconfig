aa8da460fd9c74ff1bb8b45e92baacd24cff8aaa
kindlyway <githubaccountusing@proton.me>
2026-07-11 12:02:53 +0000

    docs: qmlls6 #4470
    Some distros like arch have a separate package with a cmd of qmlls6.

--- lsp/qmlls.lua
+++ lsp/qmlls.lua
@@ -5,6 +5,11 @@
 --- > QML Language Server is a tool shipped with Qt that helps you write code in your favorite (LSP-supporting) editor.
 ---
 --- Source in the [QtDeclarative repository](https://code.qt.io/cgit/qt/qtdeclarative.git/)
+---
+--- Note: On some distros, the cmd of qmlls is `qmlls6`. You can override `cmd`:
+--- ```lua
+--- vim.lsp.config('qmlls', { cmd = { 'qmlls6' } })
+--- ```
 
 ---@type vim.lsp.Config
 return {
