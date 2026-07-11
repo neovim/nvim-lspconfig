468b550934fd2d7f9033f35d71e07fef5fa456e4
OkObjective <githubaccountusing@proton.me>
2026-07-11 19:17:06 +0800

    docs(qmlls): different qmlls cmd

--- lsp/qmlls.lua
+++ lsp/qmlls.lua
@@ -5,6 +5,8 @@
 --- > QML Language Server is a tool shipped with Qt that helps you write code in your favorite (LSP-supporting) editor.
 ---
 --- Source in the [QtDeclarative repository](https://code.qt.io/cgit/qt/qtdeclarative.git/)
+---
+--- Note: On some distros, the cmd of qmlls is `qmlls6`. **You can do this by changing `cmd` in your call to `vim.lsp.config`**
 
 ---@type vim.lsp.Config
 return {
