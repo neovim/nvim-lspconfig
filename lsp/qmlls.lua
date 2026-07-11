f3e9e8b4b9464ce0756a0f90687ee1326b7fae97
OkObjective <githubaccountusing@proton.me>
2026-07-11 19:41:58 +0800

    docs(qmlls): add info about different cmd

--- lsp/qmlls.lua
+++ lsp/qmlls.lua
@@ -6,7 +6,7 @@
 ---
 --- Source in the [QtDeclarative repository](https://code.qt.io/cgit/qt/qtdeclarative.git/)
 ---
---- Note: On some distros, the cmd of qmlls is `qmlls6`. **You can do this by changing `cmd` in your call to `vim.lsp.config`**
+--- Note: On some distros, the cmd of qmlls is `qmlls6`. **You can fix this by changing `cmd` to `qmlls6` in your call to `vim.lsp.config`**
 
 ---@type vim.lsp.Config
 return {
