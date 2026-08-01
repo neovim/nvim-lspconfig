4dd307d6764c933707ee4c2a37323079e61ea32d
€šm̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰̰�Ř§Ů Â£╟©舐æØ¢£ðsÞ¥¿— <memchr@proton.me>
2026-08-01 10:07:15 +0000

    docs(emmylua_ls): remove irrelevant issue link #4490
    emmylua_ls deduplicates all workspace files before analysing them.
    Therefore, using the recommended
    `workspace.library = vim.api.nvim_get_runtime_file("", true)`
    will not result in erroneous duplicate field warnings as discussed
    in #3189.

--- lsp/emmylua_ls.lua
+++ lsp/emmylua_ls.lua
@@ -43,7 +43,7 @@
 ---           -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
 ---           vim.api.nvim_get_runtime_file('lua/lspconfig', false)[1],
 ---         },
----         -- Or pull in all of 'runtimepath'. May be slower! https://github.com/neovim/nvim-lspconfig/issues/3189
+---         -- Or pull in all of 'runtimepath'. May be slower!
 ---         -- library = vim.api.nvim_get_runtime_file('', true),
 ---       },
 ---     },
