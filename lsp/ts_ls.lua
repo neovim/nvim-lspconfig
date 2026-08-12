033492baa0972c1a9e3916fc8d634ae6a9b8b155
Robert Liebowitz <rliebz@gmail.com>
2026-08-12 10:06:02 -0400

    feat: tsc #4493
    Problem:
    `tsgo` was a temporary executable available as a preview version for the
    Typescript 7.0 Go rewrite. Now that Typescript 7.0 has been released,
    the `tsgo` executable name and project are being rolled back into the
    main Typescript project. As of that release, the standard executable is
    installed from `npm` via the name `typescript`, and executable itself is
    named `tsc`.
    
    Solution:
    Deprecate `tsgo` as a language server name, and instead configure a more
    appropriately named language server that supports the permanent `tsc`
    executable name in addition to the `tsgo` executable name to keep
    backward compatibility.
    
    The only changes are names, docs, and within the `cmd` for `tsc`,
    support for both the `tsc` and `tsgo` executable names, with a
    preference for `tsc`.

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -4,6 +4,9 @@
 ---
 --- `ts_ls`, aka `typescript-language-server`, is a Language Server Protocol implementation for TypeScript wrapping `tsserver`. Note that `ts_ls` is not `tsserver`.
 ---
+--- While `typescript-language-server` will likely eventually be replaced by [tsc](#tsc), the official language server
+--- for TypeScript, `typescript-language-server` may still be useful for features such as es5 support.
+---
 --- `typescript-language-server` depends on `typescript`. Both packages can be installed via `npm`:
 --- ```sh
 --- npm install -g typescript typescript-language-server
@@ -37,41 +40,7 @@
 ---
 --- ### Monorepo support
 ---
---- `ts_ls` supports monorepos by default. It will automatically find the `tsconfig.json` or `jsconfig.json` corresponding to the package you are working on.
---- This works without the need of spawning multiple instances of `ts_ls`, saving memory.
----
---- It is recommended to use the same version of TypeScript in all packages, and therefore have it available in your workspace root. The location of the TypeScript binary will be determined automatically, but only once.
----
---- Some care must be taken here to correctly infer whether a file is part of a Deno program, or a TS program that
---- expects to run in Node or Web Browsers. This supports having a Deno module using the denols LSP as a part of a
---- mostly-not-Deno monorepo. We do this by finding the nearest package manager lock file, and the nearest deno.json
---- or deno.jsonc.
----
---- Example:
----
---- ```
---- project-root
---- +-- node_modules/...
---- +-- package-lock.json
---- +-- package.json
---- +-- packages
----     +-- deno-module
----     |   +-- deno.json
----     |   +-- package.json <-- It's normal for Deno projects to have package.json files!
----     |   +-- src
----     |       +-- index.ts <-- this is a Deno file
----     +-- node-module
----         +-- package.json
----         +-- src
----             +-- index.ts <-- a non-Deno file (ie, should use ts_ls or tsgols)
---- ```
----
---- From the file being edited, we walk up to find the nearest package manager lockfile. This is PROJECT ROOT.
---- From the file being edited, find the nearest deno.json or deno.jsonc. This is DENO ROOT.
---- From the file being edited, find the nearest deno.lock. This is DENO LOCK ROOT
---- If DENO LOCK ROOT is found, and PROJECT ROOT is missing or shorter, then this is a deno file, and we abort.
---- If DENO ROOT is found, and it's longer than or equal to PROJECT ROOT, then this is a Deno file, and we abort.
---- Otherwise, attach at PROJECT ROOT, or the cwd if not found.
+--- See the monorepo support documentation for [tsc](#tsc)
 
 ---@type vim.lsp.Config
 return {
