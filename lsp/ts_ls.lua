87a8c3e23c45e8d9480fccb0887c1aad59b45ab8
Justin M. Keyes <justinkz@gmail.com>
2026-09-06 14:39:47 -0400

    Apply suggestion from justinmk

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -9,8 +9,8 @@
 ---
 --- `typescript-language-server` depends on `typescript`. Both packages can be installed via `npm`:
 --- ```sh
---- npm install -g typescript@<version> typescript-language-server
 --- # if you are using `ts_ls` you will need to install a `typescript` version < 7 for now
+--- npm install -g typescript@<version> typescript-language-server
 --- ```
 ---
 --- To configure typescript language server, add a
